05e6f0cb64e2823a99354bf9d2bbb1435b9b1a8e
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2008-11-11 03:28:44 +0000

    Prevent another file descriptor leak in taglib
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406167

--- src/formats/taglib_plug.ml
+++ src/formats/taglib_plug.ml
@@ -29,26 +29,29 @@ let get_tags ~format fname =
       raise Not_found ;
 
     let f = open_file ~file_type:Mpeg fname in
-    let ans = ref [] in
-    let gt (n, t) =
-      try
-        ans := (n, t f)::!ans
-      with
-        | _ -> ()
-    in
-      List.iter gt
-        [
-          "Title", tag_title;
-          "Artist", tag_artist;
-          "Album", tag_album;
-          "Track", (fun x -> string_of_int (tag_track x));
-          "Year", (fun x -> string_of_int (tag_year x));
-          "Genre", tag_genre;
-          "Comment", tag_comment;
-        ];
-      close_file f;
-      !ans
+    try
+      let ans = ref [] in
+      let gt (n, t) =
+        try
+          ans := (n, t f)::!ans
+        with
+          | _ -> ()
+      in
+        List.iter gt
+          [
+            "Title", tag_title;
+            "Artist", tag_artist;
+            "Album", tag_album;
+            "Track", (fun x -> string_of_int (tag_track x));
+            "Year", (fun x -> string_of_int (tag_year x));
+            "Genre", tag_genre;
+            "Comment", tag_comment;
+          ];
+        close_file f;
+        !ans
+    with
+      | _ -> close_file f; raise Not_found
   with
-    | _ -> []
+    | _ -> raise Not_found
 
 let () = Request.mresolvers#register "MP3" get_tags 
