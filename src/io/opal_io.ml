1633513627d364929e12b94875aad2aef4392057
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2009-01-23 13:50:07 +0000

    More code towards VOIP.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406347

--- src/io/opal_io.ml
+++ src/io/opal_io.ml
@@ -1,7 +1,19 @@
 type t
 external init : unit -> t = "caml_opal_init"
 external shutdown : t -> unit = "caml_opal_shutdown"
+external set_general_parameters : t -> string -> string -> (string -> string -> unit) -> unit = "caml_opal_set_general_parameters"
 external set_protocol_parameters : t -> string -> string -> string -> unit = "caml_opal_set_protocol_parameters"
+
+(** {2 Messages} *)
+type message
+external get_message : t -> int option -> message = "caml_opal_get_message"
+let get_message ?timeout h = get_message h timeout
+external free_message : message -> unit = "caml_opal_free_message"
+type message_type =
+  | Type_unknown
+  | Type_ind_incoming_call of string * string * string * string * string * string * string (** call token, local address, remote address, remote party number, remote display name, called address, called party number *)
+external get_message_type : message -> message_type = "caml_opal_get_message_type"
+external answer_call : t -> string -> unit = "caml_opal_answer_call"
 
 class input =
 object (self)
@@ -17,9 +29,25 @@ object (self)
   method output_get_ready =
     (* TODO: init only once *)
     let h = init () in
+    let reader () =
+      while true do
+        let m = get_message h in
+          (
+            Printf.printf "Got message!\n%!";
+            match get_message_type m with
+              | Type_ind_incoming_call (t, la, ra, rpn, rdn, ca, cpn) ->
+                  Printf.printf "Call from %s (%s to %s).\n%!" rdn ra la;
+                  answer_call h t
+              | _ -> ()
+          );
+          free_message m
+      done
+    in
       handle <- Some h;
       ignore (Dtools.Init.at_stop (fun () -> shutdown h));
-      set_protocol_parameters h "liq" "Liquidsoap live" "*"
+      set_general_parameters h "audio" "audio" (fun f d -> Printf.printf "Read %s.\n%!" f);
+      set_protocol_parameters h "liq" "Liquidsoap live" "*";
+      ignore (Thread.create reader ())
 
   method output_reset = ()
   method is_active = true
@@ -35,7 +63,6 @@ object (self)
       AFrame.add_break frame (AFrame.size frame)
 end
 
-(*
 let () =
   Lang.add_operator "input.voip"
     [
@@ -45,4 +72,3 @@ let () =
     (fun p ->
        (* let e f v = f (List.assoc v p) in *)
          ((new input):>Source.source))
-*)
--- src/io/opal_io_c.c
+++ src/io/opal_io_c.c
@@ -4,8 +4,10 @@
 #include <caml/memory.h>
 #include <caml/misc.h>
 #include <caml/mlvalues.h>
+#include <caml/signals.h>
 
 #include <opal.h>
+#include <limits.h>
 #include <assert.h>
 #include <stdio.h>
 
@@ -67,31 +69,44 @@ CAMLprim value caml_opal_init(value unit)
   }
 
   version = OPAL_C_API_VERSION;
-  assert(hOPAL = InitialiseFunction(&version, OPAL_PREFIX_H323 " " OPAL_PREFIX_SIP " " OPAL_PREFIX_IAX2 " " OPAL_PREFIX_LOCAL " TraceLevel=4"));
+  caml_enter_blocking_section();
+  hOPAL = InitialiseFunction(&version, OPAL_PREFIX_H323 " " OPAL_PREFIX_SIP " " OPAL_PREFIX_IAX2 " " OPAL_PREFIX_LOCAL " TraceLevel=4");
+  caml_leave_blocking_section();
+  assert(hOPAL);
 
   return (value)hOPAL;
 }
 
 CAMLprim value caml_opal_shutdown(value h)
 {
-  ShutDownFunction(Handle_val(h));
+  OpalHandle hOPAL = Handle_val(h);
+
+  caml_enter_blocking_section();
+  ShutDownFunction(hOPAL);
+  caml_leave_blocking_section();
 
   return Val_unit;
 }
 
+/* This function is called in a blocking section! */
 /* TODO: raise exceptions */
-static OpalMessage *MySendCommand(OpalHandle hOPAL, OpalMessage *command, const char *errorMessage)
+static OpalMessage *MySendCommand(OpalHandle hOPAL, OpalMessage *command)
 {
   OpalMessage *response;
-  if ((response = SendMessageFunction(hOPAL, command)) == NULL)
+
+  caml_enter_blocking_section();
+  response = SendMessageFunction(hOPAL, command);
+  caml_leave_blocking_section();
+
+  if (!response)
     return NULL;
   if (response->m_type != OpalIndCommandError)
     return response;
 
   if (response->m_param.m_commandError == NULL || *response->m_param.m_commandError == '\0')
-    printf("%s.\n", errorMessage);
+    printf("OPAL error.\n");
   else
-    printf("%s: %s\n", errorMessage, response->m_param.m_commandError);
+    printf("OPAL error: %s\n", response->m_param.m_commandError);
 
   FreeMessageFunction(response);
 
@@ -103,8 +118,8 @@ CAMLprim value caml_opal_set_protocol_parameters(value h, value username, value 
   /* TODO: product */
   /* TODO: blocking section */
   OpalHandle hOPAL = Handle_val(h);
-  OpalMessage   command;
-  OpalMessage * response;
+  OpalMessage command;
+  OpalMessage *response;
 
   memset(&command, 0, sizeof(command));
   command.m_type = OpalCmdSetProtocolParameters;
@@ -113,17 +128,129 @@ CAMLprim value caml_opal_set_protocol_parameters(value h, value username, value 
   command.m_param.m_protocol.m_displayName = String_val(displayname);
   command.m_param.m_protocol.m_interfaceAddresses = String_val(interface);
 
-  assert(response = MySendCommand(hOPAL, &command, "Could not set protocol options"));
-
+  assert(response = MySendCommand(hOPAL, &command));
   FreeMessageFunction(response);
 
   return Val_unit;
 }
 
-CAMLprim value caml_opal_listen(value h, value cb)
+/* TODO: not a global value */
+value write_cb;
+
+int MyWriteMediaData(const char *token, const char *id, const char *format, void *userData, void *data, int size)
+{
+  value vdata;
+
+  caml_leave_blocking_section();
+
+  vdata = caml_alloc_string(size);
+  memcpy(String_val(vdata), data, size);
+  /* Also give the token and the id? */
+  caml_callback2(write_cb, caml_copy_string(format), vdata);
+  caml_enter_blocking_section();
+
+  return size;
+}
+
+CAMLprim value caml_opal_set_general_parameters(value h, value auto_rx_media, value auto_tx_media, value write_callback)
 {
   OpalHandle hOPAL = Handle_val(h);
-  /* HandleMessages(timeout, UINT_MAX); */
+  OpalMessage command;
+  OpalMessage *response;
 
+  memset(&command, 0, sizeof(command));
+  command.m_type = OpalCmdSetGeneralParameters;
+  command.m_param.m_general.m_autoRxMedia = String_val(auto_rx_media);
+  command.m_param.m_general.m_autoTxMedia = String_val(auto_tx_media);
+  command.m_param.m_general.m_mediaWriteData = MyWriteMediaData;
+  command.m_param.m_general.m_mediaDataHeader = OpalMediaDataPayloadOnly; /* TODO: param */
+
+  write_cb = write_callback;
+  caml_register_global_root(&write_cb); /* TODO: unregister */
+
+  assert(response = MySendCommand(hOPAL, &command));
+  FreeMessageFunction(response);
+
+  return Val_unit;
+}
+
+#define Message_val(v) ((OpalMessage*)v)
+
+/* TODO: finalizer */
+static value val_message(OpalMessage *message)
+{
+  return (value)message;
+}
+
+/* TODO: finalizer */
+CAMLprim value caml_opal_free_message(value msg)
+{
+  OpalMessage *message = Message_val(msg);
+
+  caml_enter_blocking_section();
+  FreeMessageFunction(message);
+  caml_leave_blocking_section();
+
+  return Val_unit;
+}
+
+CAMLprim value caml_opal_get_message(value h, value timeout)
+{
+  OpalHandle hOPAL = Handle_val(h);
+  OpalMessage *message;
+
+  caml_enter_blocking_section();
+  message = GetMessageFunction(hOPAL, UINT_MAX); /* TODO: use timeout */
+  caml_leave_blocking_section();
+
+  assert(message);
+
+  return val_message(message);
+}
+
+CAMLprim value caml_opal_get_message_type(value msg)
+{
+  CAMLparam1(msg);
+  CAMLlocal1(ans);
+  OpalMessage *message = Message_val(msg);
+
+  switch (message->m_type)
+  {
+    case OpalIndIncomingCall:
+      ans = caml_alloc(7, 0);
+      Store_field(ans, 0, caml_copy_string(message->m_param.m_incomingCall.m_callToken));
+      Store_field(ans, 1, caml_copy_string(message->m_param.m_incomingCall.m_localAddress));
+      Store_field(ans, 2, caml_copy_string(message->m_param.m_incomingCall.m_remoteAddress));
+      Store_field(ans, 3, caml_copy_string(message->m_param.m_incomingCall.m_remotePartyNumber));
+      Store_field(ans, 4, caml_copy_string(message->m_param.m_incomingCall.m_remoteDisplayName));
+      Store_field(ans, 5, caml_copy_string(message->m_param.m_incomingCall.m_calledAddress));
+      Store_field(ans, 6, caml_copy_string(message->m_param.m_incomingCall.m_calledPartyNumber));
+      CAMLreturn(ans);
+
+    case OpalIndAlerting:
+      break;
+
+    default:
+      break;
+  }
+
+  CAMLreturn(Val_int(0));
+}
+
+CAMLprim value caml_opal_answer_call(value h, value token)
+{
+  OpalHandle hOPAL = Handle_val(h);
+  OpalMessage command;
+  OpalMessage *response;
+  char *t = malloc(caml_string_length(token));
+
+  memcpy(t, String_val(token), caml_string_length(token));
+  memset(&command, 0, sizeof(command));
+  command.m_type = OpalCmdAnswerCall;
+  command.m_param.m_callToken = t;
+  assert(response = MySendCommand(hOPAL, &command));
+  FreeMessageFunction(response);
+
+  free(t);
   return Val_unit;
 }
