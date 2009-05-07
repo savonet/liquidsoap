8b3163204ade095bab3dfe65e4303ccffb72d143
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2009-05-07 07:27:58 +0000

    Add support for AAC in MP4.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406562

--- src/formats/aacformat.ml
+++ src/formats/aacformat.ml
@@ -113,6 +113,73 @@ let decoder file =
   in
     { Decoder.fill = fill ; Decoder.close = close }
 
+let decoder_mp4 file =
+  let dec = Faad.create () in
+  let fd = Unix.openfile file [Unix.O_RDONLY] 0o644 in
+  let mp4 = Faad.Mp4.openfile_fd fd in
+
+  let abg = Generator.create () in
+  let buffer_length = Decoder.buffer_length () in
+
+  let track = Faad.Mp4.find_aac_track mp4 in
+  let sample_freq, chans = Faad.Mp4.init mp4 dec track in
+  let samples = Faad.Mp4.samples mp4 track in
+  let sample = ref 0 in
+
+  let closed = ref false in
+  let close () =
+    assert (not !closed) ;
+    closed := true ;
+    Faad.close dec;
+    Unix.close fd
+  in
+
+  let fill buf =
+    assert (not !closed) ;
+
+    begin
+      try
+        while Generator.length abg < buffer_length do
+          try
+            if !sample >= samples then raise End_of_file;
+            Generator.feed abg ~sample_freq (Faad.Mp4.decode mp4 track !sample dec);
+            incr sample
+          with
+            | Faad.Error n ->
+                Printf.printf "Faad error: %s\n%!" (Faad.error_message n)
+        done
+      with _ -> () (* TODO: log the error *)
+    end ;
+
+    (* TODO: duration *)
+    (*
+    let offset = AFrame.position buf in
+    *)
+      Float_pcm.Generator.fill abg buf ;
+    (*
+      in_bytes := Unix.lseek fd 0 Unix.SEEK_CUR ;
+      out_samples := !out_samples + AFrame.position buf - offset ;
+      (* Compute an estimated number of remaining ticks. *)
+      let abglen = Generator.length abg in
+        assert (!in_bytes!=0) ;
+        let compression =
+          (float (!out_samples+abglen)) /. (float !in_bytes)
+        in
+        let remaining_samples =
+          (float (file_size - !in_bytes)) *. compression
+          +. (float abglen)
+        in
+          (* I suspect that in_bytes in not accurate, since I don't
+           * get an exact countdown after than in_size=in_bytes, but there
+           * is a stall at the beginning after which the countdown starts. *)
+          Fmt.ticks_of_samples (int_of_float remaining_samples)
+     *)
+    0
+  in
+    { Decoder.fill = fill ; Decoder.close = close }
+
 let () =
   Decoder.formats#register "AAC"
-    (fun name -> try Some (decoder name) with _ -> None)
+    (fun name -> try Some (decoder name) with _ -> None);
+  Decoder.formats#register "AACMP4"
+    (fun name -> try Some (decoder_mp4 name) with _ -> None)
