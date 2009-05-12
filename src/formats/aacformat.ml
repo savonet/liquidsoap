79487fb4439d90edc7b9e03c76fc8ad5063ecf7d
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2009-05-12 23:09:28 +0000

    Fix file descriptor leak in aacformat.ml..
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406574

--- src/formats/aacformat.ml
+++ src/formats/aacformat.ml
@@ -30,37 +30,40 @@ let decoder =
   let openfile file = 
     let dec = Faad.create () in
     let fd = Unix.openfile file [Unix.O_RDONLY] 0o644 in
-    let abg = Generator.create () in
-    let aacbuflen = 1024 in
-    let aacbuf = String.create aacbuflen in
-    let aacbufpos = ref aacbuflen in
-    let pos = ref 0 in
-    let fill_aacbuf () =
-      String.blit aacbuf !aacbufpos aacbuf 0 (aacbuflen - !aacbufpos);
-      let n = Unix.read fd aacbuf (aacbuflen - !aacbufpos) !aacbufpos in
-      let n = !aacbufpos + n in
-      aacbufpos := 0;
-      pos := !pos + n;
-      n
-    in
+    try
+      let abg = Generator.create () in
+      let aacbuflen = 1024 in
+      let aacbuf = String.create aacbuflen in
+      let aacbufpos = ref aacbuflen in
+      let pos = ref 0 in
+      let fill_aacbuf () =
+        String.blit aacbuf !aacbufpos aacbuf 0 (aacbuflen - !aacbufpos);
+        let n = Unix.read fd aacbuf (aacbuflen - !aacbufpos) !aacbufpos in
+        let n = !aacbufpos + n in
+        aacbufpos := 0;
+        pos := !pos + n;
+        n
+      in
 
-    (* Dummy decoding in order to test format. *)
-    let () =
-      let offset, _, _ = Faad.init dec aacbuf 0 (Unix.read fd aacbuf 0 aacbuflen) in
+      (* Dummy decoding in order to test format. *)
+      let () =
+        let offset, _, _ = Faad.init dec aacbuf 0 (Unix.read fd aacbuf 0 aacbuflen) in
+        ignore (Unix.lseek fd offset Unix.SEEK_SET);
+        let aacbuflen = fill_aacbuf () in
+        if aacbuflen = 0 then raise End_of_file;
+        if aacbuf.[0] <> '\255' then raise End_of_file;
+        ignore (Faad.decode dec aacbuf 0 aacbuflen);
+        ignore (Unix.lseek fd 0 Unix.SEEK_SET)
+      in
+
+      let offset, sample_freq, chans =
+        Faad.init dec aacbuf 0 (Unix.read fd aacbuf 0 aacbuflen)
+      in
+      aacbufpos := aacbuflen;
       ignore (Unix.lseek fd offset Unix.SEEK_SET);
-      let aacbuflen = fill_aacbuf () in
-      if aacbuflen = 0 then raise End_of_file;
-      if aacbuf.[0] <> '\255' then raise End_of_file;
-      ignore (Faad.decode dec aacbuf 0 aacbuflen);
-      ignore (Unix.lseek fd 0 Unix.SEEK_SET)
-    in
-
-    let offset, sample_freq, chans =
-      Faad.init dec aacbuf 0 (Unix.read fd aacbuf 0 aacbuflen)
-    in
-    aacbufpos := aacbuflen;
-    ignore (Unix.lseek fd offset Unix.SEEK_SET);
-    (dec,fd,aacbuf,fill_aacbuf,aacbufpos,sample_freq,pos),abg
+      (dec,fd,aacbuf,fill_aacbuf,aacbufpos,sample_freq,pos),abg
+    with
+      | e -> Unix.close fd; raise e
   in
   let close (dec,fd,_,_,_,_,_) =
     Faad.close dec;
@@ -97,13 +100,16 @@ let decoder_mp4 =
   let openfile file = 
     let dec = Faad.create () in
     let fd = Unix.openfile file [Unix.O_RDONLY] 0o644 in
-    let mp4 = Faad.Mp4.openfile_fd fd in
-    let abg = Generator.create () in
-    let track = Faad.Mp4.find_aac_track mp4 in
-    let sample_freq, chans = Faad.Mp4.init mp4 dec track in
-    let samples = Faad.Mp4.samples mp4 track in
-    let sample = ref 0 in
-    (dec,fd,track,mp4,sample,samples,sample_freq),abg
+    try
+      let mp4 = Faad.Mp4.openfile_fd fd in
+      let abg = Generator.create () in
+      let track = Faad.Mp4.find_aac_track mp4 in
+      let sample_freq, chans = Faad.Mp4.init mp4 dec track in
+      let samples = Faad.Mp4.samples mp4 track in
+      let sample = ref 0 in
+      (dec,fd,track,mp4,sample,samples,sample_freq),abg
+    with
+      | e -> Unix.close fd; raise e
   in
   let close (dec,fd,_,_,_,_,_) =
     Faad.close dec;
