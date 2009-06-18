2874f56b31a6b272f84897fdd6a2a404ec08f489
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2009-06-18 21:58:07 +0000

    Fix wrong use of taglib: reverted to 0.3.8 behaviour (and also removed some imperative code !)
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406664

--- src/formats/taglib_plug.ml
+++ src/formats/taglib_plug.ml
@@ -23,17 +23,18 @@
 open Taglib
 
 let get_tags ~format fname =
+  if format <> "MP3" then [] else
   try
     let f = open_file ~file_type:Mpeg fname in
     try
-      let ans = ref [] in
-      let gt (n, t) =
+      let gt l (n, t) =
         try
-          ans := (n, t f)::!ans
+          (n, t f) :: l
         with
-          | _ -> ()
+          | _ -> l
       in
-        List.iter gt
+      let ans = 
+        List.fold_left gt []
           [
             "Title", tag_title;
             "Artist", tag_artist;
@@ -42,9 +43,10 @@ let get_tags ~format fname =
             "Year", (fun x -> string_of_int (tag_year x));
             "Genre", tag_genre;
             "Comment", tag_comment;
-          ];
-        close_file f;
-        !ans
+          ]
+      in
+      close_file f;
+      ans
     with
       | _ -> close_file f; raise Not_found
   with
