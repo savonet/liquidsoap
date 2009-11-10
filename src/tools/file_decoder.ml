d9deefb7566854614f8122e5b71dff5070b6d1ba
dbaelde@aec24677-d710-0410-a355-ac75e2bdf181 <dbaelde@aec24677-d710-0410-a355-ac75e2bdf181>
2009-11-10 04:38:22 +0000

    Whitespace.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406919

--- src/tools/file_decoder.ml
+++ src/tools/file_decoder.ml
@@ -22,93 +22,93 @@
 
 (** Generic file decoder. *)
 
-  type 'a file_decoder = 
-    {
-      log      : Dtools.Log.t;
-      openfile : string -> 'a;
-      get_type : 'a -> Frame.content_type;
-      decode   : 'a -> Generator.t -> unit; 
-      position : 'a -> int;
-      close    : 'a -> unit
-    }
+type 'a file_decoder =
+  {
+    log      : Dtools.Log.t;
+    openfile : string -> 'a;
+    get_type : 'a -> Frame.content_type;
+    decode   : 'a -> Generator.t -> unit;
+    position : 'a -> int;
+    close    : 'a -> unit
+  }
 
-  let decode decoder file = 
-    let fd =
-      decoder.log#f 5 "open %S" file ;
-      try
-        decoder.openfile file
-      with
-        | e -> 
-           decoder.log#f 5 "Could not decode file." ;
-           raise e
-    in
-    let abg = Generator.create () in
-    let buffer_length =
-      Frame.audio_of_seconds Decoder.conf_buffer_length#get
-    in
-    let stats = Unix.stat file in
-    let file_size = stats.Unix.st_size in
-    let out_samples = ref 0 in
-    let closed = ref false in
-    let close () =
+let decode decoder file =
+  let fd =
+    decoder.log#f 5 "open %S" file ;
+    try
+      decoder.openfile file
+    with
+      | e ->
+         decoder.log#f 5 "Could not decode file." ;
+         raise e
+  in
+  let abg = Generator.create () in
+  let buffer_length =
+    Frame.audio_of_seconds Decoder.conf_buffer_length#get
+  in
+  let stats = Unix.stat file in
+  let file_size = stats.Unix.st_size in
+  let out_samples = ref 0 in
+  let closed = ref false in
+  let close () =
+    assert (not !closed) ;
+    closed := true ;
+    decoder.log#f 5 "close %S" file ;
+    decoder.close fd
+  in
+ let fill =
+    fun buf ->
       assert (not !closed) ;
-      closed := true ;
-      decoder.log#f 5 "close %S" file ;
-      decoder.close fd
-    in
-   let fill =
-      fun buf ->
-        assert (not !closed) ;
-  
-        begin
-          try
-            while Generator.length abg < buffer_length do
-              decoder.decode fd abg
-            done
-          with
-            | _ -> () 
-        end ;
-  
-        let offset = AFrame.position buf in
-          Generator.fill abg buf ;
-          let in_bytes = decoder.position fd in
-          out_samples := !out_samples + AFrame.position buf - offset ;
-          (* Compute an estimated number of remaining ticks. *)
-          let abglen = Generator.length abg in
-            if in_bytes = 0 then
-              0
-            else
-              let compression =
-                (float (!out_samples+abglen)) /. (float in_bytes)
-              in
-              let remaining_samples =
-                (float (file_size - in_bytes)) *. compression
-                +. (float abglen)
-              in
-                (* I suspect that in_bytes in not accurate, since I don't
-                 * get an exact countdown after that in_size=in_bytes.
-                 * Instead, there is a stall at the beginning
-                 * after which the countdown starts. *)
-                Frame.master_of_audio (int_of_float remaining_samples)
-    in
-      { Decoder.fill = fill ; Decoder.close = close }
 
-   (** Top-level wrapper to also check content_type *)
-   let decode decoder name kind = 
-     try
-       let data_type = 
-         let data = decoder.openfile name in
-         let data_type = 
-          try
-            decoder.get_type data
-          with
-           | e -> decoder.close data; raise e
-         in
-         decoder.close data; 
-         data_type
+      begin
+        try
+          while Generator.length abg < buffer_length do
+            decoder.decode fd abg
+          done
+        with
+          | _ -> ()
+      end ;
+
+      let offset = AFrame.position buf in
+        Generator.fill abg buf ;
+        let in_bytes = decoder.position fd in
+        out_samples := !out_samples + AFrame.position buf - offset ;
+        (* Compute an estimated number of remaining ticks. *)
+        let abglen = Generator.length abg in
+          if in_bytes = 0 then
+            0
+          else
+            let compression =
+              (float (!out_samples+abglen)) /. (float in_bytes)
+            in
+            let remaining_samples =
+              (float (file_size - in_bytes)) *. compression
+              +. (float abglen)
+            in
+              (* I suspect that in_bytes in not accurate, since I don't
+               * get an exact countdown after that in_size=in_bytes.
+               * Instead, there is a stall at the beginning
+               * after which the countdown starts. *)
+              Frame.master_of_audio (int_of_float remaining_samples)
+  in
+    { Decoder.fill = fill ; Decoder.close = close }
+
+ (** Top-level wrapper to also check content_type *)
+ let decode decoder name kind =
+   try
+     let data_type =
+       let data = decoder.openfile name in
+       let data_type =
+        try
+          decoder.get_type data
+        with
+         | e -> decoder.close data; raise e
        in
-       if Frame.type_has_kind data_type kind then
-         Some (decode decoder name)
-       else
-         None
-      with _ -> None
+       decoder.close data;
+       data_type
+     in
+     if Frame.type_has_kind data_type kind then
+       Some (decode decoder name)
+     else
+       None
+    with _ -> None
