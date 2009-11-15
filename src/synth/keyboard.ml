a48523c823c1884a764d80c7c3eff37c8ba0bdbc
dbaelde@aec24677-d710-0410-a355-ac75e2bdf181 <dbaelde@aec24677-d710-0410-a355-ac75e2bdf181>
2009-11-15 23:42:29 +0000

    Use content_of_kind for writing in the midi channel. Also cleanup a bit the reading task so that it terminates at least when liquidsoap shuts down -- though you still need to enter a character to unlock it...
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406977

--- src/synth/keyboard.ml
+++ src/synth/keyboard.ml
@@ -20,7 +20,9 @@
 
  *****************************************************************************)
 
-let knotes = [|'a'; '?'; 'z'; '"'; 'e'; 'r'; '('; 't'; '-'; 'y'; '?'; 'u'; 'i'; '?'; 'o'; '?'; 'p'|]
+let knotes =
+  [|'a'; '?'; 'z'; '"'; 'e'; 'r'; '('; 't'; '-'; 'y'; '?';
+    'u'; 'i'; '?'; 'o'; '?'; 'p'|]
 
 let array_index a x =
   let ans = ref None in
@@ -47,62 +49,64 @@ object (self)
   val mutable ev = []
   val ev_m = Mutex.create ()
 
-  method add_event (t:int) (e:Midi.event) =
+  method private add_event (t:int) (e:Midi.event) =
     Mutex.lock ev_m;
     ev <- (t,e)::ev;
     Mutex.unlock ev_m
 
-  method get_events =
+  method private get_events =
     Mutex.lock ev_m;
     let e = List.rev ev in
       ev <- [];
       Mutex.unlock ev_m;
       e
 
+  (** This source is not really clean for production, due to its
+    * highly blocking reading task: it is hard to get it to
+    * stop and restart cleanly when the source is asked to do so.
+    * So we start it once for all and never stop it,
+    * unless the full app stops. *)
   val mutable reader = None
 
-  method output_get_ready =
+  method private sleep = ()
+
+  method private output_get_ready =
     if reader = None then
-      reader <-
-      Some
-        (Tutils.create
-           (fun () ->
-              while true do
-                let c =
-                  let c = String.create 1 in
-                    ignore (Unix.read Unix.stdin c 0 1);
-                    c.[0]
-                in
-                  try
-                    Printf.printf "\nPlaying note %d.\n%!" (note_of_char c);
-                    self#add_event 0 (Midi.Note_on (note_of_char c, 0.8))
-                  with
-                    | Not_found -> ()
-              done
-           ) () "Virtual keyboard")
+      let task () =
+        while not !Root.shutdown do
+          let c =
+            let c = String.create 1 in
+              ignore (Unix.read Unix.stdin c 0 1);
+              c.[0]
+          in
+            try
+              Printf.printf "\nPlaying note %d.\n%!" (note_of_char c);
+              self#add_event 0 (Midi.Note_on (note_of_char c, 0.8))
+               with
+              | Not_found -> ()
+        done
+      in
+        reader <- Some (Tutils.create task () "Virtual keyboard")
 
   method output_reset = ()
   method is_active = true
 
-  method get_frame frame =
+  method private get_frame frame =
     assert (0 = MFrame.position frame);
-    let m = MFrame.content frame 0 in
+    let m = MFrame.content_of_type ~channels:1 frame 0 in
     let t = self#get_events in
       for c = 0 to Array.length m - 1 do
         m.(c) := t
       done;
       MFrame.add_break frame (MFrame.size ())
+
 end
 
 let () =
   Lang.add_operator "input.keyboard"
-    [
-    ]
+    []
     ~kind:Lang.midi_one
     ~category:Lang.Input
     ~flags:[Lang.Hidden; Lang.Experimental]
     ~descr:"Play notes from the keyboard."
-    (fun p kind ->
-       (* let e f v = f (List.assoc v p) in *)
-         ((new keyboard ~kind):>Source.source)
-    )
+    (fun p kind -> ((new keyboard ~kind):>Source.source))
