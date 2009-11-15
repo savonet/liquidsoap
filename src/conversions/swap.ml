bfe5ec8aba7c3413dfc537f5823141e2af91c056
dbaelde@aec24677-d710-0410-a355-ac75e2bdf181 <dbaelde@aec24677-d710-0410-a355-ac75e2bdf181>
2009-11-15 23:56:03 +0000

    Move swap to conversions, avoid a copy when we're at the beginning of a frame, and fix indices of audio channels.
    --HG--
    rename : liquidsoap/src/operators/swap.ml => liquidsoap/src/conversions/swap.ml
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406983

--- src/conversions/swap.ml
+++ src/conversions/swap.ml
@@ -1,6 +1,6 @@
 (*****************************************************************************
 
-  Liquidsoap, a programmable audio stream generator.
+  Liquidsoap, a programmable stream generator.
   Copyright 2003-2009 Savonet team
 
   This program is free software; you can redistribute it and/or modify
@@ -33,12 +33,16 @@ object (self)
 
   method private get_frame buf =
     let offset = AFrame.position buf in
-      source#get buf ;
-      let buffer = AFrame.content buf offset in
+    let buffer = source#get buf ; AFrame.content buf offset in
+      if offset = 0 then
+        let tmp = buffer.(1) in
+          buffer.(1) <- buffer.(2) ;
+          buffer.(2) <- tmp
+      else
         for i = offset to AFrame.position buf -1 do
-           let tmp = buffer.(1).(i) in
-              buffer.(1).(i) <- buffer.(2).(i);
-              buffer.(2).(i) <- tmp
+          let tmp = buffer.(0).(i) in
+            buffer.(0).(i) <- buffer.(1).(i) ;
+            buffer.(1).(i) <- tmp
         done
 end
 
