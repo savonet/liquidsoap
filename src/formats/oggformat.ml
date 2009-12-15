b8306b0a7f73f958938842b966df4010e26d87ac
dbaelde@aec24677-d710-0410-a355-ac75e2bdf181 <dbaelde@aec24677-d710-0410-a355-ac75e2bdf181>
2009-12-15 07:13:18 +0000

    Add video resampling using a simple nearest neighbour algo. Add a state record that keeps all local state. Move the video converter to that state instead of having a global one, which lead to strange bugs before (e.g. decoding works for one file, but not if another single(video) has been created). Remove long lines of blank and other minor cleanup.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407048

--- src/formats/oggformat.ml
+++ src/formats/oggformat.ml
@@ -28,34 +28,30 @@ module Generator = Generator.From_audio_video
 
 exception Channels of int
 
-(** Yeah, that's lazy type for the format :-P *)
-type 'a format = { mutable converter : 'a option }
-
-let converter () = 
-  let current_format = { converter = None } in
-  (fun format -> 
-    let format = 
+let converter () =
+  let current_format = ref None in
+  (fun format ->
+    let format =
       match format with
         | Ogg_demuxer.Yuvj_422 -> Video_converter.Yuvj_422
         | Ogg_demuxer.Yuvj_420 -> Video_converter.Yuvj_420
         | Ogg_demuxer.Yuvj_444 -> Video_converter.Yuvj_444
     in
-    match current_format.converter with
+    match !current_format with
       | Some x when fst(x) = format -> snd(x)
       | _ ->
-        let converter =  
+        let converter =
           Video_converter.find_converter
              (Video_converter.YUV format)
              (Video_converter.RGB Video_converter.Rgba_32)
         in
-        current_format.converter <- Some (format,converter) ;
+        current_format := Some (format,converter) ;
         converter)
-      
 
-(** Convert a frame *)
-let video_convert () = 
+(** Convert a video frame to RGB *)
+let video_convert () =
   let converter = converter () in
-  (fun buf -> 
+  (fun buf ->
     let converter = converter buf.Ogg_demuxer.format in
     let width = Lazy.force Frame.video_width in
     let height = Lazy.force Frame.video_height in
@@ -71,8 +67,57 @@ let video_convert () =
       frame;
     rgb)
 
+(** Stupid nearest neighbour resampling.
+  * For meaningful results, one should first partially apply the freq params,
+  * and re-use the resulting functions on consecutive chunks of a single
+  * input stream. *)
+let resample ~in_freq ~out_freq =
+  (* We have something like this:
+   *
+   * i i i i i i i i i i i i i i i i i i i ...
+   * o     o       o     o       o     o   ...
+   *
+   * (1) We ensure that out_len/out_freq = in_len/in_freq asymptotically.
+   *     For doing so, we must keep track of the full input length,
+   *     modulo in_freq.
+   * (2) We do the simplest possible thing to choose which i becomes
+   *     which o: nearest neighbour in the currently available buffer.
+   *     This is not as good as nearest neighbour in the real stream.
+   *
+   * Turns out the same code codes for when out_freq>in_freq too. *)
+  let in_pos = ref 0 in
+    fun input off len ->
+      let new_in_pos = !in_pos+len in
+      let already_out_len = !in_pos * out_freq / in_freq in
+      let needed_out_len = new_in_pos * out_freq / in_freq in
+      let out_len = needed_out_len - already_out_len in
+        in_pos := new_in_pos mod in_freq ;
+        Array.init
+          out_len
+          (fun i -> input.(off + i * in_freq / out_freq))
+
+let video_resample () =
+  let in_out = ref None in
+  let resampler = ref None in
+    fun ~in_freq ~out_freq buf off len ->
+      if !in_out = Some (in_freq,out_freq) then
+        (Utils.get_some !resampler) buf off len
+      else begin
+        in_out := Some (in_freq,out_freq) ;
+        resampler := Some (resample ~in_freq ~out_freq) ;
+        (Utils.get_some !resampler) buf off len
+      end
+
+type state = {
+  decoder : Ogg_demuxer.t ;
+  fd : Unix.file_descr ;
+  video_convert : Ogg_demuxer.video -> RGB.t ;
+  audio_resample : ?audio_src_rate:float -> Float_pcm.pcm -> Float_pcm.pcm*int ;
+  video_resample : in_freq:int -> out_freq:int ->
+                   RGB.t array -> int -> int -> RGB.t array
+}
+
 let decoder =
-  let video_convert = video_convert () in
   {
    File_decoder.
     log = log;
@@ -81,14 +126,18 @@ let decoder =
         let sync,fd = Ogg.Sync.create_from_file file in
         begin
          try
-           Ogg_demuxer.init sync,
-           fd,
-           Rutils.create_audio ()
+           {
+             decoder = Ogg_demuxer.init sync ;
+             fd = fd ;
+             video_convert = video_convert () ;
+             audio_resample = Rutils.create_audio () ;
+             video_resample = video_resample ()
+           }
          with
            | e -> (try Unix.close fd with _ -> ()); raise e
         end);
-    get_kind = 
-      (fun (decoder,_,_) ->
+    get_kind =
+      (fun {decoder=decoder} ->
         let feed ((buf,_),_) =
           raise (Channels (Array.length buf))
         in
@@ -100,66 +149,63 @@ let decoder =
             | Channels x -> x
             | Not_found  -> 0
         in
-        let feed (buf,_) =                                                                  
-          raise (Channels 1)                                                                
-        in                                                                                  
-        let video =                                                                         
-          try                                                                               
-            Ogg_demuxer.decode_video_rec decoder feed;                                          
-            raise Not_found                                                                 
-          with                                                                              
-            | Channels x -> x                                                               
-            | Not_found  -> 0  
-        in    
+        let feed (buf,_) =
+          raise (Channels 1)
+        in
+        let video =
+          try
+            Ogg_demuxer.decode_video_rec decoder feed;
+            raise Not_found
+          with
+            | Channels x -> x
+            | Not_found  -> 0
+        in
         { Frame.
             audio = Frame.mul_of_int audio ;
             video = Frame.mul_of_int video ;
             midi  = Frame.mul_of_int 0 });
     decode =
-      (fun (decoder,_,converter) buffer ->
-          if Ogg_demuxer.eos decoder then
+      (fun s buffer ->
+          if Ogg_demuxer.eos s.decoder then
             raise Ogg_demuxer.End_of_stream;
           let feed ((buf,sample_freq),_) =
             let audio_src_rate = float sample_freq in
             let content,length =
-              converter ~audio_src_rate buf
+              s.audio_resample ~audio_src_rate buf
             in
             Generator.put_audio buffer content 0 length
           in
-          let got_audio = 
+          let got_audio =
            try
-            Ogg_demuxer.decode_audio decoder feed ;
+            Ogg_demuxer.decode_audio s.decoder feed ;
             true
-           with 
-             | Not_found 
+           with
+             | Not_found
              | Ogg.Not_enough_data -> false
           in
-          let feed (buf,_) =               
-            (* TODO: rate conversion !                                               
-            let src_rate = buf.Ogg_demuxer.fps in *)
-            assert( 
-              int_of_float (buf.Ogg_demuxer.fps +. 0.5)
-               = Frame.video_of_seconds 1.) ;
-            let frame = video_convert buf in
-            (* Length is in samples *)
-            Generator.put_video buffer [|[|frame|]|] 0 1
+          let feed (buf,_) =
+            let in_freq = int_of_float buf.Ogg_demuxer.fps in
+            let out_freq = Lazy.force Frame.video_rate in
+            let rgb = s.video_convert buf in
+            let stream = s.video_resample ~in_freq ~out_freq [|rgb|] 0 1 in
+              Generator.put_video buffer [|stream|] 0 (Array.length stream)
           in
           (* Try to decode video. *)
-          let got_video = 
+          let got_video =
             try
-              Ogg_demuxer.decode_video decoder feed ;
+              Ogg_demuxer.decode_video s.decoder feed ;
               true
             with
-              | Not_found 
+              | Not_found
               | Ogg.Not_enough_data -> false
           in
-          if not got_audio && not got_video then 
-            Ogg_demuxer.feed decoder );
-    position = (fun (_,fd,_) -> Unix.lseek fd 0 Unix.SEEK_CUR);
-    close = (fun (_,fd,_) -> 
-               try                                                                                
-                 Unix.close fd                                                                    
-               with _ -> ()) 
+          if not got_audio && not got_video then
+            Ogg_demuxer.feed s.decoder);
+    position = (fun s -> Unix.lseek s.fd 0 Unix.SEEK_CUR);
+    close = (fun s ->
+               try
+                 Unix.close s.fd
+               with _ -> ())
   }
 
 let () = Decoder.formats#register "OGG" (File_decoder.decode decoder)
