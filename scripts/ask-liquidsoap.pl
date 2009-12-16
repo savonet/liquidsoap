a317c2350a4462fb7ccaf54c058faaa8b2dfd037
dbaelde@aec24677-d710-0410-a355-ac75e2bdf181 <dbaelde@aec24677-d710-0410-a355-ac75e2bdf181>
2009-12-16 09:36:44 +0000

    Die kindly.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407051

--- scripts/ask-liquidsoap.pl
+++ scripts/ask-liquidsoap.pl
@@ -6,6 +6,7 @@ use Net::Telnet ;
 my $telnet = new Net::Telnet ( Timeout=>10, Errmode=>'die', Port=>1234) ;
 $telnet->open('localhost') ;
 
+die "Usage: $0 <command>\n" unless @ARGV ;
 $telnet->print($ARGV[0]) ;
 my ($output,$end) = $telnet->waitfor('/END$/') ;
 print $output;
