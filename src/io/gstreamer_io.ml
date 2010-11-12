aa9a2bcf19f9ac599a6f5712e5287170a9606465
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2010-11-12 09:06:12 +0000

    Better gstreamer inocation, v4l1 devices are now also supported.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407952

--- src/io/gstreamer_io.ml
+++ src/io/gstreamer_io.ml
@@ -29,10 +29,9 @@ module G = Image.Generic
 let () =
   Gstreamer.init ()
 
-class v4l2_input ~kind dev =
+class v4l_input ~kind v4l_version dev =
   let width = Lazy.force Frame.video_width in
   let height = Lazy.force Frame.video_height in
-  let vfps = 10 in
   let poller_img_m = Mutex.create () in
   let poller_m =
     let m = Mutex.create () in
@@ -72,16 +71,30 @@ object (self)
     )
 
   method output_get_ready =
+    (*
     let pipeline =
       Printf.sprintf
-        "v4l2src device=%s ! ffmpegcolorspace ! videoscale ! \
+        "v4l%ssrc device=%s ! ffmpegcolorspace ! videoscale ! \
          appsink max-buffers=2 drop=true name=sink \
            caps=\"video/x-raw-rgb,width=%d,height=%d,\
                   pixel-aspect-ratio=1/1,bpp=(int)32,depth=(int)24,\
                   endianness=(int)4321,red_mask=(int)0xff000000,\
                   green_mask=(int)0x00ff0000,blue_mask=(int)0x0000ff00,\
                   framerate=(fraction)%d/1\""
+        (if v4l_version = 1 then "" else "2")
         dev width height vfps
+    in
+    *)
+    let pipeline =
+      Printf.sprintf
+        "v4l%ssrc device=%s ! ffmpegcolorspace ! videoscale ! \
+         video/x-raw-rgb,width=%d,height=%d, \
+         pixel-aspect-ratio=1/1,bpp=(int)32,depth=(int)24,\
+         endianness=(int)4321,red_mask=(int)0xff000000,\
+         green_mask=(int)0x00ff0000,blue_mask=(int)0x0000ff00 ! \
+         appsink max-buffers=2 drop=true name=sink"
+        (if v4l_version = 1 then "" else "2")
+        dev width height
     in
     let bin = Pipeline.parse_launch pipeline in
     let s = Bin.get_by_name (Bin.of_element bin) "sink" in
@@ -115,6 +128,28 @@ let () =
     Lang.kind_type_of_kind_format ~fresh:1
       (Lang.Constrained
          { Frame. audio = Lang.Any_fixed 0 ;
+           video = Lang.Fixed 1 ;
+           midi = Lang.Fixed 0 })
+  in
+  Lang.add_operator "input.gstreamer.v4l"
+    [
+      "device", Lang.string_t, Some (Lang.string "/dev/video0"),
+      Some "V4L device to use.";
+    ]
+    ~kind:(Lang.Unconstrained k)
+    ~category:Lang.Input
+    ~flags:[Lang.Experimental;Lang.Hidden]
+    ~descr:"Stream from a video4linux input device, such as a webcam. The most recent version of this library is provided by input.gstreamer.v4l2, only use this one if the other does not work."
+    (fun p kind ->
+      let e f v = f (List.assoc v p) in
+      let device = e Lang.to_string "device" in
+      ((new v4l_input ~kind 1 device):>Source.source))
+
+let () =
+  let k =
+    Lang.kind_type_of_kind_format ~fresh:1
+      (Lang.Constrained
+         { Frame. audio = Lang.Any_fixed 0 ;
                   video = Lang.Fixed 1 ;
                   midi = Lang.Fixed 0 })
   in
@@ -126,8 +161,8 @@ let () =
     ~kind:(Lang.Unconstrained k)
     ~category:Lang.Input
     ~flags:[Lang.Experimental;Lang.Hidden]
-    ~descr:"Stream from a gstreamer input device, such as a webcam."
+    ~descr:"Stream from a video4linux 2 input device, such as a webcam."
     (fun p kind ->
        let e f v = f (List.assoc v p) in
        let device = e Lang.to_string "device" in
-         ((new v4l2_input ~kind device):>Source.source))
+         ((new v4l_input ~kind 2 device):>Source.source))
