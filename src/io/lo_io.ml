25023b83013ae554e177bac7de7eddb12179289d
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2010-12-20 13:41:51 +0000

    Add osc.bool.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%408056

--- src/io/lo_io.ml
+++ src/io/lo_io.ml
@@ -12,21 +12,25 @@ let conf_port =
   Conf.int ~p:(conf_oss#plug "port") ~d:7777
     "Port for OSC server."
 
+let osc_bool = ref []
 let osc_float = ref []
 
 let handler path data =
   List.iter
-    (function
-      | `Float f | `Double f ->
-        (
-          log#f 6 "Float %f on path %s" f path;
-          try
+    (fun d ->
+      try
+        match d with
+          | `Float f | `Double f ->
+            log#f 6 "Float %f on path %s" f path;
             let v = List.assoc path !osc_float in
             v := f
-          with
-            | _ -> ()
-        )
-      | _ -> ()
+          | `True | `False ->
+            let b = (d = `True) in
+            let v = List.assoc path !osc_bool in
+            v := b
+          | _ -> ()
+      with
+        | _ -> ()
     ) data
 
 let server = ref None
@@ -50,3 +54,16 @@ let () =
       osc_float := (path,v) :: !osc_float;
       start_server ();
       Lang.val_fun [] ~ret_t:Lang.float_t (fun p _ -> Lang.float !v))
+
+let () =
+  Lang.add_builtin "osc.bool" ~category:"Interaction"
+    ["",Lang.string_t,None,None; "",Lang.bool_t,None,None]
+    (Lang.fun_t [] Lang.bool_t)
+    ~descr:"Read a boolean from an OSC path."
+    (fun p _ ->
+      let path = Lang.to_string (Lang.assoc "" 1 p) in
+      let v = Lang.to_bool (Lang.assoc "" 2 p) in
+      let v = ref v in
+      osc_bool := (path,v) :: !osc_bool;
+      start_server ();
+      Lang.val_fun [] ~ret_t:Lang.bool_t (fun p _ -> Lang.bool !v))
