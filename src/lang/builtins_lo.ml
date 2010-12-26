326d02b9f25ef11b773a43a3fc576f00cd2e027c
dbaelde@aec24677-d710-0410-a355-ac75e2bdf181 <dbaelde@aec24677-d710-0410-a355-ac75e2bdf181>
2010-12-26 20:52:27 +0000

    Move files around for a cleaner layout, also rename in src/lang to separate builtins files from core lang files. Move LO support from io to lang where it belongs, add a license and fix a typo.
    --HG--
    rename : liquidsoap/src/lang/lang_cry.ml => liquidsoap/src/lang/builtins_cry.ml
    rename : liquidsoap/src/lang/lang_lastfm.ml => liquidsoap/src/lang/builtins_lastfm.ml
    rename : liquidsoap/src/io/lo_io.ml => liquidsoap/src/lang/builtins_lo.ml
    rename : liquidsoap/src/lang/lang_magic.ml => liquidsoap/src/lang/builtins_magic.ml
    rename : liquidsoap/src/ogg_demuxer.ml => liquidsoap/src/ogg_formats/ogg_demuxer.ml
    rename : liquidsoap/src/ogg_demuxer.mli => liquidsoap/src/ogg_formats/ogg_demuxer.mli
    rename : liquidsoap/src/ogg_muxer.ml => liquidsoap/src/ogg_formats/ogg_muxer.ml
    rename : liquidsoap/src/ogg_muxer.mli => liquidsoap/src/ogg_formats/ogg_muxer.mli
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%408097

--- src/lang/builtins_lo.ml
+++ src/lang/builtins_lo.ml
@@ -1,3 +1,25 @@
+(*****************************************************************************
+
+  Liquidsoap, a programmable audio stream generator.
+  Copyright 2003-2010 Savonet team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details, fully stated in the COPYING
+  file at the root of the liquidsoap distribution.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ *****************************************************************************)
+
 open Dtools
 
 module S = LO.Server
@@ -7,7 +29,7 @@ let m = Mutex.create ()
 let log = Log.make ["osc"]
 
 let conf_oss =
-  Conf.void ~p:(Configure.conf#plug "oss")
+  Conf.void ~p:(Configure.conf#plug "osc")
     "Interactions through the OSC protocol."
 
 let conf_port =
