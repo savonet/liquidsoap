d4a1a6a00394b75b1d94c25b277eea0e5c4ec966
David Baelde <david.baelde@ens-lyon.org>
2011-07-26 10:14:48 +0200

    Add a bunch of ignore(..) in some test to avoid new warnings
    --HG--
    extra : rebase_source : 3a1e9af84d56c87f4de5c1d0a1367d9dee84db59

--- scripts/tests/type_errors.pl
+++ scripts/tests/type_errors.pl
@@ -41,10 +41,10 @@ incorrect('fun(x)->x(snd(x))');
 
 section("SUBTYPING");
 incorrect('(1:unit)');
-correct('((blank():source(1,1,1)):source(*,*,*))');
+correct('ignore(((blank():source(1,1,1)):source(*,*,*)))');
 incorrect('((blank():source(*,*,*)):source(1,1,1))');
 # Next one requires the inference of a subtype (fixed vs. variable arity)
-correct('audio_to_stereo(add([]))');
+correct('ignore(audio_to_stereo(add([])))');
 
 section("CONSTRAINTS");
 incorrect('"bl"+"a"');
@@ -53,8 +53,8 @@ incorrect('fun(x)->x(x)'); # TODO is it an accident that we get same varname
 incorrect('def f(x) y=snd(x) y(x) end');
 
 section("LET GENERALIZATION");
-correct('def f(x) = y=x ; y end f(3)+snd(f((1,2)))');
-incorrect('def f(x) = y=x ; y end f(3)+"3"');
+correct('def f(x) = y=x ; y end ignore(f(3)+snd(f((1,2))))');
+incorrect('def f(x) = y=x ; y end ignore(f(3)+"3")');
 
 section("ARGUMENTS");
 # The errors should be about the type of the param, not of the function.
@@ -70,7 +70,7 @@ incorrect('fallback(transitions=[fun(x,y=blank())->y])');
 incorrect('fallback(transitions=[fun(x,y)->0])');
 correct('f=fallback(transitions=[fun(x,y,a=2)->x])');
 incorrect('fallback(transitions=[fun(x,y)->y+1])');
-correct('x=fun(f)->f(3) y=x(fun(f,u="1")->u)');
+correct('x=fun(f)->f(3) y=x(fun(f,u="1")->u) ignore(y)');
 
 section("CONTENT KIND");
 incorrect('output.file(%vorbis(stereo),"foo",mean(blank()))');
