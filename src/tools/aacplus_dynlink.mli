3293fa3a05e757f262c160fb4b93c4b5fcd22e39
David Baelde <david.baelde@ens-lyon.org>
2012-06-01 22:03:13 +0200

    Update copyrights.

--- src/tools/aacplus_dynlink.mli
+++ src/tools/aacplus_dynlink.mli
@@ -1,1 +1,11 @@
-../../../ocaml-aacplus/src/aacplus_dynlink.mli
+module type Aacplus_t =
+  sig
+    type t
+    exception Invalid_data
+    exception Invalid_config
+    val create : channels:int -> samplerate:int -> bitrate:int -> unit -> t
+    val frame_size : t -> int
+    val encode : t -> float array array -> string
+  end
+type handler = { mutable encoder : (module Aacplus_t) option; }
+val handler : handler
