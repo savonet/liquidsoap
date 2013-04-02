17f9c2c56b5ccfb2fc075b8e5bc5e4c05c664523
Samuel Mimram <smimram@gmail.com>
2013-04-02 16:03:40 +0200

    Better implementation of major mode.

--- scripts/liquidsoap-mode.el
+++ scripts/liquidsoap-mode.el
@@ -1,20 +1,25 @@
 ;;; liquidsoap-mode.el -- Liquidsoap major mode
-;; Based on http://emacswiki.org/emacs/GenericMode
 
-(require 'generic-x)
-
-(define-generic-mode
-  'liquidsoap-mode ;; name of the mode to create
-  '("#") ;; comments
-  '("fun" "def" "begin" "end" "if" "then" "else" "elsif" "and" "or" "not" "mod" "ref" "->" ":=" ";") ;; keywords
-  '(
-    ;("\\(;\\|:=\\|->\\|\\<not\\>\\)" . 'font-lock-builtin-face)
-    ("\\(true\\|false\\)" 1 'font-lock-constant-face)
-    ("\\(%ifdef .*\\|%endif\\|%include\\)" . 'font-lock-preprocessor-face)
-    ("def[ \t]+\\([^ (]*\\)" 1 'font-lock-function-name-face)
-    ;("^[ \t]*\\([a-z]+\\)[ \t]*=" 1 'font-lock-variable-name-face)
-   )
-  '("\\.liq$") ;; file extension
-  nil ;; other functions to call
-  "A mode for Liquidsoap files." ;; Description
+(defvar liquidsoap-font-lock-keywords
+ '(
+   ("#.*" . 'font-lock-comment-face)
+   ("^\\(%ifdef .*\\|%endif\\|%include\\)" . 'font-lock-preprocessor-face)
+   ("\\<\\(fun\\|def\\|begin\\|end\\|if\\|then\\|else\\|elsif\\)\\>\\|->\\|;" . font-lock-keyword-face)
+   ("\\<\\(and\\|or\\|not\\|mod\\|ref\\)\\>\\|:=" . font-lock-builtin-face)
+   ("\\<\\(true\\|false\\)\\>" . font-lock-constant-face)
+   ("\\<def[ \t]+\\([^ (]*\\)" 1 'font-lock-function-name-face)
   )
+)
+
+(define-derived-mode liquidsoap-mode fundamental-mode
+  "Liquidsoap" "Major mode for Liquidsoap files."
+  (set (make-local-variable 'comment-start) "#")
+  (set (make-local-variable 'comment-start-skip) "#+\\s-*")
+  (set (make-local-variable 'font-lock-defaults) '(liquidsoap-font-lock-keywords))
+  (setq mode-name "Liquidsoap")
+)
+
+(provide 'liquidsoap-mode)
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.liq\\'" . liquidsoap-mode))
