7aac035f31a2a84d2d102d66b2d9aa98fd561046
Romain Beauxis <toots@rastageeks.org>
2013-08-29 08:18:44 -0500

    Also revert this.

--- src/operators/cross.ml
+++ src/operators/cross.ml
@@ -255,7 +255,10 @@ object (self)
     Generator.feed buffer
       ~metadata:(Frame.get_all_metadata buf_frame)
       content start (stop-start) ;
-    if Frame.is_partial buf_frame then
+    if Frame.is_partial buf_frame then begin
+      (* Add a break to properly terminate the generator,
+       * so that remaining time info can be issued. *)
+      Generator.add_break buffer ;
       (* As for Switch's transitions, we avoid stacking compositions
        * because this would lead to huge sources, never simplified.
        * We compose the end of a track with the original source [s] instead of
@@ -293,7 +296,7 @@ object (self)
         source#leave (self:>source) ;
         source <- s ;
         status <- `After inhibit
-    else
+    end else
       if n>0 then self#buffering buffer (n - Frame.position buf_frame)
 
 end
