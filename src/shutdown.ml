bc350b59e98e7d59ec5781d674a75fdcd7a46ee5
Romain Beauxis <toots@rastageeks.org>
2014-05-10 11:42:33 -0500

    Use Utils.get_some

--- src/shutdown.ml
+++ src/shutdown.ml
@@ -3,19 +3,14 @@ let restart = ref false
 
 let dummy = (None : Dtools.Init.t option)
 
-let get x =
-  match !x with
-  | Some t -> t
-  | None -> assert false
-
 let duppy_atom = ref dummy
 (** Duppy shutdown task. *)
-let duppy_scheduler () = get duppy_atom
+let duppy_scheduler () = Utils.get_some !duppy_atom
 
 let final_atom = ref dummy
 (** Final shutown task. This task might restart the application so other tasks
     should be executed before this one. *)
-let final () = get final_atom
+let final () = Utils.get_some !final_atom
 
 (** Add a task to be executed at stop. Using this instead of
     [Dtools.Init.at_stop] ensures that it will be executed before the final
