28b518e2d26d40696421e78d6018968d7a014dcc
Samuel Mimram <smimram@gmail.com>
2015-08-25 15:23:13 +0200

    Use camomile in travis.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 # Hacking the build into Travis-CI "C" environment
 # See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
 
-OPAM_PACKAGES='ocamlfind base-bytes camlp4 pcre syslog magic xmlm ocamlnet yojson'
+OPAM_PACKAGES='ocamlfind base-bytes camlp4 pcre camomile syslog magic xmlm ocamlnet yojson'
 
 export OPAMYES=1
 opam init
