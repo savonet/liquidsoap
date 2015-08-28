e4a4f5ec4025b14def82dc1e6ffb07329a92e34d
Samuel Mimram <smimram@gmail.com>
2015-08-28 10:36:55 +0200

    Travis: nicer output.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,16 +1,3 @@
-# Hacking the build into Travis-CI "C" environment
-# See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
-
-OPAM_PACKAGES='ocamlfind base-bytes camlp4 pcre camomile syslog magic xmlm ocamlnet yojson inotify'
-
-export OPAMYES=1
-opam init
-if [ -n "${OPAM_SWITCH}" ]; then
-    opam switch ${OPAM_SWITCH}
-fi
-eval `opam config env`
-opam install -q -y ${OPAM_PACKAGES}
-
 # Download full Liquidsoap and build libraries
 git clone https://github.com/savonet/liquidsoap-full.git
 cd liquidsoap-full
