e2904df898893e9c1458dd5d5377932de3c2385b
Samuel Mimram <smimram@gmail.com>
2015-08-28 08:50:32 +0200

    Don't clip.

--- src/operators/filter_rc.ml
+++ src/operators/filter_rc.ml
@@ -58,7 +58,7 @@ object
        for c = 0 to Array.length b - 1 do
          let b_c = b.(c) in
          for i = offset to position - 1 do
-           prev.(c) <- Audio.clip (alpha *. b_c.(i) +. alpha' *. prev.(c));
+           prev.(c) <- alpha *. b_c.(i) +. alpha' *. prev.(c);
            b_c.(i) <- wet *. prev.(c) +. wet' *. b_c.(i)
          done
        done
