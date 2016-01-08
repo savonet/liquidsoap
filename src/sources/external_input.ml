60ed400f6aff0f7c8a05451ee7afce8cdc3c1185
Samuel Mimram <smimram@gmail.com>
2016-01-08 10:33:32 +0100

    Preliminary implementation of AVI decoder.

--- src/sources/external_input.ml
+++ src/sources/external_input.ml
@@ -36,8 +36,7 @@ class external_input ~kind ~restart ~bufferize ~channels
     Rutils.create_from_iff ~format:`Wav ~channels ~samplesize:16
                            ~audio_src_rate:in_freq
   in
-  (* We need a temporary log until
-   * the source has an id *)
+  (* We need a temporary log until the source has an id *)
   let log_ref = ref (fun _ -> ()) in
   let log = (fun x -> !log_ref x) in
   let abg = Generator.create ~log ~kind `Audio in
@@ -176,3 +175,155 @@ let () =
           ((new external_input ~kind ~restart ~bufferize ~channels
                                ~restart_on_error ~max
                                ~samplerate command):>Source.source))
+
+
+
+
+(* Video-only input. Ideally this should be merged with previous one and we
+   should add support for audio in AVI files. *)
+
+module Img = Image.RGBA32
+      
+class video ~kind ~restart ~bufferize ~restart_on_error ~max command =
+  let width = Lazy.force Frame.video_width in
+  let height = Lazy.force Frame.video_height in
+  let abg_max_len = Frame.master_of_seconds max in
+  (* We need a temporary log until the source has an id *)
+  let log_ref = ref (fun _ -> ()) in
+  let log = (fun x -> !log_ref x) in
+  let abg = Generator.create ~log ~kind `Video in
+  let priority = Tutils.Non_blocking in
+object (self)
+  inherit Source.source ~name:"input.external.video" kind
+  inherit Generated.source abg ~empty_on_abort:false ~bufferize
+
+  val mutable should_stop = false
+
+  method stype = Source.Fallible
+
+  method wake_up _ =
+    (* Now we can create the log function *)
+    log_ref := self#log#f 3 "%s" ;
+    self#log#f 2 "Starting process.";
+    let create () =
+      let in_e = Unix.open_process_in command in
+      let f = Unix.descr_of_in_channel in_e in
+      let h, _ = Avi.Read.headers_simple f in
+      (
+        match h with
+        | [`Video (w,h,fps)] ->
+           if w <> width then failwith "Wrong width.";
+           if h <> height then failwith "Wrong height.";
+           if fps <> float (Lazy.force Frame.video_rate) then failwith "Wrong rate.";
+        | _ -> failwith "Only AVI with video only are supported for now."
+      );
+      in_e, f
+    in
+    let (_, in_d) as x = create () in
+    let counter = ref 0 in
+    let rec process ((in_e,in_d) as x) l =
+      let get_data () =
+        try
+          match Avi.Read.chunk in_d with
+          | `Frame (`Video, _, data) ->
+             assert (Bytes.length data = width * height * 3);
+             incr counter; self#log#f 2 "FRAME: %d%!" !counter;
+             let data = Img.of_RGB24_string data width in
+             Img.swap_rb data;
+             (* Img.Effect.flip data; *)
+             Generator.put_video abg [|[|data|]|] 0 1
+          | _ -> failwith "Invalid chunk."
+        with
+        | End_of_file -> raise (Finished ("Process exited.", restart))
+      in
+      let do_restart s restart f =
+        self#log#f 2 "%s" s;
+        begin
+          try
+            ignore (Unix.close_process_in in_e);
+          with
+          | _ -> ()
+        end;
+        if restart then
+          begin
+            self#log#f 2 "Restarting process.";
+            let ((_,in_d) as x) = create () in
+            [{ Duppy.Task.
+               priority = priority;
+               events   = [`Read in_d];
+               handler  = process x
+             }]
+          end
+        else
+          begin
+            f ();
+            self#log#f 2 "Task exited.";
+            []
+          end
+      in
+      try
+        let events =
+          if should_stop then raise (Finished ("Source stoped: closing process.",false));
+          let len = Generator.length abg - abg_max_len in
+          if len >= 0 then
+            let delay = Frame.seconds_of_audio len in
+            [`Delay delay]
+          else
+            begin
+              if List.mem (`Read in_d) l then get_data ();
+              [`Read in_d]
+            end
+        in
+        [{ Duppy.Task.
+           priority = priority;
+           events   = events;
+           handler  = process x
+         }]
+      with
+      | Finished (s,b) -> do_restart s b (fun () -> ())
+      | e ->
+         do_restart
+           (Printf.sprintf "Process exited with error: %s" (Printexc.to_string e)) restart_on_error
+           (fun () -> raise e)
+    in
+    let task =
+      { Duppy.Task.
+        priority = priority;
+        events   = [`Read in_d];
+        handler  = process x
+      }
+    in
+    Duppy.Task.add Tutils.scheduler task
+
+  method sleep = should_stop <- true
+end
+
+let () =
+    Lang.add_operator "input.external.video"
+      ~category:Lang.Input
+      ~flags:[Lang.Experimental]
+      ~descr:"Stream data from an external application."
+      [
+        "buffer", Lang.float_t, Some (Lang.float 1.),
+         Some "Duration of the pre-buffered data." ;
+
+        "max", Lang.float_t, Some (Lang.float 10.),
+        Some "Maximum duration of the buffered data.";
+
+        "restart", Lang.bool_t, Some (Lang.bool true),
+        Some "Restart process when exited.";
+
+        "restart_on_error", Lang.bool_t, Some (Lang.bool false),
+        Some "Restart process when exited with error.";
+
+        "", Lang.string_t, None,
+        Some "Command to execute."
+      ]
+      ~kind:Lang.video_only
+      (fun p kind ->
+         let command = Lang.to_string (List.assoc "" p) in
+         let bufferize = Lang.to_float (List.assoc "buffer" p) in
+         let restart = Lang.to_bool (List.assoc "restart" p) in
+         let restart_on_error = Lang.to_bool (List.assoc "restart_on_error" p) in
+         let max = Lang.to_float (List.assoc "max" p) in
+          ((new video ~kind ~restart ~bufferize ~restart_on_error ~max command):>Source.source))
--- src/tools/avi.ml
+++ src/tools/avi.ml
@@ -20,6 +20,8 @@
 
 *****************************************************************************)
 
+open Stdlib
+
 let word n =
   let s = Bytes.create 2 in
   Bytes.set s 0 (char_of_int (n land 0xff));
@@ -174,3 +176,166 @@ let audio_chunk b =
 (* Video in RGB. *)
 let video_chunk b =
   chunk "00db" b
+
+module Read = struct
+  let read n f =
+    let s = Bytes.create n in
+    let k = Unix.read_retry f s 0 n in
+    if k = 0 && n <> 0 then raise End_of_file;
+    assert (k = n);
+    s
+
+  let word f =
+    let s = read 2 f in
+    int_of_char (Bytes.get s 0)
+    + int_of_char (Bytes.get s 1) lsl 8
+
+  let dword f =
+    let s = read 4 f in
+    int_of_char (Bytes.get s 0)
+    + int_of_char (Bytes.get s 1) lsl 8
+    + int_of_char (Bytes.get s 2) lsl 16
+    + int_of_char (Bytes.get s 3) lsl 24
+
+  exception Invalid
+
+  let must b = if not b then raise Invalid
+
+  let rec chunk f =
+    let tag = read 4 f in
+    let len = dword f in
+    (* Printf.printf "Read: %s\n%!" tag; *)
+    len + 8,
+    match tag with
+    | "LIST" ->
+       let subtag = read 4 f in
+       if subtag = "movi" then
+         (* for obvious size reasons we stop parsing here *)
+         `movi (len - 4)
+       else
+         let rem = ref (len - 4) in
+         let ll = ref [] in
+         (* Printf.printf "<<\n%!"; *)
+         while !rem <> 0 do
+           if !rem < 0 then raise Invalid;
+           let l,c = chunk f in
+           rem := !rem - l;
+           ll := c :: !ll
+         done;
+         (* Printf.printf ">>\n%!"; *)
+         `LIST (subtag, List.rev !ll)
+    | "avih" ->
+       let microsec_per_frame = dword f in
+       let max_bytes_per_sec = dword f in
+       let reserved = read 4 f in
+       let flags = dword f in
+       must (flags land 0x0100 <> 0); (* interleaved *)
+       let total_frame = dword f in
+       let init_frame = dword f in
+       let nb_stream = dword f in
+       let sug_buf_size = dword f in
+       let width = dword f in
+       let height = dword f in
+       let scale = dword f in
+       must (scale = 0);
+       let rate = dword f in
+       let start = dword f in
+       let length = dword f in
+       `avih (width, height)
+    | "strh" ->
+       let stream_type = read 4 f in
+       must (len = 56);
+       let fourcc = dword f in
+       must (fourcc = 0);
+       let flags = dword f in
+       must (flags = 0);
+       let priority = word f in
+       let language = word f in
+       let init_frames = dword f in
+       let scale = dword f in
+       let rate = dword f in
+       let fps = float rate /. float scale in
+       let start = dword f in
+       let length = dword f in
+       let buf_size = dword f in
+       let quality = dword f in
+       let sample_size = dword f in
+       let left = word f in
+       let top = word f in
+       let right = word f in
+       let bottom = word f in
+       `strh (stream_type, fps)
+    | "strf" ->
+       let s = read len f in
+       `strf s
+    | "JUNK" ->
+       let s = read len f in
+       `JUNK s
+    | "00dc" ->
+       let s = read len f in
+       (* TODO: other channels and audio frames too (first argument is channel
+          number) *)
+       `Frame (`Video, 0, s)
+    | _ ->
+       let s = read len f in
+       `Other s
+
+  let chunk f = snd (chunk f)
+
+  let headers f =
+    must (read 4 f = "RIFF");
+    let filesize = dword f in
+    must (read 4 f = "AVI ");
+    let h = ref [] in
+    try
+      while true do
+        let c = chunk f in
+        h := c :: !h;
+        match c with
+        | `movi _ -> raise Exit
+        | _ -> ()
+      done;
+      assert false
+    with
+    | Exit ->
+       List.rev !h
+
+  let headers_simple f =
+    let headers = headers f in
+    if List.length headers < 2 then raise Invalid;
+    let h =
+      match List.hd headers with
+      | `LIST ("hdrl", h) -> h
+      | _ -> raise Invalid
+    in
+    let width, height =
+      match List.hd h with
+      | `avih (width, height) -> width, height
+      | _  -> raise Invalid;
+    in
+    let h = List.tl h in
+    let streams = ref [] in
+    List.iter
+      (function
+      | `LIST ("strl", l) ->
+         if List.length l < 2 then raise Invalid;
+        let strf =
+          match List.hd (List.tl l) with
+          | `strf s -> s
+          | _ -> raise Invalid
+        in
+        begin
+          match List.hd l with
+          | `strh (stream_type, fps) ->
+             if stream_type = "vids" then streams := `Video (width, height, fps) :: !streams
+             else if stream_type = "auds" then streams := `Audio :: !streams
+             else raise Invalid
+          | _ -> ()
+        end
+      | _ -> ()
+      ) h;
+    let streams = List.rev !streams in
+    match List.last headers with
+    | `movi len -> streams, len
+    | _ -> raise Invalid
+end
