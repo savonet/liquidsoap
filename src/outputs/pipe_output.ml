e3761ef2675a596c3b5dca2438d55c1785363bae
Romain Beauxis <toots@rastageeks.org>
2017-03-18 14:57:08 -0500

    Revert "Also copy stored metadata here."
    This reverts commit 889d123fc4bf54d197ddf2593e6275524a1842cb.

--- src/outputs/pipe_output.ml
+++ src/outputs/pipe_output.ml
@@ -160,7 +160,7 @@ object (self)
   method insert_metadata m =
     if reload_on_metadata then
      begin
-      current_metadata <- Some (Hashtb.copy m) ; 
+      current_metadata <- Some m ; 
       need_reset <- true 
      end 
     else
