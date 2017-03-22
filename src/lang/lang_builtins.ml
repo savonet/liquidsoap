ff81734138d09b96ec95a50c5c2544def209b3b2
Romain Beauxis <toots@rastageeks.org>
2017-03-21 19:37:23 -0500

    Cleanup some categories and flags.

--- src/lang/lang_builtins.ml
+++ src/lang/lang_builtins.ml
@@ -1388,8 +1388,10 @@ let () =
       Shutdown.restart := true ;
       Tutils.shutdown () ;
       Lang.unit);
-  add_builtin "exit" ~cat:Sys ~flags:[Lang.Hidden]
-    ~descr:"Immediately stop the application. This should only be used in extreme cases or to specify an exit value. The recommended way of stopping Liquidsoap is to use shutdown."
+  add_builtin "exit" ~cat:Sys
+    ~descr:"Immediately stop the application. This should only be used in extreme cases \
+            or to specify an exit value. The recommended way of stopping Liquidsoap is \
+            to use shutdown."
     ["", Lang.int_t, None, Some "Exit value."] Lang.unit_t
     (fun p ->
       let n = Lang.to_int (List.assoc "" p) in
