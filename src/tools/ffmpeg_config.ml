26d218ce6d9e67d47f6b0940c89f775e3515e981
Romain Beauxis <toots@rastageeks.org>
2018-08-11 16:01:44 -0500

    Add ffmpeg logging settings.

---
+++ src/tools/ffmpeg_config.ml
@@ -0,0 +1,37 @@
+let log = Dtools.Log.make ["ffmpeg"]
+
+let conf_ffmpeg =
+  Dtools.Conf.void ~p:(Configure.conf#plug "ffmpeg") "FFMPEG configuration"
+
+let conf_log =
+  Dtools.Conf.void ~p:(conf_ffmpeg#plug "log") "Log configuration"
+
+let conf_verbosity =
+  Dtools.Conf.string ~p:(conf_log#plug "verbosity") "Verbosity" ~d:"quiet"
+    ~comments:["Set FFMPEG log level, one of: \"quiet\", \"panic\", \"fatal\"";
+               "\"error\", \"warning\", \"info\", \"verbose\" or \"debug\""]
+
+let conf_level =
+  Dtools.Conf.int ~p:(conf_log#plug "level") "Level" ~d:4
+
+
+let () =
+  ignore(Dtools.Init.at_start (fun () ->
+    let verbosity =
+      match conf_verbosity#get with
+        | "quiet" -> `Quiet
+        | "panic" -> `Panic
+        | "fatal" -> `Fatal
+        | "error" -> `Error
+        | "warning" -> `Warning
+        | "info" -> `Info
+        | "verbose" -> `Verbose
+        | "debug" -> `Debug
+        | _ ->
+            log#f 2 "Invalid value for \"ffmpeg.log.verbosity\"!";
+            `Quiet
+    in
+    let level = conf_level#get in
+    FFmpeg.Avutil.Log.set_level verbosity;
+    FFmpeg.Avutil.Log.set_callback (fun s ->
+      log#f level "%s" (String.trim s))))
