80332f0e83fabf90fbf1c5795469ab63add2f880
Romain Beauxis <toots@rastageeks.org>
2019-04-13 19:21:24 -0500

    Better this way.

--- src/operators/cross.ml
+++ src/operators/cross.ml
@@ -214,7 +214,7 @@ object (self)
           (* We started buffering but the track didn't end.
            * Play the beginning of the buffer while filling it more. *)
           let len = Generator.length gen_before in
-          if len < cross_length then
+          if len <= cross_length then
             self#buffering (cross_length-len) ;
           Generator.fill gen_before frame
       | `Limit ->
