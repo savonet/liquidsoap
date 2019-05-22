47254b0068f6263a7fcde9e01ae472f2ea8c817c
Romain Beauxis <toots@rastageeks.org>
2019-05-22 16:20:29 +0200

    Number errors and warnings.

--- src/lang/lang_errors.ml
+++ src/lang/lang_errors.ml
@@ -35,11 +35,11 @@ let warning  = Console.colorize [`yellow;`bold] "Warning"
 let position pos =
   Console.colorize [`white;`bold] (String.capitalize_ascii pos)
 
-let error_header pos =
-  Format.printf "@[<7>%s:\n%s: " (position pos) error
+let error_header idx pos =
+  Format.printf "@[<7>%s:\n%s %i: " (position pos) error idx
 
-let warning_header pos =
-  Format.printf "@[<9>%s:\n%s: " (position pos) warning
+let warning_header idx pos =
+  Format.printf "@[<9>%s:\n%s %i: " (position pos) warning idx
 
 (** Exception raised by report_error after an error has been displayed.
   * Unknown errors are re-raised, so that their content is not totally lost. *)
@@ -48,7 +48,7 @@ exception Error
 let strict = ref false
 
 let report lexbuf f =
-  let print_error error =
+  let print_error idx error =
     flush_all () ;
     let pos =
       let start = lexbuf.Sedlexing_compat.lex_curr_p in
@@ -60,25 +60,52 @@ let report lexbuf f =
         (1+start.Lexing.pos_cnum-start.Lexing.pos_bol)
         (if buf = "" then "" else (Printf.sprintf " before %S" buf)) ;
     in
-    error_header pos;
+    error_header idx pos;
     Format.printf "%s\n@]@." error
   in
     try f () with
-      | Failure s when s = "lexing: empty token" -> print_error "Empty token" ; raise Error
-      | Parsing.Parse_error -> print_error "Parse error" ; raise Error
+    (* Warnings *)
+      | Term.Ignored tm when Term.is_fun (T.deref tm.Term.t) ->
+          flush_all () ;
+          warning_header 1 (T.print_pos (Utils.get_some tm.Term.t.T.pos));
+          Format.printf "This function application is partial,@ \
+                         maybe some arguments are missing.@]@.";
+          if !strict then raise Error
+      | Term.Ignored tm when Term.is_source (T.deref tm.Term.t) ->
+          flush_all () ;
+          warning_header 2 (T.print_pos (Utils.get_some tm.Term.t.T.pos));
+          Format.printf "This source is unused, maybe it needs to@ \
+                         be connected to an output.@]@.";
+          if !strict then raise Error
+      | Term.Ignored tm ->
+          flush_all () ;
+          warning_header 3 (T.print_pos (Utils.get_some tm.Term.t.T.pos));
+          Format.printf "This expression should have type unit.@]@.";
+          if !strict then raise Error
+      | Term.Unused_variable (s,pos) ->
+          flush_all () ;
+          warning_header 4 (T.print_single_pos pos);
+          Format.printf "Unused variable %s@]@." s;
+          if !strict then raise Error
+
+    (* Errors *)
+      | Failure s when s = "lexing: empty token" ->
+          print_error 1 "Empty token" ; raise Error
+      | Parsing.Parse_error ->
+          print_error 2 "Parse error" ; raise Error
       | Lang_values.Parse_error (pos,s) ->
         let pos = T.print_pos pos in
-        error_header pos;
+        error_header 3 pos;
         Format.printf "@%s@]@." s;
         raise Error
       | Term.Unbound (pos,s) ->
           let pos = T.print_pos (Utils.get_some pos) in
-          error_header pos;
+          error_header 4 pos;
           Format.printf "Undefined variable %s@]@." s;
           raise Error
       | T.Type_Error explain ->
           flush_all () ;
-          Lang_types.print_type_error error_header explain ;
+          Lang_types.print_type_error (error_header 5) explain ;
           raise Error
       | Term.No_label (f,lbl,first,x) ->
           let pos_f =
@@ -86,57 +113,35 @@ let report lexbuf f =
           in
           let pos_x = T.print_pos (Utils.get_some x.Term.t.T.pos) in
           flush_all () ;
-          error_header pos_x ;  
+          error_header 6 pos_x ;  
           Format.printf "Cannot apply that parameter because the function %s@ has %s@ %s!@]@."
               pos_f
               (if first then "no" else "no more")
               (if lbl="" then "unlabeled argument" else
                  Format.sprintf "argument labeled %S" lbl) ;
           raise Error
-      | Term.Ignored tm when Term.is_fun (T.deref tm.Term.t) ->
-          flush_all () ;
-          warning_header (T.print_pos (Utils.get_some tm.Term.t.T.pos));
-          Format.printf "This function application is partial,@ \
-                         maybe some arguments are missing.@]@.";
-          if !strict then raise Error
-      | Term.Ignored tm when Term.is_source (T.deref tm.Term.t) ->
-          flush_all () ;
-          warning_header (T.print_pos (Utils.get_some tm.Term.t.T.pos));
-          Format.printf "This source is unused, maybe it needs to@ \
-                         be connected to an output.@]@.";
-          if !strict then raise Error
-      | Term.Ignored tm ->
-          flush_all () ;
-          warning_header (T.print_pos (Utils.get_some tm.Term.t.T.pos));
-          Format.printf "This expression should have type unit.@]@.";
-          if !strict then raise Error
-      | Term.Unused_variable (s,pos) ->
-          flush_all () ;
-          warning_header (T.print_single_pos pos);
-          Format.printf "Unused variable %s@]@." s;
-          if !strict then raise Error
       | Invalid_value (v,msg) ->
-          error_header (T.print_pos (Utils.get_some v.Term.V.t.T.pos));
+          error_header 7 (T.print_pos (Utils.get_some v.Term.V.t.T.pos));
           Format.printf "Invalid value:@ %s@]@." msg;
           raise Error
       | Lang_encoders.Error (v,s) ->
-          error_header (T.print_pos (Utils.get_some v.Lang_values.t.T.pos));
+          error_header 8 (T.print_pos (Utils.get_some v.Lang_values.t.T.pos));
           Format.printf "%s@]@." (String.capitalize_ascii s);
           raise Error
       | Failure s ->
-          print_error (Printf.sprintf "Failure: %s" s);
+          print_error 9 (Printf.sprintf "Failure: %s" s);
           raise Error
       | Clock_conflict (pos,a,b) ->
           (* TODO better printing of clock errors: we don't have position
            *   information, use the source's ID *)
-          error_header (T.print_pos (Utils.get_some pos));
+          error_header 10 (T.print_pos (Utils.get_some pos));
           Format.printf "A source cannot belong to two clocks (%s,@ %s).@]@."
             a b ;
           raise Error
       | Clock_loop (pos,a,b) ->
-          error_header (T.print_pos (Utils.get_some pos));
+          error_header 11 (T.print_pos (Utils.get_some pos));
           Format.printf "Cannot unify two nested clocks (%s,@ %s).@]@."
             a b ;
           raise Error
       | End_of_file -> raise End_of_file
-      | e -> print_error "Unknown error" ; raise e
+      | e -> print_error (-1) "Unknown error" ; raise e
