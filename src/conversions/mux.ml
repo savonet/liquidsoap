55d8dc6cda90ab8a36944411a5484fcb18d5780a
Samuel Mimram <smimram@gmail.com>
2019-05-27 14:40:25 +0200

    Keep master tracks' boundaries when muxing.
    Fixes #795.

--- src/conversions/mux.ml
+++ src/conversions/mux.ml
@@ -57,61 +57,79 @@ object (self)
       if master = -1 && aux = -1 then -1 else min master aux
 
   method private get_frame frame =
-    match mode with
+    let pos = Frame.position frame in
+    (* Immediately get the final destination frame. It will be used to obtain
+       data from aux and master sources directly at the right place, so that
+       muxing only consists in putting channels together, without the need for
+       blitting. It remains possible that one of our sources ends up writing in
+       another content layer than the expected one. So we call blit just in
+       case; it doesn't cost anything when the array are already identical. *)
+    let dest = Frame.content_of_type frame pos dest_type in
+    let get until adapt_layer s =
+      let breaks = Frame.breaks frame in
+      let inicon = adapt_layer dest in
+      let inicon = Frame.content_of_type ~force:inicon frame pos (Frame.type_of_content inicon) in
+      let once = ref true in
+      let more () =
+        match until with
+        | `Once ->
+           let b = !once in
+           once := false;
+           b
+        | `Full -> true
+        | `Pos p -> Frame.position frame < p
+      in
+      while s#is_ready && Frame.is_partial frame && more () do
+        s#get frame
+      done;
+      let _, c = Frame.content frame pos in
+      let end_pos = Frame.position frame in
+      if inicon != c then self#log#f 5 "Copy-avoiding optimization isn't working!";
+      Frame.set_breaks frame breaks;
+      c, end_pos
+    in
+    (* Hiding contents to avoid the following. If the master layer is present in
+       the frame, it may be used by one of the sources. For example, master is
+       stereo, we call the aux source with a mono layer (half of the stereo one)
+       but that source is a mean so its sub-source will write on the underlying
+       stereo layer (before computing the mean in the mono layer) overwriting
+       the data written before by the master source.  For similar reasons we
+       hide what the master has written before the aux source gets the frame. *)
+    let restore = Frame.hide_contents frame in
+    let new_content, end_pos =
+      match mode with
       | Symmetric ->
-          (* We get as much info as possible from one source, save the content,
-           * repeat the operation for the other, then merge the contents.
-           * If one source produces more than the other, extra data is dropped.
-           * This could be avoided if we could get sample by sample, or know in
-           * advance how many samples each source can produce. *)
-          let pos = Frame.position frame in
-          (* Immediately get the final destination frame.
-           * It will be used to obtain data from aux and master sources
-           * directly at the right place, so that muxing only consists
-           * in putting channels together, without the need for blitting.
-           * It remains possible that one of our sources ends up writing
-           * in another content layer than the expected one. So we call
-           * blit just in case; it doesn't cost anything when the array
-           * are already identical. *)
-          let dest = Frame.content_of_type frame pos dest_type in
-          let get adapt_layer s =
-            let breaks = Frame.breaks frame in
-            let inicon = adapt_layer dest in
-            let inicon =
-              Frame.content_of_type ~force:inicon
-                frame pos (Frame.type_of_content inicon)
-            in
-              while s#is_ready && Frame.is_partial frame do
-                s#get frame
-              done ;
-              let _,c = Frame.content frame pos in
-              let end_pos = Frame.position frame in
-                if inicon != c then
-                  self#log#f 4 "Copy-avoiding optimization isn't working!" ;
-                Frame.set_breaks frame breaks ;
-                c, end_pos
-          in
-          (* Hiding contents to avoid the following.
-           * If the master layer is present in the frame, it may be used by one
-           * of the sources. For example, master is stereo, we call the aux source
-           * with a mono layer (half of the stereo one) but that source is a
-           * mean so its sub-source will write on the underlying stereo layer
-           * (before computing the mean in the mono layer) overwriting the
-           * data written before by the master source.
-           * For similar reasons we hide what the master has written
-           * before the aux source gets the frame. *)
-          let restore = Frame.hide_contents frame in
-          let master,end_master = get master_layer master in
-          let (_:unit->unit) = Frame.hide_contents frame in
-          let aux,end_aux = get aux_layer aux in
-          let end_pos = min end_master end_aux in
-          let new_content = mux_content master aux in
-            restore () ;
-            Frame.blit_content new_content pos dest pos (end_pos-pos) ;
-            Frame.add_break frame end_pos
-      | _ ->
-          failwith "Not yet implemented" (* TODO *)
-
+         (* We get as much info as possible from one source, save the content,
+            repeat the operation for the other, then merge the contents. If one
+            source produces more than the other, extra data is dropped. This
+            could be avoided if we could get sample by sample, or know in
+            advance how many samples each source can produce. *)
+         let master,end_master = get `Full master_layer master in
+         let (_:unit->unit) = Frame.hide_contents frame in
+         let aux,end_aux = get `Full aux_layer aux in
+         let end_pos = min end_master end_aux in
+         let new_content = mux_content master aux in
+         new_content, end_pos
+      | Master ->
+         (* The aux tries to produce as much as the master. Drop if too much. *)
+         let master,end_master = get `Once master_layer master in
+         let (_:unit->unit) = Frame.hide_contents frame in
+         let aux,end_aux = get (`Pos end_master) aux_layer aux in
+         let end_pos = min end_master end_aux in
+         let new_content = mux_content master aux in
+         new_content, end_pos
+      | Auxiliary ->
+         (* The opposite *)
+         let aux,end_aux = get `Once aux_layer aux in
+         let (_:unit->unit) = Frame.hide_contents frame in
+         let master,end_master = get (`Pos end_aux) master_layer master in
+         let end_pos = min end_master end_aux in
+         let new_content = mux_content master aux in
+         new_content, end_pos
+    in
+    restore ();
+    Frame.blit_content new_content pos dest pos (end_pos-pos);
+    Frame.add_break frame end_pos
 end
 
 let () =
@@ -137,9 +155,8 @@ let () =
          let mux_content master aux =
            { master with Frame.video = aux.Frame.video }
          in
-         let mode = Symmetric in
-           new mux ~kind ~mode
-             ~master ~aux ~master_layer ~aux_layer mux_content)
+         let mode = Master in
+         new mux ~kind ~mode ~master ~aux ~master_layer ~aux_layer mux_content)
 
 let () =
   let out_t = Lang.kind_type_of_kind_format ~fresh:1 Lang.any_fixed in
@@ -164,19 +181,15 @@ let () =
          let mux_content master aux =
            { master with Frame.audio = aux.Frame.audio }
          in
-         let mode = Symmetric in
-           new mux ~kind ~mode
-             ~master ~aux ~master_layer ~aux_layer mux_content)
+         let mode = Master in
+         new mux ~kind ~mode ~master ~aux ~master_layer ~aux_layer mux_content)
 
 let add_audio_mux label n =
   let master_t = Lang.kind_type_of_kind_format ~fresh:1 Lang.any_fixed in
-  let aux_t =
-    Lang.frame_kind_t ~audio:(Lang.type_of_int n)
-                      ~video:Lang.zero_t ~midi:Lang.zero_t
-  in
+  let aux_t = Lang.frame_kind_t ~audio:(Lang.type_of_int n) ~video:Lang.zero_t ~midi:Lang.zero_t in
   let out_t =
     let { Frame. audio=audio ; video=video ; midi=midi } = Lang.of_frame_kind_t master_t in
-      Lang.frame_kind_t ~audio:(Lang.add_t n audio) ~video ~midi
+    Lang.frame_kind_t ~audio:(Lang.add_t n audio) ~video ~midi
   in
     Lang.add_operator ("mux_"^label)
       ~category:Lang.Conversions
@@ -209,10 +222,9 @@ let add_audio_mux label n =
            in
              { master with Frame.audio = audio }
          in
-         let mode = Symmetric in
-           new mux ~kind ~mode
-             ~master ~aux ~master_layer ~aux_layer mux_content)
+         let mode = Master in
+         new mux ~kind ~mode ~master ~aux ~master_layer ~aux_layer mux_content)
 
 let () =
-  add_audio_mux "mono" 1 ;
+  add_audio_mux "mono" 1;
   add_audio_mux "stereo" 2
