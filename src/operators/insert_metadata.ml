48954580c763684a18da098e05a68f17e4ad2f3c
Samuel Mimram <smimram@gmail.com>
2019-05-30 10:20:43 +0200

    Add insert_track.

--- src/operators/insert_metadata.ml
+++ src/operators/insert_metadata.ml
@@ -23,8 +23,8 @@
 open Source
 exception Error
 
-class insert_metadata ~kind source =
-object
+class insert_metadata ~kind ~newtrack source =
+object (self)
   inherit operator ~name:"insert_metadata" kind [source]
 
   method stype = source#stype
@@ -41,55 +41,90 @@ object
     metadata <- Some m ;
     Mutex.unlock lock_m
 
+  method has_metadata =
+    Tutils.mutexify lock_m (fun () -> metadata <> None) ()
+
   method private get_frame buf =
     let p = Frame.position buf in
-      source#get buf ;
-      Mutex.lock lock_m ;
-      Tutils.finalize ~k:(fun () -> Mutex.unlock lock_m)
+    if newtrack && self#has_metadata then
+      Frame.add_break buf p
+    else
+      source#get buf;
+    Tutils.mutexify lock_m
       (fun () ->
         match metadata with
-          | Some m ->
-              Frame.set_metadata buf p m ;
-              metadata <- None
-          | None -> ())
+        | Some m ->
+           Frame.set_metadata buf p m;
+           metadata <- None
+        | None -> ()) ()
 end
 
 let () =
   let kind = Lang.univ_t 1 in
   let return_t =
     Lang.product_t
-     (Lang.fun_t [false,"",Lang.metadata_t] Lang.unit_t)
-     (Lang.source_t kind)
+      (Lang.fun_t [false,"",Lang.metadata_t] Lang.unit_t)
+      (Lang.source_t kind)
   in
   Lang.add_builtin "insert_metadata"
     ~category:(Lang.string_of_category Lang.TrackProcessing)
-    ~descr:"Dynamically insert metadata in a stream. \
-            Returns a pair `(f,s)` where s is a new source and \
-            `f` is a function of type `(metadata)->unit`, used to \
-            insert metadata in `s`."
+    ~descr:"Dynamically insert metadata in a stream. Returns a pair `(f,s)` \
+            where s is a new source and `f` is a function of type \
+            `(metadata)->unit`, used to insert metadata in `s`."
     [ "id",Lang.string_t,Some (Lang.string ""),
       Some "Force the value of the source ID.";
       "",Lang.source_t kind,None,None ] return_t
     (fun p t ->
-       let s = Lang.to_source (List.assoc "" p) in
-       let id = Lang.to_string (List.assoc "id" p) in
-       let (_,t) = Lang.of_product_t t in
-       let kind =
-         Lang.frame_kind_of_kind_type (Lang.of_source_t t)
-       in
-       let s = new insert_metadata ~kind s in
-       if id <> "" then s#set_id id ;
-       let f =
-         Lang.val_fun ["","",Lang.metadata_t,None] ~ret_t:Lang.unit_t
-                      (fun p _ ->
-                         s#insert_metadata
-                           (Lang.to_metadata (List.assoc "" p));
-                         Lang.unit)
-       in
-       Lang.product f (Lang.source (s :> Source.source)))
+      let s = Lang.to_source (List.assoc "" p) in
+      let id = Lang.to_string (List.assoc "id" p) in
+      let (_,t) = Lang.of_product_t t in
+      let kind =
+        Lang.frame_kind_of_kind_type (Lang.of_source_t t)
+      in
+      let s = new insert_metadata ~kind ~newtrack:false s in
+      if id <> "" then s#set_id id ;
+      let f =
+        Lang.val_fun ["","",Lang.metadata_t,None] ~ret_t:Lang.unit_t
+          (fun p _ ->
+            s#insert_metadata (Lang.to_metadata (List.assoc "" p));
+            Lang.unit)
+      in
+      Lang.product f (Lang.source (s :> Source.source)))
 
-(** Insert metadata at the beginning if none is set.
-  * Currently used by the switch classes. *)
+let () =
+  let kind = Lang.univ_t 1 in
+  let return_t =
+    Lang.product_t
+      (Lang.fun_t [false,"",Lang.metadata_t] Lang.unit_t)
+      (Lang.source_t kind)
+  in
+  Lang.add_builtin "insert_track"
+    ~category:(Lang.string_of_category Lang.TrackProcessing)
+    ~descr:"Dynamically insert tracks in a stream. Returns a pair `(f,s)` where \
+            s is a new source and `f` is a function of type `(metadata)->unit`, \
+            used to insert track with given metadata in `s`."
+    [ "id",Lang.string_t,Some (Lang.string ""),
+      Some "Force the value of the source ID.";
+      "",Lang.source_t kind,None,None ] return_t
+    (fun p t ->
+      let s = Lang.to_source (List.assoc "" p) in
+      let id = Lang.to_string (List.assoc "id" p) in
+      let (_,t) = Lang.of_product_t t in
+      let kind =
+        Lang.frame_kind_of_kind_type (Lang.of_source_t t)
+      in
+      let s = new insert_metadata ~kind ~newtrack:true s in
+      if id <> "" then s#set_id id ;
+      let f =
+        Lang.val_fun ["","",Lang.metadata_t,None] ~ret_t:Lang.unit_t
+          (fun p _ ->
+            s#insert_metadata (Lang.to_metadata (List.assoc "" p));
+            Lang.unit)
+      in
+      Lang.product f (Lang.source (s :> Source.source)))
+
+(** Insert metadata at the beginning if none is set. Currently used by the
+   switch classes. *)
 class replay ~kind meta src =
 object
   inherit operator ~name:"replay_metadata" kind [src]
@@ -103,8 +138,9 @@ object
 
   method private get_frame ab =
     let start = Frame.position ab in
-      src#get ab ;
-      if first then begin
+    src#get ab ;
+    if first then
+      begin
         if Frame.get_metadata ab start = None then
           Frame.set_metadata ab start meta ;
         first <- false
