e0343c1c6004f4d522a1847004de9b561c095810
Romain Beauxis <toots@rastageeks.org>
2019-07-08 12:55:24 -0500

    Getting there.

--- .travis-deb.sh
+++ .travis-deb.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 COMMIT=$1
+BRANCH=$1
 
 mkdir debian
-id=$(docker create ${IMAGE})
+id=$(docker create liquidsoap-build)
 docker cp $id:/liquidsoap.deb debian/liquidsoap-${COMMIT}.deb
 docker rm -v $id
+
+if test -n "${BRANCH}"; then
+  cp debian/liquidsoap-${COMMIT}.deb debian/liquidsoap-${BRANCH}.deb
+fi
