1832f9bfbfc5e34a0d2918438909cff3ba691c48
Romain Beauxis <toots@rastageeks.org>
2019-07-08 12:50:02 -0500

    This is more correct.

--- .travis-deb.sh
+++ .travis-deb.sh
@@ -4,5 +4,5 @@ COMMIT=$1
 
 mkdir debian
 id=$(docker create ${IMAGE})
-docker cp $id:/tmp/liquidsoap-full/liquidsoap/debian/liquidsoap.deb debian/liquidsoap-${COMMIT}.deb
+docker cp $id:/liquidsoap.deb debian/liquidsoap-${COMMIT}.deb
 docker rm -v $id
