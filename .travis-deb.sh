ed54eb986407d363bea132a1dc0bb67cfa277183
Romain Beauxis <toots@rastageeks.org>
2019-07-08 13:25:38 -0500

    Fix vars.

--- .travis-deb.sh
+++ .travis-deb.sh
@@ -13,9 +13,9 @@ fi
 
 mkdir debian
 id=$(docker create liquidsoap-build)
-docker cp $id:/tmp/liquidsoap.deb debian/liquidsoap-${COMMIT}.deb
+docker cp $id:/tmp/liquidsoap.deb debian/liquidsoap-${TRAVIS_COMMIT}.deb
 docker rm -v $id
 
 if test -n "${BRANCH}"; then
-  cp debian/liquidsoap-${COMMIT}.deb debian/liquidsoap-${BRANCH}.deb
+  cp debian/liquidsoap-${TRAVIS_COMMIT}.deb debian/liquidsoap-${BRANCH}.deb
 fi
