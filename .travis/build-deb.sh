917b365d6962d29bf4c34763f410ca458a8b3689
Romain Beauxis <toots@rastageeks.org>
2019-07-09 22:56:03 -0500

    Try this.

--- .travis/build-deb.sh
+++ .travis/build-deb.sh
@@ -2,16 +2,29 @@
 
 set -e
 
-cd /tmp/liquidsoap-full/liquidsoap
-
-COMMIT=$1
+TRAVIS_COMMIT=$1
+TRAVIS_BRANCH=$2
+TRAVIS_PULL_REQUEST_BRANCH=$3
+TRAVIS_PULL_REQUEST=$4
 
 DEBFULLNAME="The Savonet Team"
 DEBEMAIL="savonet-users@lists.sourceforge.net"
 
-dch --create --distribution unstable --package "liquidsoap" --newversion "0+${COMMIT}-1" "Build ${COMMIT}"
+if test "${TRAVIS_PULL_REQUEST}" = "false"; then
+  BRANCH="${TRAVIS_BRANCH}"
+else
+  BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
+fi
 
 eval $(opam config env)
+
+cd /tmp/liquidsoap-full/liquidsoap
+
+dch --create --distribution unstable --package "liquidsoap" --newversion "0+${COMMIT}-1" "Build ${COMMIT}"
+
+fakeroot debian/rules binary
+
+dch --create --distribution unstable --package "liquidsoap" --newversion "0+${BRANCH}-1" "Build ${BRANCH}"
 
 fakeroot debian/rules binary
 
--- .travis/get-deb.sh
+++ .travis/get-deb.sh
@@ -2,19 +2,7 @@
 
 set -e
 
-TRAVIS_COMMIT=$1
-TRAVIS_BRANCH=$2
-TRAVIS_PULL_REQUEST_BRANCH=$3
-TRAVIS_PULL_REQUEST=$4
-
-if test "${TRAVIS_PULL_REQUEST}" = "false"; then
-  BRANCH="${TRAVIS_BRANCH}"
-else
-  BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
-fi
-
 rm -rf debian
 id=$(docker create liquidsoap-build)
 docker cp $id:/tmp/debian debian/
-cp -rf debian/pkgs/liquidsoap_0+${TRAVIS_COMMIT}-1_amd64.deb debian/pkgs/liquidsoap_0+${BRANCH}-1_amd64.deb
 docker rm -v $id
