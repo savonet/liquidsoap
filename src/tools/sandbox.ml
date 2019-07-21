85f9f7fbf14e511065f569fb4fc2c3d010ff3629
Romain Beauxis <toots@rastageeks.org>
2019-07-21 13:34:12 -0700

    Remove those dirs from bwrap default rw. /var/run has proven buggy, there shouldn't be default need for daemon logging in /var/log through run_process. Can be added if/when needed.

--- src/tools/sandbox.ml
+++ src/tools/sandbox.ml
@@ -59,11 +59,6 @@ let conf_binary =
 let conf_rw =
   let rw = [tmpdir] in
   let rw =
-    if Sys.unix then
-      ("/var/log"::"/var/run"::rw)
-    else rw
-  in
-  let rw =
     match Sys.getenv_opt "HOME" with
       | Some h -> h::rw
       | None -> rw
