97d1b79e82cdbe29fa7a03050d0749daf706dfb2
Romain Beauxis <toots@rastageeks.org>
2019-07-27 15:43:25 -0700

    Some progress!

--- src/io/srt_io.ml
+++ src/io/srt_io.ml
@@ -25,29 +25,17 @@
 module G = Generator
 module Generator = Generator.From_audio_video_plus
 module Generated = Generated.Make(Generator)
-  
-class input ~kind ~bind_address ~bufferize ~max ~payload_size
-            ~on_connect ~on_disconnect format =
-  let max_ticks = Frame.master_of_seconds (Stdlib.max max bufferize) in
-  let log_ref = ref (fun _ -> ()) in
-  let log = (fun x -> !log_ref x) in
-  let abg =
-    Generator.create ~log ~kind ~overfull:(`Drop_old max_ticks) `Undefined
-  in
-object (self)
-  
-  inherit  Source.source ~name:"input.srt" kind as super
-  inherit
-    Generated.source abg ~empty_on_abort:false ~bufferize
 
+class virtual base ~payload_size ~messageapi =
+object(self)
   val mutable socket = None
-  val mutable kill_feeding = None
-  val mutable wait_feeding = None
 
   method private string_of_address = function
     | Unix.ADDR_UNIX _ -> assert false
     | Unix.ADDR_INET (addr,port) ->
         Printf.sprintf "%s:%d" (Unix.string_of_inet_addr addr) port
+
+  method virtual private prepare_socket : Srt.socket -> unit
 
   method private get_socket =
     match socket with
@@ -58,13 +46,36 @@ object (self)
           in
           Srt.setsockflag s Srt.payloadsize payload_size;
           Srt.setsockflag s Srt.transtype `Live;
-          Srt.setsockflag s Srt.messageapi true;
-          Srt.bind s bind_address;
-          Srt.listen s 1;
-          self#log#info "Setting up socket to listen at %s"
-            (self#string_of_address bind_address);
+          Srt.setsockflag s Srt.messageapi messageapi;
+          self#prepare_socket s;
           socket <- Some s;
           s
+
+  method private close_socket =
+    match socket with
+      | None -> ()
+      | Some s ->
+          Srt.close s;
+          socket <- None
+end
+  
+class input ~kind ~bind_address ~bufferize ~max ~payload_size
+            ~on_connect ~on_disconnect ~messageapi format =
+  let max_ticks = Frame.master_of_seconds (Stdlib.max max bufferize) in
+  let log_ref = ref (fun _ -> ()) in
+  let log = (fun x -> !log_ref x) in
+  let abg =
+    Generator.create ~log ~kind ~overfull:(`Drop_old max_ticks) `Undefined
+  in
+object (self)
+  
+  inherit base ~payload_size ~messageapi
+  inherit Source.source ~name:"input.srt" kind as super
+  inherit
+    Generated.source abg ~empty_on_abort:false ~bufferize
+
+  val mutable kill_feeding = None
+  val mutable wait_feeding = None
 
   method private close_socket =
     match socket with
@@ -82,6 +93,12 @@ object (self)
     with exn ->
       self#log#important "Error while fetching connection source: %s"
         (Printexc.to_string exn)
+
+  method private prepare_socket s =
+    Srt.bind s bind_address;
+    Srt.listen s 1;
+    self#log#info "Setting up socket to listen at %s"
+      (self#string_of_address bind_address);
 
   method private feed (should_stop,has_stopped) =
     let client, origin =
@@ -164,9 +181,9 @@ let () =
     [ "bind_address", Lang.string_t, Some (Lang.string "0.0.0.0"),
       Some "Address to bind on the local machine.";
 
-      "port", Lang.int_t, Some (Lang.int 0),
+      "port", Lang.int_t, Some (Lang.int 8000),
       Some "Port to bind on the local machine (note: ports in SRT are \
-            different and not related to TCP ports.";
+            not related to TCP ports.)";
   
       "buffer", Lang.float_t, Some (Lang.float 2.),
       Some "Duration of the pre-buffered data.";
@@ -185,6 +202,9 @@ let () =
 
       "payload_size", Lang.int_t, Some (Lang.int 1316),
       Some "Payload size." ;
+
+      "messageapi", Lang.bool_t, Some (Lang.bool true),
+      Some "Use message api" ;
 
       "", Lang.string_t, None,
         Some "Mime (Content-Type) used to find a decoder for the iput stream." ]
@@ -208,6 +228,7 @@ let () =
            raise (Lang_errors.Invalid_value
                     (List.assoc "max" p,
                      "Maximum buffering inferior to pre-buffered data"));
+         let messageapi = Lang.to_bool (List.assoc "messageapi" p) in
          let payload_size = Lang.to_int (List.assoc "payload_size" p) in
          let on_connect () =
            ignore
@@ -219,5 +240,127 @@ let () =
          in
          let format = Lang.to_string (List.assoc "" p) in
          ((new input ~kind ~bind_address ~payload_size
-                    ~on_connect ~on_disconnect
+                    ~on_connect ~on_disconnect ~messageapi
                     ~bufferize ~max format):>Source.source))
+
+class output ~kind ~payload_size ~messageapi
+  ~on_start ~on_stop ~infallible ~autostart
+  ~port ~hostname ~encoder_factory source =
+object (self)
+
+  inherit base ~payload_size ~messageapi
+  inherit
+    Output.encoded ~output_kind:"srt" ~content_kind:kind
+      ~on_start ~on_stop ~infallible ~autostart
+      ~name:"output.srt" source
+
+  val buffer = Buffer.create payload_size
+  val tmp = Bytes.create payload_size
+  val mutable encoder = None
+
+  method private prepare_socket socket =
+    let ipaddr =
+      (Unix.gethostbyname hostname).Unix.h_addr_list.(0)
+    in
+    let sockaddr = Unix.ADDR_INET (ipaddr, port) in
+    self#log#important "Connecting to srt://%s:%d.." hostname port;
+    Srt.connect socket sockaddr;
+    self#log#important "Output connected!"
+
+  method private output_start =
+    encoder <-
+      Some (encoder_factory self#id Meta_format.empty_metadata)
+
+  method private output_reset = self#output_start ; self#output_stop
+
+  method private output_stop =
+    self#close_socket;
+    Buffer.reset buffer;
+    encoder <- None
+
+  method private encode frame ofs len =
+    (Utils.get_some encoder).Encoder.encode frame ofs len
+
+  method private insert_metadata m =
+    (Utils.get_some encoder).Encoder.insert_metadata m
+
+  method private send data =
+    Buffer.add_string buffer data;
+    match Buffer.length buffer with
+      | x when x < payload_size -> ()
+      | _ ->
+         let send socket data =
+           if messageapi then
+             Srt.sendmsg socket data (-1) false
+           else
+             Srt.send socket data
+         in
+         Buffer.blit buffer 0 tmp 0 payload_size;
+         Utils.buffer_drop buffer payload_size;
+         let rec f socket = function
+           | pos when pos < payload_size ->
+             let ret =
+               send socket (Bytes.sub tmp pos (payload_size-pos))
+             in
+             f socket (pos+ret)
+           | _ -> ()
+         in
+         try
+           f self#get_socket 0
+         with exn ->
+           self#log#important "Error while sending data: %s" (Printexc.to_string exn);
+           self#close_socket
+end
+
+let () =
+  let kind = Lang.univ_t 1 in
+  Lang.add_operator "output.srt" ~active:true
+    ~kind:(Lang.Unconstrained kind)
+    ~category:Lang.Output
+    ~descr:"Send a SRT stream."
+    (Output.proto @ [ 
+      "host", Lang.string_t, Some (Lang.string "localhost"),
+      Some "Address to connect to.";
+
+      "port", Lang.int_t, Some (Lang.int 8000),
+      Some "Port to connect to (note: ports in SRT are \
+            not related to TCP ports.)";
+
+      "payload_size", Lang.int_t, Some (Lang.int 1316),
+      Some "Payload size." ;
+
+      "messageapi", Lang.bool_t, Some (Lang.bool true),
+      Some "Use message api" ;
+
+      "", Lang.format_t kind, None, Some "Encoding format.";
+
+      "", Lang.source_t kind, None, None ])
+      (fun p kind ->
+         let hostname =
+           Lang.to_string (List.assoc "host" p)
+         in
+         let port = Lang.to_int (List.assoc "port" p) in
+         let messageapi = Lang.to_bool (List.assoc "messageapi" p) in
+         let payload_size = Lang.to_int (List.assoc "payload_size" p) in
+         let source = Lang.assoc "" 2 p in
+         let infallible = not (Lang.to_bool (List.assoc "fallible" p)) in
+         let autostart = Lang.to_bool (List.assoc "start" p) in
+         let on_start =
+           let f = List.assoc "on_start" p in
+             fun () -> ignore (Lang.apply ~t:Lang.unit_t f [])
+         in
+         let on_stop =
+           let f = List.assoc "on_stop" p in
+             fun () -> ignore (Lang.apply ~t:Lang.unit_t f [])
+         in
+         let encoder_factory =
+           let fmt = Lang.assoc "" 1 p in
+           try Encoder.get_factory (Lang.to_format fmt) with
+             | Not_found ->
+                 raise (Lang_errors.Invalid_value
+                          (fmt,
+                           "Cannot get a stream encoder for that format"))
+         in
+         ((new output ~kind ~hostname ~port ~payload_size ~autostart
+                      ~on_start ~on_stop ~infallible ~messageapi
+                      ~encoder_factory source):>Source.source))
