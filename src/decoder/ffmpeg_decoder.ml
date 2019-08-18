43be4033fa4e3840a843e88c439606d640f60531
Romain Beauxis <toots@rastageeks.org>
2019-08-18 15:38:41 -0500

    Use library specific error API for ffmpeg decoder.

--- src/decoder/ffmpeg_decoder.ml
+++ src/decoder/ffmpeg_decoder.ml
@@ -112,15 +112,21 @@ let create_decoder fname =
     try
       FFmpeg.Av.seek stream `Millisecond position [||];
       ticks
-    with Failure _ -> 0
+    with FFmpeg.Avutil.Error _ -> 0
+  in
+  let rec read_frame () =
+    try FFmpeg.Av.read_frame stream with
+      | FFmpeg.Avutil.Error `Invalid_data ->
+         read_frame ()
   in
   let decode gen =
-    match FFmpeg.Av.read_frame stream with
-      | `Frame frame ->
-          let content = convert frame in
-          G.set_mode gen `Audio ;
-          G.put_audio gen content 0 (Array.length content.(0))
-      | `End_of_file -> 
+    try
+      let frame = read_frame () in
+      let content = convert frame in
+      G.set_mode gen `Audio ;
+      G.put_audio gen content 0 (Array.length content.(0))
+    with
+      | FFmpeg.Avutil.Error `Eof -> 
           G.add_break gen;
           raise End_of_file
   in
@@ -254,17 +260,23 @@ let create_decoder input =
     try
       FFmpeg.Av.seek stream `Millisecond position [||];
       ticks
-    with Failure _ -> 0
+    with FFmpeg.Avutil.Error _ -> 0
+  in
+  let rec read_frame () =
+    try FFmpeg.Av.read_frame stream with
+      | FFmpeg.Avutil.Error `Invalid_data ->
+         read_frame ()
   in
   let decode gen =
-    match FFmpeg.Av.read_frame stream with
-      | `Frame frame ->
-          let content =
-            Converter.convert converter frame
-          in
-          Generator.set_mode gen `Audio ;
-          Generator.put_audio gen content 0 (Array.length content.(0))
-      | `End_of_file ->
+    try
+      let frame = read_frame () in
+      let content =
+        Converter.convert converter frame
+      in
+      Generator.set_mode gen `Audio ;
+      Generator.put_audio gen content 0 (Array.length content.(0))
+    with
+      | FFmpeg.Avutil.Error `Eof ->
           Generator.add_break gen;
           raise End_of_file
   in
