b8a66779e73fae5c0c17ea919a42dcdb83c025d6
Samuel Mimram <smimram@gmail.com>
2019-08-21 11:54:56 +0200

    Move all JSON builtins to the same file.

--- src/lang/builtins_json.ml
+++ src/lang/builtins_json.ml
@@ -22,6 +22,135 @@
 
 let log = Log.make ["lang";"json"]
 
+let rec to_json_compact v =
+  (* Utils.escape implements
+   * JSON's escaping RFC. *)
+  let print_s s =
+    Utils.escape_string (fun x -> Utils.escape_utf8 x) s
+  in
+  match v.Lang.value with
+    | Lang.Bool b -> Printf.sprintf "%b" b
+    | Lang.Int  i -> Printf.sprintf "%i" i
+    | Lang.String s -> print_s s
+    (* JSON specs do not allow a trailing . *)
+    | Lang.Float  n ->
+          let s = string_of_float n in
+          let s = Printf.sprintf "%s" s in
+          if s.[ (String.length s) - 1 ] = '.' then
+            Printf.sprintf "%s0" s
+          else
+            s
+    | Lang.List l ->
+        (* Convert (string*'a) list to object *)
+        begin
+         try
+          let t = v.Lang.t in
+          let t = Lang.of_list_t t in
+          let (t,_) = Lang.of_product_t t in
+          let compare = Lang_types.( <: ) in
+          ignore(compare t Lang.string_t);
+          let l =
+            List.map (fun x ->
+                        let (x,y) = Lang.to_product x in
+                        Printf.sprintf "%s:%s"
+                          (to_json_compact x) (to_json_compact y))
+                      l
+          in
+          Printf.sprintf "{%s}" (String.concat "," l)
+         with _ ->
+               Printf.sprintf "[%s]"
+                (String.concat ","
+                  (List.map to_json_compact l))
+        end
+    | Lang.Tuple l -> "[" ^ String.concat "," (List.map to_json_compact l) ^ "]"
+    | Lang.Source _ -> "\"<source>\""
+    | Lang.Ref v -> Printf.sprintf  "{\"reference\":%s}" (to_json_compact !v)
+    | Lang.Encoder e -> print_s (Encoder.string_of_format e)
+    | Lang.Request _ -> "\"<request>\""
+    | Lang.FFI _
+    | Lang.Fun _ -> "\"<fun>\""
+
+let rec to_json_pp f v =
+  match v.Lang.value with
+    | Lang.List l ->
+        (* Convert (string*'a) list to object *)
+        begin
+         try
+          let t = v.Lang.t in
+          let t = Lang.of_list_t t in
+          let (t,_) = Lang.of_product_t t in
+            let compare = Lang_types.( <: ) in
+            ignore(compare t Lang.string_t);
+            let print f l =
+              let len = List.length l in
+              let f pos x =
+                let (x,y) = Lang.to_product x in
+                if pos != len - 1 then
+                  Format.fprintf f "%a: %a,@;<1 0>"
+                    to_json_pp x to_json_pp y
+                else
+                  Format.fprintf f "%a: %a"
+                    to_json_pp x to_json_pp y ;
+                pos+1
+              in
+              ignore(List.fold_left f 0 l)
+            in
+            Format.fprintf f "@[{@;<1 1>@[%a@]@;<1 0>}@]" print l
+         with _ ->
+               let print f l =
+                 let len = List.length l in
+                 let f pos x =
+                   if pos < len -1 then
+                     Format.fprintf f "%a,@;<1 0>"
+                       to_json_pp x
+                   else
+                     Format.fprintf f "%a"
+                        to_json_pp x ;
+                 pos+1
+                 in
+                 ignore(List.fold_left f 0 l)
+               in
+               Format.fprintf f "@[[@;<1 1>@[%a@]@;<1 0>]@]" print l
+        end
+    | Lang.Tuple l ->
+       Format.fprintf f "@[[@;<1 1>@[";
+       let rec aux = function
+         | [] -> ()
+         | [p] -> Format.fprintf f "%a" to_json_pp p
+         | p::l -> Format.fprintf f "%a,;<1 0>" to_json_pp p; aux l
+       in
+       aux l;
+       Format.fprintf f "@]@;<1 0>]@]"
+    | Lang.Ref v ->
+       Format.fprintf  f
+         "@[{@;<1 1>@[\"reference\":@;<0 1>%a@]@;<1 0>}@]"
+         to_json_pp !v
+    | _ -> Format.fprintf f "%s" (to_json_compact v)
+
+let to_json_pp v =
+  let b = Buffer.create 10 in
+  let f = Format.formatter_of_buffer b in
+  ignore(to_json_pp f v);
+  Format.pp_print_flush f ();
+  Buffer.contents b
+
+let to_json ~compact v =
+  if compact then
+     to_json_compact v
+  else
+     to_json_pp v
+
+let () =
+  Lang_builtins.add_builtin "json_of" ~cat:Lang_builtins.String
+    ~descr:"Convert a value to a json string."
+     ["compact",Lang.bool_t,Some (Lang.bool false),
+      Some "Output compact text.";
+      "",Lang.univ_t 1,None,None] Lang.string_t
+    (fun p ->
+      let compact = Lang.to_bool (List.assoc "compact" p) in
+      let v = to_json ~compact (List.assoc "" p) in
+      Lang.string v)
+
 exception Failed
 
 let () =
