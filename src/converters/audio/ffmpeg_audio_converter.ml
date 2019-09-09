b84987c19fa176ede161db6b0feb5b0ccae0194e
Samuel Mimram <smimram@gmail.com>
2019-09-09 16:16:47 +0200

    Don't create a resampler each time.

--- src/converters/audio/ffmpeg_audio_converter.ml
+++ src/converters/audio/ffmpeg_audio_converter.ml
@@ -26,15 +26,21 @@ module Resampler = Swresample.Make (Swresample.PlanarFloatArray) (Swresample.Pla
 let samplerate_converter () =
   let chans = `Mono in
   let in_freq = Lazy.force Frame.audio_rate in
-  fun x ->
+  let rs = ref None in
+  let rs_out_freq = ref 0 in
+  fun x buf ofs len ->
     let out_freq = int_of_float (float in_freq *. x) in
-    let r = Resampler.create chans in_freq chans out_freq in
-    fun buf ofs len ->
-      let buf =
-        if ofs = 0 && len = Array.length buf then buf
-        else Array.sub buf ofs len
-      in
-      (Resampler.convert r [|buf|]).(0)
+    if !rs = None || !rs_out_freq <> out_freq then
+      (
+        rs := Some (Resampler.create chans in_freq chans out_freq);
+        rs_out_freq := out_freq
+      );
+    let rs = Utils.get_some !rs in
+    let buf =
+      if ofs = 0 && len = Array.length buf then buf
+      else Array.sub buf ofs len
+    in
+    (Resampler.convert rs [|buf|]).(0)
 
 let () = 
   Audio_converter.Samplerate.converters#register "ffmpeg" samplerate_converter
