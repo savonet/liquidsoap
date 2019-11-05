ee460799e57bc180f1a2d53fe03ef7175793d118
Samuel Mimram <smimram@gmail.com>
2019-11-05 18:04:11 +0100

    Children time.

--- src/lang/profiler.ml
+++ src/lang/profiler.ml
@@ -20,18 +20,45 @@
 
  *****************************************************************************)
 
-let calls = ref ([] : (string * float) list)
+(** Function call information. *)
+type t =
+  {
+    total_time : float; (** Time spent in the function. *)
+    self_time : float; (** Time spent in the function excluding children. *)
+  }
+
+let calls = ref []
+
+(* Call stack. *)
+let stack = ref []
+
+(* Time spent in children. *)
+let children = ref [ref 0.]
 
 (** Indicate the time spent in a given function. *)
 let add f t =
-  calls := (f,t) :: !calls
+  calls := (f, t) :: !calls
 
 (** Measure time for a given function. *)
 let time fname f x =
+  stack := fname :: !stack;
+  children := (ref 0.) :: !children;
+
   let t0 = Unix.gettimeofday () in
   let ans = f x in
   let t1 = Unix.gettimeofday () in
-  add fname (t1 -. t0);
+
+  stack := List.tl !stack;
+  let children_time = !(List.hd !children) in
+  children := List.tl !children;
+  let dt = t1 -. t0 in
+  List.hd !children := !(List.hd !children) +. dt;
+
+  (* We count recursive calls as zero otherwise time is counted multiple times. *)
+  let total_time = if List.mem fname !stack then 0. else dt in
+  let self_time = total_time -. children_time in
+  let t = { total_time; self_time } in
+  add fname t;
   ans
 
 module M = Map.Make(struct type t = string let compare = compare end)
@@ -49,7 +76,17 @@ let stats () =
     ) !calls;
   let m = !m in
   let l = M.bindings m in
-  let l = List.map (fun (f,tt) -> f, List.fold_left (+.) 0. tt) l in
+  let l =
+    List.map
+      (fun (f,t) ->
+         f,
+         (
+           List.fold_left (fun x t -> x +. t.total_time) 0. t,
+           List.fold_left (fun x t -> x +. t.self_time) 0. t,
+           List.length t
+         )
+      ) l
+  in
   let l = List.sort (fun (_,t) (_,t') -> - compare t t') l in
   "PROFILING STATS\n\n" ^
-  String.concat "" (List.map (fun (f,t) -> Printf.sprintf "%s: %f\n" f t) l)
+  String.concat "" (List.map (fun (f,(s,c,n)) -> Printf.sprintf "%s: %f %f %d\n" f s c n) l)
