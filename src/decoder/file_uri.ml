852db7a6ef25264ff1d293ac395517a354d56217
Garinot Pierre <garinot.pierre@errlock.org>
2019-12-11 16:10:34 +0100

    uri_decoder: file_uri uses get_file_decoder

--- src/decoder/file_uri.ml
+++ src/decoder/file_uri.ml
@@ -3,7 +3,8 @@ let log = Log.make ["decoder";"file"]
 let () =
   Decoder.uri_decoders#register "file"
   ~sdoc:"Decode a local file."
-  (fun ~metadata:_ uri kind ->
-    let filename = uri.URI.path in
-    Decoder.get_file_decoder ~metadata filename kind
+  (fun ~metadata uri kind ->
+    match Decoder.get_file_decoder ~metadata uri.URI.path kind with
+      | None -> None
+      | Some (_, f) -> Some (f)
   )
