4846478bb373548b30c23aaf9e0e6c5cea5421a0
Romain Beauxis <toots@rastageeks.org>
2020-03-17 13:02:17 -0500

    Properly trim unicode in id3v2 decoder. Fixes: #1113

--- src/tools/id3v2.ml
+++ src/tools/id3v2.ml
@@ -35,16 +35,11 @@ let parse f =
     let v2 = read_byte f in
     [| 2; v1; v2 |]
   in
-  (* Printf.printf "version: %s\n" (String.concat "." (List.map string_of_int (Array.to_list version))); *)
   let v = version.(1) in
   if v <> 3 && v <> 4 then raise Invalid;
   let flags = read_byte f in
-  (* let unsynchronization = flags land 0b10000000 <> 0 in *)
-  (* Printf.printf "unsynchronization: %b\n%!" unsynchronization; *)
   let extended_header = flags land 0b1000000 <> 0 in
-  (* Printf.printf "extended header: %b\n" extended_header; *)
   let size = read_size f in
-  (* Printf.printf "size: %d\n" size; *)
   if extended_header then (
     let size = read_size ~synch_safe:(v > 3) f in
     (* size *)
@@ -54,16 +49,12 @@ let parse f =
   let tags = ref [] in
   while !len > 0 do
     try
-      (* Printf.printf "len: %d\n" !len; *)
       let id = read f 4 in
-      (* Printf.printf "id: %s\n" id; *)
       if id = "\000\000\000\000" then len := 0 (* stop tag *)
       else (
         let size = read_size ~synch_safe:(v > 3) f in
-        (* Printf.printf "size: %d\n" size; *)
         let flags = read f 2 in
         let data = read f size in
-        (* if id <> "APIC" then Printf.printf "data: %S\n%!" data; *)
         len := !len - (size + 10);
         let compressed = int_of_char flags.[1] land 0b10000000 <> 0 in
         let encrypted = int_of_char flags.[1] land 0b01000000 <> 0 in
@@ -82,11 +73,17 @@ let parse f =
           in
           let encoding = int_of_char data.[0] in
           let z =
-            if encoding = 0 then (
-              try String.index_from data 1 '\000'
-              with Not_found -> String.length data )
-            else (* TODO: look for \000\000 *)
-              String.length data
+            match encoding with
+              | 0 (* ISO-8859-1 *) -> (
+                  try String.index_from data 1 '\000'
+                  with Not_found -> raise Invalid )
+              | 1 (* 16-bit unicode 2.0 *) -> (
+                  try
+                    let pos = String.index_from data 1 '\000' in
+                    ignore (String.index_from data pos '\000');
+                    pos
+                  with Not_found -> raise Invalid )
+              | _ -> String.length data
           in
           let text = String.sub data 1 (z - 1) in
           let text = recode encoding text in
