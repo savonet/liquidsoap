f33fd257cb2bb1255dfbcdb61d3efba120a67a6e
Samuel Mimram <smimram@gmail.com>
2020-04-01 17:59:26 +0200

    Use 443 as default port for https.
    Refs #1127.

--- src/sources/https_secure_transport_source.ml
+++ src/sources/https_secure_transport_source.ml
@@ -24,6 +24,7 @@ module Config = struct
   module Http = Https_secure_transport
 
   let url_expr = Str.regexp "^[Hh][Tt][Tt][Pp][sS]://\\([^/]+\\)\\(/.*\\)?$"
+  let default_port = 443
 end
 
 module Input_https = Http_source.Make (Config)
