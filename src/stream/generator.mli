c359759ce251550b0cf87640d38a38433773126d
Samuel Mimram <smimram@gmail.com>
2020-04-04 11:40:55 +0200

    Playlists as lists. (#1133)
    We can now play a list of files (as opposed to a playlist stored in a
    file). This is useful for scripts generating playlists.

--- src/stream/generator.mli
+++ src/stream/generator.mli
@@ -32,23 +32,23 @@ exception Incorrect_stream_type
 module type S = sig
   type t
 
-  (** Length of the generator in ticks. *)
   val length : t -> int
+  (** Length of the generator in ticks. *)
 
-  (** Duration of data (in ticks) before the next break, -1 if there's none. *)
   val remaining : t -> int
+  (** Duration of data (in ticks) before the next break, -1 if there's none. *)
 
-  (** Clear the generator. *)
   val clear : t -> unit
+  (** Clear the generator. *)
 
-  (** Fill a frame from the generator. *)
   val fill : t -> Frame.t -> unit
+  (** Fill a frame from the generator. *)
 
-  (** Forget a given duration (in ticks) of the generator. *)
   val remove : t -> int -> unit
+  (** Forget a given duration (in ticks) of the generator. *)
 
-  (** Add metadata. *)
   val add_metadata : t -> Frame.metadata -> unit
+  (** Add metadata. *)
 end
 
 (** Content-agnostic generator. *)
@@ -56,29 +56,29 @@ module Generator : sig
   (** A generator. *)
   type 'a t
 
-  (** Create a generator. *)
   val create : unit -> 'a t
+  (** Create a generator. *)
 
-  (** Empty a generator. *)
   val clear : 'a t -> unit
+  (** Empty a generator. *)
 
-  (** Length of data contained in a generator (in ticks). *)
   val length : 'a t -> int
+  (** Length of data contained in a generator (in ticks). *)
 
-  (** Remove given amount (in ticks) of data. *)
   val remove : 'a t -> int -> unit
+  (** Remove given amount (in ticks) of data. *)
 
+  val put : 'a t -> 'a -> int -> int -> unit
   (** [put data ofs len] adds [len] of data [data] starting from [ofs]. Data is
       not copied. *)
-  val put : 'a t -> 'a -> int -> int -> unit
 
+  val get : 'a t -> int -> ('a * int * int * int) list
   (** Get given amount of data from a generator. Returns a list where each
       element will typically be passed to a blit: its elements are of the form
       [b,o,o',l] where [o] is the offset of data in the block [b], [o'] is the
       position at which it should be written (the first position [o'] will
       always be [0]), and [l] is the length of data to be taken from that
       block. *)
-  val get : 'a t -> int -> ('a * int * int * int) list
 end
 
 (** A generator for metadata. *)
@@ -86,34 +86,34 @@ module Metadata : sig
   (** A metadata generator. *)
   type t
 
-  (** Create a generator. *)
   val create : unit -> t
+  (** Create a generator. *)
 
-  (** Clear generator. *)
   val clear : t -> unit
+  (** Clear generator. *)
 
-  (** Length in ticks. *)
   val length : t -> int
+  (** Length in ticks. *)
 
-  (** Time until next break, or -1 if there is none. *)
   val remaining : t -> int
+  (** Time until next break, or -1 if there is none. *)
 
-  (** Drop a portion at the begining. *)
   val advance : t -> int -> unit
+  (** Drop a portion at the begining. *)
 
+  val drop_initial_break : t -> unit
   (** Drop break at the beginning. This should be called after filling a partial
       frame manually (i.e. not using [fill]). *)
-  val drop_initial_break : t -> unit
 
-  (** Retrieve all metadata between now and given time. *)
   val metadata : t -> int -> (int * Frame.metadata) list
+  (** Retrieve all metadata between now and given time. *)
 
-  (** Feed all breaks and metadata from a frame. *)
   val feed_from_frame : t -> Frame.t -> unit
+  (** Feed all breaks and metadata from a frame. *)
 
+  val fill : t -> Frame.t -> unit
   (** Fill a frame (until the next break) with metadata and add a break at the
       end. *)
-  val fill : t -> Frame.t -> unit
 end
 
 (** A generator that consumes frames (or frame content) and produces frames. *)
@@ -121,28 +121,27 @@ module From_frames : sig
   (** A generator. *)
   type t
 
-  (** Create a generator. *)
   val create : unit -> t
+  (** Create a generator. *)
 
-  (** Remove all data from a generator. *)
   val clear : t -> unit
+  (** Remove all data from a generator. *)
 
-  (** Length of data in the generator. *)
   val length : t -> int
+  (** Length of data in the generator. *)
 
-  (** Remaining data before next break (-1) if there is none. *)
   val remaining : t -> int
+  (** Remaining data before next break (-1) if there is none. *)
 
-  (** Add metadata at the current position. *)
   val add_metadata : t -> Frame.metadata -> unit
+  (** Add metadata at the current position. *)
 
-  (** Add a break at the current position. *)
   val add_break : t -> unit
+  (** Add a break at the current position. *)
 
-  (** Remove data. *)
   val remove : t -> int -> unit
+  (** Remove data. *)
 
-  (** Feed the generator with data. *)
   val feed :
     t ->
     ?copy:bool ->
@@ -152,13 +151,14 @@ module From_frames : sig
     int ->
     int ->
     unit
+  (** Feed the generator with data. *)
 
+  val feed_from_frame : t -> Frame.t -> unit
   (** Feed the generator with the contents of a frame (the contents is
       copied). *)
-  val feed_from_frame : t -> Frame.t -> unit
 
-  (** Fill a frame from the generator. *)
   val fill : t -> Frame.t -> unit
+  (** Fill a frame from the generator. *)
 end
 
 (** Generator that consumes audio and video asynchronously, and produces
@@ -173,44 +173,44 @@ module From_audio_video : sig
       mode is assigned before any use. *)
   type mode = [ `Audio | `Video | `Both | `Undefined ]
 
-  (** Create a generator with given mode. *)
   val create : mode -> t
+  (** Create a generator with given mode. *)
 
+  val mode : t -> mode
   (** Current mode: in Audio mode (resp. Video), only audio (resp. Audio) can be
       fed, otherwise both have to be fed. *)
-  val mode : t -> mode
 
+  val set_mode : t -> mode -> unit
   (** Change the generator mode. Only allowed when there is as much audio as
       video. *)
-  val set_mode : t -> mode -> unit
 
-  (** Length of available audio data. *)
   val audio_length : t -> int
+  (** Length of available audio data. *)
 
-  (** Length of available video data. *)
   val video_length : t -> int
+  (** Length of available video data. *)
 
-  (** Length of data available in both audio and video. *)
   val length : t -> int
+  (** Length of data available in both audio and video. *)
 
-  (** Size of audio data in bytes. *)
   val audio_size : t -> int
+  (** Size of audio data in bytes. *)
 
-  (** Size of video data in bytes. *)
   val video_size : t -> int
+  (** Size of video data in bytes. *)
 
-  (** Size of data in bytes. *)
   val size : t -> int
+  (** Size of data in bytes. *)
 
-  (** Duration of data (in ticks) before the next break, -1 if there's none. *)
   val remaining : t -> int
+  (** Duration of data (in ticks) before the next break, -1 if there's none. *)
 
+  val add_metadata : t -> Frame.metadata -> unit
   (** Add metadata at the minimum position of audio and video. You probably want
       to call this when there is as much audio as video. *)
-  val add_metadata : t -> Frame.metadata -> unit
 
-  (** Add a track limit. Audio and video length should be equal. *)
   val add_break : ?sync:[ `Strict | `Ignore | `Drop ] -> t -> unit
+  (** Add a track limit. Audio and video length should be equal. *)
 
   (* [put_audio buffer data offset length]: offset and length
    * are in samples ! *)
@@ -220,11 +220,11 @@ module From_audio_video : sig
    * are in samples ! *)
   val put_video : t -> Frame.video_t array -> int -> int -> unit
 
-  (** Feed from a frame, only copying data according to the mode. *)
   val feed_from_frame : t -> Frame.t -> unit
+  (** Feed from a frame, only copying data according to the mode. *)
 
-  (** Fill a frame from the generator. *)
   val fill : t -> Frame.t -> unit
+  (** Fill a frame from the generator. *)
 
   val remove : t -> int -> unit
   val clear : t -> unit
