deec649f9c6f935c0624ac86eb582a17ca06d416
Samuel Mimram <smimram@gmail.com>
2020-04-07 11:41:53 +0200

    Properly scale alpha.

--- src/operators/video_effects.ml
+++ src/operators/video_effects.ml
@@ -87,11 +87,11 @@ let () =
     ~return_t ~category:Lang.VideoProcessing ~descr:"Scale opacity of video."
     (fun p kind ->
       let a = Lang.to_float_getter (Lang.assoc "" 1 p) in
-      let a () = int_of_float (a () *. 255.) in
       let src = Lang.to_source (Lang.assoc "" 2 p) in
-      (* TODO: we should scale the alpha channel instead of filling it with
-         something in case there is already an alpha channel. *)
-      new effect ~kind (fun buf -> Video.Image.fill_alpha buf (a ())) src)
+      new effect
+        ~kind
+        (fun buf -> Video.Image.Effect.Alpha.scale buf (a ()))
+        src)
 
 (*
 let () =
