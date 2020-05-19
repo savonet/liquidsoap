79498deff4e80ff6d51b239473446c4691192227
Romain Beauxis <toots@rastageeks.org>
2020-05-19 09:15:03 -0500

    make media generation part of the testing suite, cleanup and fix.

--- tests/media/mk_stream_test.sh
+++ tests/media/mk_stream_test.sh
@@ -9,5 +9,5 @@ FORMAT=`echo $FILE | cut -d '.' -f 1 | sed -e "s#@#%#g"`
 
 MODE=$2
 
-cat "${CWD}/generic/test_stream_${MODE}.liq.in" | sed -e "s#@FORMAT@#${FORMAT}#g" >| "${CWD}/generic/test_stream_${MODE}.liq"
+cat "${CWD}/test_stream_${MODE}.liq.in" | sed -e "s#@FORMAT@#${FORMAT}#g" >| "${CWD}/test_stream_${MODE}.liq"
 
