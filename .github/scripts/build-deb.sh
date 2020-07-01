3da6ad3a828e3de9c84dfd40a9c19f26c21bd51c
Romain Beauxis <toots@rastageeks.org>
2020-07-01 15:59:15 -0500

    Try attaching assets to release.

--- .github/scripts/build-deb.sh
+++ .github/scripts/build-deb.sh
@@ -3,18 +3,10 @@
 set -e
 
 GITHUB_SHA=$1
-GITHUB_HEAD_REF=$2
+BRANCH=$2
 DOCKER_TAG=$3
-BUILD_NUMBER=$4
-RELEASE=`echo "${DOCKER_TAG}" | cut -d'_' -f 2`
 
 COMMIT_SHORT=`echo "${GITHUB_SHA}" | cut -c-7`
-
-if test -z "${GITHUB_HEAD_REF}"; then
-  BRANCH=master
-else
-  BRANCH=`basename "${GITHUB_HEAD_REF}"`
-fi
 
 DEBFULLNAME="The Savonet Team"
 DEBEMAIL="savonet-users@lists.sourceforge.net"
@@ -23,26 +15,8 @@ eval $(opam config env)
 
 cd /tmp/liquidsoap-full/liquidsoap
 
-TAG=`echo "${COMMIT_SHORT}" | tr '[:upper:]' '[:lower:]' | sed -e 's#[^0-9^a-z^A-Z^.^-]#-#g'`
-
-LIQ_PACKAGE="liquidsoap-${TAG}" 
-
-echo "Building ${LIQ_PACKAGE}.."
-
-rm -rf debian/changelog
-
-cp -f debian/control.in debian/control
-
-sed -e "s#@LIQ_PACKAGE@#${LIQ_PACKAGE}#g" -i debian/control
-
 LIQ_VERSION=`opam show -f version .`
-
-dch --create --distribution unstable --package "${LIQ_PACKAGE}" --newversion "1:${LIQ_VERSION}-${BUILD_NUMBER}~${RELEASE}" "Build ${COMMIT_SHORT}"
-
-fakeroot debian/rules binary
-
-# We're only exporting the sha-tagged .deb as artifact.
-echo "##[set-output name=filename;]${LIQ_PACKAGE}_${LIQ_VERSION}-${BUILD_NUMBER}~${RELEASE}_amd64.deb"
+LIQ_TAG=`echo ${DOCKER_TAG} | sed -e 's#_#-#g'`
 
 TAG=`echo "${BRANCH}" | tr '[:upper:]' '[:lower:]' | sed -e 's#[^0-9^a-z^A-Z^.^-]#-#g'`
 
@@ -56,10 +30,12 @@ cp -f debian/control.in debian/control
 
 sed -e "s#@LIQ_PACKAGE@#${LIQ_PACKAGE}#g" -i debian/control
 
-dch --create --distribution unstable --package "${LIQ_PACKAGE}" --newversion "1:${LIQ_VERSION}-${BUILD_NUMBER}~${RELEASE}" "Build ${COMMIT_SHORT}"
+dch --create --distribution unstable --package "${LIQ_PACKAGE}" --newversion "1:${LIQ_VERSION}-${LIQ_TAG}-1" "Build ${COMMIT_SHORT}"
 
 fakeroot debian/rules binary
 
 mkdir -p "/tmp/debian/pkgs/${DOCKER_TAG}"
 
 cp /tmp/liquidsoap-full/*.deb "/tmp/debian/pkgs/${DOCKER_TAG}"
+
+echo "##[set-output name=basename;]${LIQ_PACKAGE}_${LIQ_VERSION}-${LIQ_TAG}-1_amd64"
--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -3,14 +3,9 @@
 set -e
 
 SYSTEM=$1
+BRANCH=$2
 RELEASE=$GITHUB_SHA
 OCAML_VERSION=4.08.0
-
-if test -z "${GITHUB_HEAD_REF}"; then
-  BRANCH=master
-else
-  BRANCH=`basename "${GITHUB_HEAD_REF}"`
-fi
 
 DEPS_IMAGE="savonet/liquidsoap-win32-deps-${SYSTEM}"
 BASE_IMAGE="savonet/liquidsoap-win32-base-${SYSTEM}"
@@ -19,18 +14,16 @@ COMMIT_SHORT=`echo "${GITHUB_SHA}" | cut -c-7`
 
 if [ "${SYSTEM}" = "x64" ]; then
   HOST="x86_64-w64-mingw32.static"
-  BUILD="${COMMIT_SHORT}-win64~${GITHUB_RUN_NUMBER}"
+  BUILD="${BRANCH}-win64"
 else
   HOST="i686-w64-mingw32.static"
-  BUILD="${COMMIT_SHORT}-win32~${GITHUB_RUN_NUMBER}"
+  BUILD="${BRANCH}-win32"
 fi
 
-docker build -f .github/docker/Dockerfile.win32-deps -t ${BASE_IMAGE} \
-  --build-arg IMAGE=${DEPS_IMAGE} .
+docker build -f .github/docker/Dockerfile.win32-deps -t ${BASE_IMAGE} --build-arg IMAGE=${DEPS_IMAGE} .
 docker build -f .github/docker/Dockerfile.win32 -t ${IMAGE} --no-cache --build-arg RELEASE=${RELEASE} \
   --build-arg IMAGE=${BASE_IMAGE} --build-arg HOST=${HOST} --build-arg BUILD=${BUILD} .
 id=$(docker create ${IMAGE})
-docker cp ${id}:/tmp/win32 .
+docker cp ${id}:/tmp/liquidsoap-${BUILD}.zip .
 docker rm -v ${id}
-echo "##[set-output name=path;]liquidsoap-${BUILD}"
-
+echo "##[set-output name=basename;]liquidsoap-${BUILD}"
