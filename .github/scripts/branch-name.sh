0f99df6d20c0c2bc39cbf5309899ec34ee8378f2
Romain Beauxis <toots@rastageeks.org>
2020-07-02 15:46:38 -0500

    Fix branch detection.

--- .github/scripts/branch-name.sh
+++ .github/scripts/branch-name.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-if [ "${GITHUB_HEAD_REF}" ]; then
-  BRANCH=${GITHUB_HEAD_REF#refs/heads/}
+if [ -n "${GITHUB_HEAD_REF}" ]; then
+  BRANCH=${GITHUB_HEAD_REF}
 else
   BRANCH=${GITHUB_REF#refs/heads/}
 fi
 
+echo "Detected branch: ${BRANCH}"
+
 if [[ "${BRANCH}" =~ ^v[0-9] ]]; then
+  echo "Branch is release branch"
   IS_RELEASE=true
 else
+  echo "Branch is not release branch"
   IS_RELEASE=
 fi
 
