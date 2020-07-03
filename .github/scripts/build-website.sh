6f6bd559cfbfd64a920e598555c40ec22b79e945
Romain Beauxis <toots@rastageeks.org>
2020-07-03 13:23:06 -0500

    Fix path.

--- .github/scripts/build-website.sh
+++ .github/scripts/build-website.sh
@@ -7,9 +7,9 @@ BASE_DIR=`cd "${PWD}/../.." && pwd`
 
 DOCKER_IMAGE=savonet/liquidsoap-github-actions-website
 
-docker build --no-cache -t "${DOCKER_IMAGE}" -f "${BASE_DIR}/.github/docker/Dockerfile.website" .
+#docker build --no-cache -t "${DOCKER_IMAGE}" -f "${BASE_DIR}/.github/docker/Dockerfile.website" .
 
 id=$(docker create ${DOCKER_IMAGE})
-docker cp $id:/tmp/liquidsoap-fullwebsite/html html/
+docker cp $id:/tmp/liquidsoap-full/website/html html/
 docker rm -v $id
 
