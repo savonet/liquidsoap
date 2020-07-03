a4c4a3e52b2dc4fd3cd8eca98950ea806a7849bd
Romain Beauxis <toots@rastageeks.org>
2020-07-03 13:15:34 -0500

    Absolute path to docker file.

--- .github/scripts/build-website.sh
+++ .github/scripts/build-website.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+PWD=`dirname $0`
+BASE_DIR=`cd "${PWD}/../.." && pwd`
+
 DOCKER_IMAGE=savonet/liquidsoap-github-actions-website
 
-docker build --no-cache -t "${DOCKER_IMAGE}" -f docker/Dockerfile.website .
+docker build --no-cache -t "${DOCKER_IMAGE}" -f "${BASE_DIR}/.github/docker/Dockerfile.website" .
 
 id=$(docker create ${DOCKER_IMAGE})
 docker cp $id:/tmp/liquidsoap-fullwebsite/html html/
