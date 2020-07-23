54b0cdf6b20981922ed5669a26e6d22c3c7d6b42
Samuel Mimram <smimram@gmail.com>
2020-07-23 11:08:01 +0200

    Fix typing of invocations.
    Fixes #1295.

--- src/lang/lang_values.ml
+++ src/lang/lang_values.ml
@@ -774,7 +774,7 @@ let rec check ?(print_toplevel = false) ~level ~(env : T.env) e =
                    enough for records. *)
                 let x = T.fresh_evar ~level ~pos in
                 let y = T.fresh_evar ~level ~pos in
-                { t with T.pos } <: mk (T.Meth (l, ([], x), y));
+                a.t >: mk (T.Meth (l, ([], x), y));
                 x
         in
         e.t >: aux a.t
