a971ec850e5377de2443f994a86fb8bf0b96396e
Romain Beauxis <toots@rastageeks.org>
2020-07-28 10:09:48 -0500

    Set deps from oldest to newest.

--- .github/scripts/checkout-deps.sh
+++ .github/scripts/checkout-deps.sh
@@ -3,9 +3,9 @@
 CWD=`dirname $0`
 BASEDIR=`cd $CWD/../../.. && pwd`
 
-git log --pretty=format:%b origin/master..HEAD | grep '^DEPS=' | cut -d'=' -f 2  | tr ":" "\n" | while read i; do \
+git log --pretty=format:%b origin/master..HEAD | grep '^DEPS=' | cut -d'=' -f 2  | tr ":" "\n" | tail -r | while read i; do \
   MODULE=`echo "$i" | cut -d'#' -f 1`
   COMMIT=`echo "$i" | cut -d'#' -f 2` 
   echo "Checking out dep $MODULE on commit $COMMIT"
-  cd "${BASEDIR}/${MODULE}" && git fetch origin "$COMMIT" && git reset --hard && git checkout "$COMMIT" && git submodule init && git submodule update
+  #cd "${BASEDIR}/${MODULE}" && git fetch origin "$COMMIT" && git reset --hard && git checkout "$COMMIT" && git submodule init && git submodule update
 done
