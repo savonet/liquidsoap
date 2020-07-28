a26503034664ec6aa19295a2ec5a6a933398a992
Romain Beauxis <toots@rastageeks.org>
2020-07-28 10:42:17 -0500

    Fix this.

--- .github/scripts/checkout-deps.sh
+++ .github/scripts/checkout-deps.sh
@@ -7,5 +7,5 @@ git log --pretty=format:%b origin/master..HEAD | grep '^DEPS=' | cut -d'=' -f 2 
   MODULE=`echo "$i" | cut -d'#' -f 1`
   COMMIT=`echo "$i" | cut -d'#' -f 2` 
   echo "Checking out dep $MODULE on commit $COMMIT"
-  #cd "${BASEDIR}/${MODULE}" && git fetch origin "$COMMIT" && git reset --hard && git checkout "$COMMIT" && git submodule init && git submodule update
+  cd "${BASEDIR}/${MODULE}" && git fetch origin "$COMMIT" && git reset --hard && git checkout "$COMMIT" && git submodule init && git submodule update
 done
