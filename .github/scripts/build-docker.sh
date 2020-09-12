9c61057d81221892a1ad73555a48480907307166
Romain Beauxis <toots@rastageeks.org>
2020-09-12 17:17:24 -0500

    Add arm64 build.

--- .github/scripts/build-docker.sh
+++ .github/scripts/build-docker.sh
@@ -6,8 +6,11 @@ DEB_FILE=$1
 TAG=$2
 USER=$3
 PASSWORD=$4
+ARCHITECTURE=$5
 
 cp $DEB_FILE .
-docker build --no-cache -f .github/docker/Dockerfile.production -t savonet/liquidsoap:$TAG .
+docker build --no-cache -f .github/docker/Dockerfile.production -t savonet/liquidsoap:${TAG}_${ARCHITECTURE} .
+
 docker login -u "$USER" -p "$PASSWORD" 
-docker push savonet/liquidsoap:$TAG
+
+docker push savonet/liquidsoap:${TAG}_${ARCHITECTURE}
