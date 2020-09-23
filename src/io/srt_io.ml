d504c8df30e68b5fb30f39c381c0a6184da346da
Romain Beauxis <toots@rastageeks.org>
2020-09-23 09:33:36 -0500

    Actually reset the whole socket on deconnection.

--- src/io/srt_io.ml
+++ src/io/srt_io.ml
@@ -318,8 +318,9 @@ class input ~kind ~bind_address ~max ~log_overfull ~payload_size ~clock_safe
           self#log#debug "Failed to connect: %s." (Printexc.to_string exn);
           self#connect
       in
-      if not self#should_stop then
-        Poll.add_socket ~mode:`Read self#get_socket on_connect
+      if not self#should_stop then (
+        self#close_socket;
+        Poll.add_socket ~mode:`Read self#get_socket on_connect )
 
     method private get_frame frame =
       let pos = Frame.position frame in
