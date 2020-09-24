1c70b456291bd9177d2ae7659357d21220568a1e
Romain Beauxis <toots@rastageeks.org>
2020-09-24 18:07:51 -0500

    Use scrap repo for docker build, only push combined image for master and release branches.

--- .github/scripts/build-docker.sh
+++ .github/scripts/build-docker.sh
@@ -9,8 +9,8 @@ PASSWORD=$4
 ARCHITECTURE=$5
 
 cp $DEB_FILE .
-docker build --no-cache -f .github/docker/Dockerfile.production -t savonet/liquidsoap:${TAG}_${ARCHITECTURE} .
+docker build --no-cache -f .github/docker/Dockerfile.production -t savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE} .
 
 docker login -u "$USER" -p "$PASSWORD" 
 
-docker push savonet/liquidsoap:${TAG}_${ARCHITECTURE}
+docker push savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}
