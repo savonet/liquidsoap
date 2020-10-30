4103148bc70d928aa5499665c093ec5aec211264
Romain Beauxis <toots@rastageeks.org>
2020-10-30 09:36:39 -0500

    Try this.

--- .github/scripts/branch-name.sh
+++ .github/scripts/branch-name.sh
@@ -11,16 +11,16 @@ fi
 echo "Detected branch: ${BRANCH}"
 echo "##[set-output name=branch;]${BRANCH}"
 
-if [[ "${BRANCH}" =~ ^v[0-9] ]]; then
-  echo "Branch is release branch"
-  echo "##[set-output name=is_release;]${IS_RELEASE}"
-else
-  echo "Branch is not release branch"
-fi
-
 if [[ "${BRANCH}" = "master" ]] || [[ -n "${IS_RELEASE}" ]]; then
   echo "Branch has a docker release"
   echo "##[set-output name=docker_release;]${DOCKER_RELEASE}"
 else
   echo "Branch does not have a docker release"
+fi
+
+if [[ "${BRANCH}" =~ ^v[0-9] ]]; then
+  echo "Branch is release branch"
+  echo "##[set-output name=is_release;]${IS_RELEASE}"
+else
+  echo "Branch is not release branch"
 fi
