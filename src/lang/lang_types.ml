9b416beead00ec6019ac6959d5b586f662500000
Romain Beauxis <toots@rastageeks.org>
2020-12-05 12:07:20 -0600

    Fix error reported when checking type inference on records.

--- src/lang/lang_types.ml
+++ src/lang/lang_types.ml
@@ -1007,18 +1007,17 @@ let rec ( <: ) a b =
     | _, Meth (l, (g2, t2), u2) -> (
         try
           let g1, t1 = invoke a l in
-          try
-            (* TODO: we should perform proper type scheme subtyping, but this is
-               a good approximation for now... *)
-            instantiate ~level:(-1) ~generalized:g1 t1
-            <: instantiate ~level:(-1) ~generalized:g2 t2;
-            a <: hide_meth l u2
+          ( try
+              instantiate ~level:(-1) ~generalized:g1 t1
+              <: instantiate ~level:(-1) ~generalized:g2 t2
+            with Error (a, b) ->
+              raise
+                (Error
+                   (`Meth (l, ([], a), `Ellipsis), `Meth (l, ([], b), `Ellipsis)))
+          );
+          try a <: hide_meth l u2
           with Error (a, b) ->
-            (* TODO: it would be better to keep generalized variables here and
-               below *)
-            raise
-              (Error
-                 (`Meth (l, ([], a), `Ellipsis), `Meth (l, ([], b), `Ellipsis)))
+            raise (Error (a, `Meth (l, ([], `Ellipsis), b)))
         with Not_found -> (
           let a' = demeth a in
           match a'.descr with
