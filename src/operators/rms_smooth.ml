534e435db7e833635041c6f2a86a8b75580c3f20
Samuel Mimram <smimram@gmail.com>
2021-03-27 17:31:38 +0100

    Add rms.smooth.

---
+++ src/operators/rms_smooth.ml
@@ -0,0 +1,95 @@
+(*****************************************************************************
+
+  Liquidsoap, a programmable audio stream generator.
+  Copyright 2003-2021 Savonet team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details, fully stated in the COPYING
+  file at the root of the liquidsoap distribution.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+ *****************************************************************************)
+
+open Source
+
+class rms ~kind ~tau source =
+  let samplerate = float (Lazy.force Frame.audio_rate) in
+  object (self)
+    inherit operator kind [source] ~name:"rms" as super
+
+    method stype = source#stype
+
+    method is_ready = source#is_ready
+
+    method remaining = source#remaining
+
+    method seek = source#seek
+
+    method abort_track = source#abort_track
+
+    method self_sync = source#self_sync
+
+    method wake_up a = super#wake_up a
+
+    val mutable rms = 0.
+
+    method rms = sqrt rms
+
+    method private get_frame buf =
+      let chans = self#audio_channels in
+      let a = 1. /. ((tau () *. samplerate) +. 1.) in
+      let offset = AFrame.position buf in
+      source#get buf;
+      let position = AFrame.position buf in
+      let buf = AFrame.pcm buf in
+      for i = offset to position - 1 do
+        let r = ref 0. in
+        for c = 0 to chans - 1 do
+          let x = buf.(c).{i} in
+          r := !r +. (x *. x)
+        done;
+        let r = !r /. float chans in
+        rms <- ((1. -. a) *. rms) +. (a *. r)
+      done
+  end
+
+let () =
+  let kind = Lang.audio_pcm in
+  let return_t = Lang.kind_type_of_kind_format kind in
+  Lang.add_operator "rms.smooth" ~category:Lang.Visualization
+    ~meth:
+      [
+        ( "rms",
+          ([], Lang.fun_t [] Lang.float_t),
+          "Current value for the RMS.",
+          fun s -> Lang.val_fun [] (fun _ -> Lang.float s#rms) );
+      ]
+    ~return_t
+    ~descr:
+      "Compute the current RMS for the source, this varies more smoothly that \
+       `rms` and is updated more frequently. Returns the source with a method \
+       `rms`."
+    [
+      ( "duration",
+        Lang.getter_t Lang.float_t,
+        Some (Lang.float 0.5),
+        Some
+          "Duration of the window in seconds (more precisely, this is the time \
+           constant of the low-pass filter)." );
+      ("", Lang.source_t return_t, None, None);
+    ]
+    (fun p ->
+      let f v = List.assoc v p in
+      let src = Lang.to_source (f "") in
+      let duration = Lang.to_float_getter (f "duration") in
+      new rms ~kind ~tau:duration src)
