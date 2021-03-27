2d9dc7b8cd8142cafde2519c4415cb3c97247713
Samuel Mimram <smimram@gmail.com>
2021-03-27 22:22:47 +0100

    Add delay_line.

---
+++ src/operators/delay_line.ml
@@ -0,0 +1,83 @@
+(*****************************************************************************
+
+  Liquidsoap, a programmable audio stream generator.
+  Copyright 2003-2021 Savonet team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details, fully stated in the COPYING
+  file at the root of the liquidsoap distribution.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+ *****************************************************************************)
+
+open Mm
+open Source
+
+class delay ~kind (source : source) duration =
+  let length = Frame.audio_of_seconds duration in
+  object (self)
+    inherit operator ~name:"amplify" kind [source] as super
+
+    val mutable override = None
+
+    method stype = source#stype
+
+    method is_ready = source#is_ready
+
+    method remaining = source#remaining
+
+    method abort_track = source#abort_track
+
+    method seek = source#seek
+
+    method self_sync = source#self_sync
+
+    val mutable buffer = [||]
+
+    (** Position of the last valid sample in the buffer. *)
+    val mutable pos = 0
+
+    method wake_up a =
+      super#wake_up a;
+      buffer <- Audio.create self#audio_channels length
+
+    method private get_frame buf =
+      let offset = AFrame.position buf in
+      source#get buf;
+      let position = AFrame.position buf in
+      let buf = AFrame.pcm buf in
+      if length > 0 then
+        for i = offset to position - 1 do
+          for c = 0 to self#audio_channels - 1 do
+            let x = buf.(c).{i} in
+            buf.(c).{i} <- buffer.(c).{pos};
+            buffer.(c).{pos} <- x
+          done;
+          pos <- (pos + 1) mod length
+        done
+  end
+
+let () =
+  let kind = Lang.audio_pcm in
+  let k = Lang.kind_type_of_kind_format kind in
+  Lang.add_operator "delay_line"
+    [
+      ("", Lang.float_t, None, Some "Duration of the delay in seconds.");
+      ("", Lang.source_t k, None, None);
+    ]
+    ~return_t:k ~category:Lang.SoundProcessing
+    ~descr:"Delay the audio signal by a given amount of time."
+    (fun p ->
+      let duration = Lang.assoc "" 1 p |> Lang.to_float in
+      let s = Lang.assoc "" 2 p |> Lang.to_source in
+      new delay ~kind s duration)
