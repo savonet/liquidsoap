c6b17d0fb065e2c7e723d78f199e7618f8c13cf7
Samuel Mimram <smimram@gmail.com>
2021-04-26 08:58:18 +0200

    Allow dots in ids.

--- src/source.ml
+++ src/source.ml
@@ -390,8 +390,6 @@ class virtual operator ?(name = "src") ?audio_in ?video_in ?midi_in out_kind
     method id = id
 
     method set_id ?(definitive = true) s =
-      (* ID musn't contain "." *)
-      let s = Pcre.substitute ~pat:"\\." ~subst:(fun _ -> "(dot)") s in
       let s = Pcre.substitute ~pat:"[ \t\n]" ~subst:(fun _ -> "_") s in
       if not definitive_id then (
         id <- s;
