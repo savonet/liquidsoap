b5c2886363361600102f3afac6ed546ffaf47c4e
Romain Beauxis <toots@rastageeks.org>
2021-05-04 09:01:26 -0500

    Try

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+whoami
+ls -lA /home/opam
+
 eval $(opam config env)
 
 # Remove after next base image rebuild
@@ -17,7 +20,11 @@ make update
 export PKG_CONFIG_PATH=/usr/share/pkgconfig/pkgconfig
 
 ./bootstrap
-./configure
+
+./configure --prefix=/usr --includedir=\${prefix}/include --mandir=\${prefix}/share/man \
+            --infodir=\${prefix}/share/info --sysconfdir=/etc --localstatedir=/var \
+            --with-camomile-data-dir=/usr/share/liquidsoap/camomile
+
 cd liquidsoap
 make
 make doc
