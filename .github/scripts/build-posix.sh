53f34dcc1bb46a1fa9f6b37b14cfaa6d243b9473
Romain Beauxis <toots@rastageeks.org>
2021-05-05 09:41:43 -0500

    Annotate.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -9,10 +9,17 @@ opam install -y posix-time2
 
 cd /tmp/liquidsoap-full
 
+echo "### Preparing bindings\n"
+
 git pull
 make clean
 make update
+
+echo "### Setting up specific dependencies\n"
+
 ./liquidsoap/.github/scripts/checkout-deps.sh
+
+echo "### Checking out CI commit\n"
 
 cd liquidsoap
 git fetch origin $GITHUB_SHA
@@ -21,6 +28,8 @@ git checkout $GITHUB_SHA
 cd /tmp/liquidsoap-full
 
 export PKG_CONFIG_PATH=/usr/share/pkgconfig/pkgconfig
+
+echo "### Compiling\n"
 
 ./bootstrap
 ./configure --prefix=/usr --includedir=\${prefix}/include --mandir=\${prefix}/share/man \
