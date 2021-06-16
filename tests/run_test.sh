a8d2d844d1233e797769d780dc94a5268a32f64c
Romain Beauxis <toots@rastageeks.org>
2021-06-16 08:23:29 -0500

    Disable timeout in tests for now.

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -12,11 +12,7 @@ if [ -z "${TEST_NAME}" ]; then
   TEST_NAME=${TEST}
 fi
 
-TIMEOUT=1200 # 20 minutes.
 LOG_FILE=`mktemp`
-LIQ_PID=
-MAIN_PID=$$
-WATCH_PID=
 
 trap cleanup 0 1 2
 
@@ -24,37 +20,9 @@ cleanup() {
   rm -rf "${LOG_FILE}"
 }
 
-print_timeout() {
-  echo -e "\033[1;34m[timeout]\033[0m"
-  cat "${LOG_FILE}"
-  exit 1
-}
-
-timeout() {
-  kill -9 $WAITD_PID >/dev/null 2>&1
-  kill -9 $PID >/dev/null 2>&1 && print_timeout
-}
-
-watchit() {
-  sleep $TIMEOUT &
-  wait $!
-  kill -ALRM $MAIN_PID
-}
-
-watchit &
-WATCH_PID=$!
-trap timeout ALRM
-
 echo -en "Running test \033[1m${TEST_NAME}\033[0m... "
 
 ${CMD} < "${PWD}/${TEST}"  > "${LOG_FILE}" 2>&1 &
-
-PID=$!
-wait $PID
-STATUS=$?
-
-kill -ALRM $WATCH_PID 2>/dev/null
-wait $WATCH_PID
 
 if [ "${STATUS}" == "0" ]; then
   echo -e "\033[0;32m[ok]\033[0m"
