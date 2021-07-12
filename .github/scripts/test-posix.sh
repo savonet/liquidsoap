2371cc3fc33e6b6eccc0249d596b1431344c4dde
Romain Beauxis <toots@rastageeks.org>
2021-07-12 14:29:15 -0500

    Build & run tests in parallel.

--- .github/scripts/test-posix.sh
+++ .github/scripts/test-posix.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CPU_CORES=$1
+
 cd /tmp/liquidsoap-full/liquidsoap
 eval $(opam config env)
-make test
+make test -j $CPU_CORES
--- tests/run_test.sh
+++ tests/run_test.sh
@@ -16,7 +16,11 @@ run_test() {
     TEST_NAME=${TEST}
   fi
 
+  TEST_NAME=`echo ${TEST_NAME} | sed -e 's#%#%%#g'`
+
   LOG_FILE=`mktemp`
+
+  START_TIME="$(date +%s)"
 
   trap cleanup 0 1 2
 
@@ -25,7 +29,8 @@ run_test() {
   }
 
   on_timeout() {
-    echo -e "\033[1;34m[timeout]\033[0m"
+    T="$(($(date +%s)-START_TIME))"
+    printf "Ran test \033[1m${TEST_NAME}\033[0m: \033[1;34m[timeout]\033[0m (Test time: %02dm:%02ds)\n" "$((T/60))" "$((T%60))"
     cat "${LOG_FILE}"
     kill -9 "$PID"
     exit 1
@@ -33,25 +38,24 @@ run_test() {
 
   trap on_timeout 15
 
-  echo -en "Running test \033[1m${TEST_NAME}\033[0m... "
-
-  ${CMD} < "${PWD}/${TEST}"  > "${LOG_FILE}" 2>&1 &
+  ${CMD} < "${PWD}/${TEST}" > "${LOG_FILE}" 2>&1
 
   PID=$!
   wait $PID
   STATUS=$?
+  T="$(($(date +%s)-START_TIME))"
 
   if [ "${STATUS}" == "0" ]; then
-    echo -e "\033[0;32m[ok]\033[0m"
+    printf "Ran test \033[1m${TEST_NAME}\033[0m: \033[0;32m[ok]\033[0m (Test time: %02dm:%02ds)\n" "$((T/60))" "$((T%60))"
     exit 0
   fi
 
   if [ "${STATUS}" == "2" ]; then
-      echo -e "\033[1;33m[skipped]\033[0m"
+      printf "Ran test \033[1m${TEST_NAME}\033[0m: \033[1;33m[skipped]\033[0m\n"
       exit 0
   fi
 
-  echo -e "\033[0;31m[failed]\033[0m"
+  printf "Ran test \033[1m${TEST_NAME}\033[0m: \033[0;31m[failed]\033[0m (Test time: %02dm:%02ds)\n" "$((T/60))" "$((T%60))"
   cat "${LOG_FILE}"
   exit 1
 }
