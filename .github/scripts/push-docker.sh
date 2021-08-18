271bd470ca5f07599de7857e53248f3a70119d5c
Romain Beauxis <toots@rastageeks.org>
2021-08-18 15:46:34 +0200

    Fix password for final push.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -5,6 +5,8 @@ set -e
 TAG=$1
 USER=$2
 PASSWORD=$3
+GHCR_USER=$4
+GHCR_PASSWORD=$5
 
 rm -rf ~/.docker/config.json
 mkdir -p ~/.docker
@@ -17,6 +19,8 @@ docker manifest push savonet/liquidsoap:$TAG
 
 docker manifest create savonet/liquidsoap-alpine:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_alpine_arm64
 docker manifest push savonet/liquidsoap-alpine:$TAG
+
+docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
 docker manifest create ghcr.io/savonet/liquidsoap:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64
 docker manifest push ghcr.io/savonet/liquidsoap:$TAG
