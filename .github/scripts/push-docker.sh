a54549332a3d0cfbce2de0f7e0bf828f64386621
Romain Beauxis <toots@rastageeks.org>
2021-08-31 17:34:34 -0500

    Fix push-docker.sh

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -17,13 +17,13 @@ docker login -u "$USER" -p "$PASSWORD"
 docker manifest create savonet/liquidsoap:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_arm64 savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push savonet/liquidsoap:$TAG
 
-docker manifest create savonet/liquidsoap-alpine:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_alpine_arm64
+docker manifest create savonet/liquidsoap-alpine:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_alpine_arm64 savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
 docker manifest push savonet/liquidsoap-alpine:$TAG
 
 docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
-docker manifest create ghcr.io/savonet/liquidsoap:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64 savonet/liquidsoap-ci-build:${TAG}_armhf
+docker manifest create ghcr.io/savonet/liquidsoap:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64 ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push ghcr.io/savonet/liquidsoap:$TAG
 
-docker manifest create ghcr.io/savonet/liquidsoap-alpine:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64
+docker manifest create ghcr.io/savonet/liquidsoap-alpine:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
 docker manifest push ghcr.io/savonet/liquidsoap-alpine:$TAG
