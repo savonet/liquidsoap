a1ae2977b362b1b8d31e49d5625b167079bc5bbb
Romain Beauxis <toots@rastageeks.org>
2021-11-25 14:15:38 +0100

    Fix previous commit.

--- src/lang/builtins_ffmpeg_filters.ml
+++ src/lang/builtins_ffmpeg_filters.ml
@@ -359,8 +359,7 @@ let apply_filter ~args_parser ~filter ~sources_t p =
               let v = List.assoc "" p in
               if !input_set then
                 Runtime_error.error
-                  ~pos:
-                    (match v.Term.Value.pos with None -> [] | Some p -> [p])
+                  ~pos:(match v.Value.pos with None -> [] | Some p -> [p])
                   ~message:"Filter input already set!" "ffmpeg.filter";
               let audio_inputs_c = List.length filter.io.inputs.audio in
               let get_input ~mode ~ofs idx =
@@ -499,8 +498,8 @@ let () =
             in
             let args = named_args @ unnamed_args in
             let filter = apply_filter ~args_parser ~filter ~sources_t args in
-            ignore (Lang.apply (Term.Value.invoke filter "set_intput") inputs);
-            Term.Value.invoke filter "output");
+            ignore (Lang.apply (Value.invoke filter "set_intput") inputs);
+            Value.invoke filter "output");
         Lang.add_builtin ~category:`Filter
           ("ffmpeg.filter." ^ name ^ ".create")
           ~descr:
