fca665dd6097cd8e0f99cc884140ad5c29f0ed7c
Romain Beauxis <toots@rastageeks.org>
2021-11-26 13:38:28 +0100

    Parametric include

--- .github/scripts/build-details.sh
+++ .github/scripts/build-details.sh
@@ -22,10 +22,12 @@ if [[ "${BRANCH}" =~ ^v[0-9] ]]; then
   echo "Branch is versioned: building on all architectures"
   BUILD_OS='["debian_testing", "debian_buster", "debian_bullseye", "ubuntu_groovy", "ubuntu_focal", "alpine"]'
   BUILD_PLATFORM='["amd64", "arm64", "armhf"]'
+  BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64"}, {"platform": "arm64", "runs-on": "self-hosted", "alpine-arch": "aarch64", "docker-platform": "linux/arm64"}, {"platform": "armhf", "runs-on": "self-hosted", "alpine-arch": "armv7", "docker-platform": "linux/arm/v7"}]'
 else
   echo "Branch is not versioned: building on amd64 only"
   BUILD_OS='["debian_testing", "debian_buster", "debian_bullseye", "ubuntu_groovy", "ubuntu_focal", "alpine"]'
   BUILD_PLATFORM='["amd64"]'
+  BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64"}]'
 fi
 
 if [[ "${BRANCH}" = "main" ]] || [[ -n "${IS_RELEASE}" ]]; then
@@ -42,6 +44,7 @@ echo "##[set-output name=branch;]${BRANCH}"
 echo "##[set-output name=is_release;]${IS_RELEASE}"
 echo "##[set-output name=build_os;]${BUILD_OS}"
 echo "##[set-output name=build_platform;]${BUILD_PLATFORM}"
+echo "##[set-output name=build_include;]${BUILD_INCLUDE}"
 echo "##[set-output name=docker_release;]${DOCKER_RELEASE}"
 echo "##[set-output name=sha;]${SHA}"
 
