5830c973b24c6606c14f44fe3fa76a20d3456a5e
Romain Beauxis <toots@rastageeks.org>
2021-12-12 12:56:35 -0600

    Fix alpine release.

--- .github/scripts/build-apk.sh
+++ .github/scripts/build-apk.sh
@@ -7,7 +7,7 @@ DOCKER_TAG=$2
 ARCH=$3
 ALPINE_ARCH=$4
 IS_RELEASE=$5
-APK_RELEASE=r1
+APK_RELEASE=1
 
 cd /tmp/liquidsoap-full/liquidsoap
 
@@ -35,7 +35,7 @@ cp liquidsoap/.github/alpine/liquidsoap.pre-install ${APK_PACKAGE}.pre-install
 abuild-keygen -a -n
 abuild
 
-mv /home/opam/packages/tmp/${ALPINE_ARCH}/${APK_PACKAGE}-${APK_VERSION}-${APK_RELEASE}.apk /tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${ARCH}/alpine
-mv /home/opam/packages/tmp/${ALPINE_ARCH}/${APK_PACKAGE}-dbg-${APK_VERSION}-${APK_RELEASE}.apk /tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${ARCH}/alpine
+mv /home/opam/packages/tmp/${ALPINE_ARCH}/${APK_PACKAGE}-${APK_VERSION}-r${APK_RELEASE}.apk /tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${ARCH}/alpine
+mv /home/opam/packages/tmp/${ALPINE_ARCH}/${APK_PACKAGE}-dbg-${APK_VERSION}-r${APK_RELEASE}.apk /tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${ARCH}/alpine
 
-echo "##[set-output name=basename;]${APK_PACKAGE}-${APK_VERSION}-${APK_RELEASE}.apk"
+echo "##[set-output name=basename;]${APK_PACKAGE}-${APK_VERSION}-r${APK_RELEASE}.apk"
