8f461d0336e125500daa97c21113e68a3f6e507c
Romain Beauxis <toots@rastageeks.org>
2022-01-02 11:18:41 -0600

    Revert "Debug."
    This reverts commit fcc72bb1dfd7c3593e6442e36226dda8ee03ce10.

--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -27,8 +27,6 @@ export CC=""
 eval `opam config env`
 opam repository set-url default https://github.com/ocaml/opam-repository.git
 opam update
-opam info srt-windows
-opam install -y srt-windows.0.2.1
 opam remove -y ffmpeg-windows mm-windows
 opam upgrade -y --verbose `echo $OPAM_DEPS | sed -e 's#,# #g'` liquidsoap-windows
 
