af5302164994b6e99d3f037f70db2b25977efd5c
Romain Beauxis <toots@rastageeks.org>
2022-03-09 09:54:48 -0600

    Optimize.

--- src/lang/typing.ml
+++ src/lang/typing.ml
@@ -150,8 +150,10 @@ exception Unsatisfied_constraint of constr * t
 (** Check that [a] (a dereferenced type variable) does not occur in [b] and
     prepare the instantiation [a<-b] by adjusting the levels. *)
 let rec occur_check (a : var) b =
+  let constr_check (_, x) = occur_check a x in
+  let arrow_check (_, _, t) = occur_check a t in
   match b.descr with
-    | Constr c -> List.iter (fun (_, x) -> occur_check a x) c.params
+    | Constr c -> List.iter constr_check c.params
     | Tuple l -> List.iter (occur_check a) l
     | Getter t -> occur_check a t
     | List { t } -> occur_check a t
@@ -164,7 +166,7 @@ let rec occur_check (a : var) b =
         occur_check a t;
         occur_check a u
     | Arrow (p, t) ->
-        List.iter (fun (_, _, t) -> occur_check a t) p;
+        List.iter arrow_check p;
         occur_check a t
     | Ground _ -> ()
     | Var { contents = Free x } ->
