710da0053ea65ea630bd5555d7578574309d0669
Romain Beauxis <toots@rastageeks.org>
2022-03-20 15:35:47 -0400

    Prevent stopped/iddle sources from being restarted when reseting clock(s) after too much latency. Fixes: #2278

---
+++ src/test/start_stop_test.ml
@@ -0,0 +1,30 @@
+class start_stop_test _state start stop =
+  object
+    inherit Start_stop.base ~on_start:(fun () -> ()) ~on_stop:(fun () -> ())
+    method state = _state
+    method start = start ()
+    method stop = stop ()
+    method stype = Source.Fallible
+  end
+
+exception Success
+
+let () =
+  let s =
+    new start_stop_test `Idle (fun () -> assert false) (fun () -> assert false)
+  in
+  s#reset;
+  let s =
+    new start_stop_test
+      `Stopped
+      (fun () -> assert false)
+      (fun () -> assert false)
+  in
+  s#reset;
+  let s =
+    new start_stop_test `Started (fun () -> raise Success) (fun () -> ())
+  in
+  try
+    s#reset;
+    assert false
+  with Success -> ()
