453f7190ef80bf77c895e85aaeb9013df21548d9
Romain Beauxis <toots@rastageeks.org>
2022-04-27 11:28:39 -0500

    Hide runtime.mem_usage from doc.

--- src/lang/builtins_mem_usage.ml
+++ src/lang/builtins_mem_usage.ml
@@ -51,7 +51,7 @@ let () =
         ("process_physical_memory", Lang.int process_physical_memory);
       ]
   in
-  Lang.add_builtin "runtime.mem_usage" ~category:`Liquidsoap
+  Lang.add_builtin "runtime.mem_usage" ~category:`Liquidsoap ~flags:[`Hidden]
     ~descr:"Return stats about the system and process memory." [] mem_usage_t
     (fun _ -> mem_usage (Mem_usage.info ()));
   Lang.add_builtin "runtime.mem_usage.prettify_bytes" ~category:`Liquidsoap
