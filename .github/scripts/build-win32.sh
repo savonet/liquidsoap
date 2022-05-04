b707b32dc01f928f60d6a8c5efc6796f05d5ad3a
Romain Beauxis <toots@rastageeks.org>
2022-05-04 17:06:01 -0400

    Fork out rolling-release branch, adapt CI scripts. (#2379)

--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -5,17 +5,28 @@ set -e
 SYSTEM=$1
 BRANCH=$2
 CPU_CORES=$3
+IS_ROLLING_RELEASE=$4
+GITHUB_SHA=$5
+
 OPAM_PREFIX=`opam var prefix`
+VERSION=`opam show -f version . | cut -d'-' -f 1`
 PWD=`dirname $0`
 BASE_DIR=`cd "${PWD}/../.." && pwd`
+COMMIT_SHORT=`echo "${GITHUB_SHA}" | cut -c-7`
+
+if [ -n "${IS_ROLLING_RELEASE}" ]; then
+  TAG=${COMMIT_SHORT}
+else
+  TAG=${BRANCH}
+fi
 
 if [ "${SYSTEM}" = "x64" ]; then
   HOST="x86_64-w64-mingw32.static"
-  BUILD="${BRANCH}-win64"
+  BUILD="${TAG}-${VERSION}-win64"
   PKG_CONFIG_PATH="/usr/src/mxe/usr/x86_64-w64-mingw32.static/lib/pkgconfig/"
 else
   HOST="i686-w64-mingw32.static"
-  BUILD="${BRANCH}-win32"
+  BUILD="${TAG}-${VERSION}-win32"
   PKG_CONFIG_PATH="/usr/src/mxe/usr/i686-w64-mingw32.static/lib/pkgconfig/"
 fi
 
@@ -25,8 +36,8 @@ export CC=""
 
 eval `opam config env`
 opam repository set-url default https://github.com/ocaml/opam-repository.git
-opam update
-opam upgrade -y --verbose `echo $OPAM_DEPS | sed -e 's#,# #g'` liquidsoap-windows
+cd /home/opam/opam-cross-windows/
+opam upgrade -y --verbose `echo $OPAM_DEPS | sed -e 's#,# #g'` ffmpeg-windows.1.1.2 ffmpeg-avutil-windows.1.1.2 liquidsoap-windows
 
 cd ~
 cp -rf ${BASE_DIR}/.github/win32 liquidsoap-$BUILD
