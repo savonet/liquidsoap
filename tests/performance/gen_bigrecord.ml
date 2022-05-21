728d2c5a8011345997bf1f074b4458c67edcd035
Romain Beauxis <toots@rastageeks.org>
2022-05-20 23:23:38 -0500

    Convert more tests.

---
+++ tests/performance/gen_bigrecord.ml
@@ -0,0 +1,22 @@
+let () =
+  let indexes = List.init 5000 string_of_int in
+  let methods =
+    String.concat "\n"
+      (List.map (fun idx -> [%string "let r.a%{idx} = %{idx}"]) indexes)
+  in
+  let sums =
+    String.concat "\n"
+      (List.map (fun idx -> [%string "n := !n + r.a%{idx}"]) indexes)
+  in
+  Printf.printf "%s"
+    [%string
+      {|
+def sum () =
+  r = ()
+  %{methods}
+  n = ref(0)
+  %{sums}
+end
+time("sum of fields (big record)", sum)
+exit(0)
+  |}]
