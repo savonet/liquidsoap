c7996b9f581c8a530e0211bf7df1c1cca232f6ef
Romain Beauxis <toots@rastageeks.org>
2022-05-29 10:42:45 -0700

    Fix version.

--- .github/scripts/build-apk.sh
+++ .github/scripts/build-apk.sh
@@ -13,7 +13,7 @@ APK_RELEASE=0
 
 cd /tmp/liquidsoap-full/liquidsoap
 
-APK_VERSION=`opam show -f version . | cut -d'-' -f 1`
+APK_VERSION=`opam show -f version ./liquidsoap.opam | cut -d'-' -f 1`
 COMMIT_SHORT=`echo "${GITHUB_SHA}" | cut -c-7`
 
 if [ -n "${IS_ROLLING_RELEASE}" ]; then
