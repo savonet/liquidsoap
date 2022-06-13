c3ed9a1f1caad9b8b46e962848d2a908d8cab592
Romain Beauxis <toots@rastageeks.org>
2022-06-13 02:38:50 -0500

    Try this.

--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -37,6 +37,7 @@ export CC=""
 eval `opam config env`
 opam repository set-url default https://github.com/ocaml/opam-repository.git
 cd /home/opam/opam-cross-windows/
+opam remove -y ppx_tools_versioned-windows
 opam upgrade -y --verbose `echo $OPAM_DEPS | sed -e 's#,# #g'` ffmpeg-windows.1.1.2 ffmpeg-avutil-windows.1.1.2 liquidsoap-windows
 
 cd ~
