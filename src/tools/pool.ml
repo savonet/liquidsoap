8b04562b74bb5930f5bf27f4cd53a91e6c446e73
Romain Beauxis <toots@rastageeks.org>
2022-06-18 00:13:06 -0500

    Make request pool a weak hash. Fixes: #2462

--- src/tools/pool.ml
+++ src/tools/pool.ml
@@ -26,89 +26,60 @@
 module type T = sig
   (** Type of objects stored in the pool. *)
   type t
+
+  val id : t -> int
+  val destroyed : int -> t
+  val is_destroyed : t -> bool
 end
 
 module type S = sig
   type t
 
   val add : (int -> t) -> t
-  val kill : int -> float -> unit
   val find : int -> t option
   val fold : (int -> t -> 'a -> 'a) -> 'a -> 'a
   val iter : (int -> t -> unit) -> unit
   val remove : int -> unit
   val size : unit -> int
+  val clear : unit -> unit
 end
 
 module Make (P : T) : S with type t = P.t = struct
   type t = P.t
-  type entry = { death_time : float option; value : t option }
 
-  let m = Mutex.create ()
-  let h : (int, entry) Hashtbl.t = Hashtbl.create 100
+  module WeakHash = Weak.Make (struct
+    type t = P.t
 
-  let find =
-    Tutils.mutexify m (fun i ->
-        try (Hashtbl.find h i).value with Not_found -> None)
+    let equal t t' = P.id t = P.id t'
+    let hash = P.id
+  end)
 
-  let fold f x =
-    Tutils.mutexify m
-      (fun f x ->
-        Hashtbl.fold
-          (fun i entry x ->
-            match entry.value with Some t -> f i t x | None -> x)
-          h x)
-      f x
+  let h = WeakHash.create 10
 
-  let iter f =
-    Tutils.mutexify m
-      (fun f ->
-        Hashtbl.iter
-          (fun i entry ->
-            match entry.value with
-              | Some t ->
-                  Mutex.unlock m;
-                  f i t;
-                  Mutex.lock m
-              | None -> ())
-          h)
-      f
+  let find id =
+    match WeakHash.find_opt h (P.destroyed id) with
+      | Some v when not (P.is_destroyed v) -> Some v
+      | _ -> None
 
-  let remove i = Hashtbl.remove h i
-
-  let kill i grace =
-    Tutils.mutexify m
-      (fun () ->
-        Hashtbl.replace h i
-          { (Hashtbl.find h i) with death_time = Some (Unix.time () +. grace) })
-      ()
-
-  let clear () =
-    let time = Unix.time () in
-    Hashtbl.filter_map_inplace
-      (fun _ -> function
-        | { death_time = Some f } when f <= time -> None
-        | v -> Some v)
+  let fold f =
+    WeakHash.fold
+      (fun v cur -> if P.is_destroyed v then cur else f (P.id v) v cur)
       h
 
-  let next () =
-    clear ();
-    let rec find i =
-      match Hashtbl.find_opt h i with None -> i | Some _ -> find (i + 1)
-    in
-    find 0
+  let iter f =
+    WeakHash.iter
+      (fun entry -> if not (P.is_destroyed entry) then f (P.id entry) entry)
+      h
+
+  let remove id = WeakHash.remove h (P.destroyed id)
 
   let add fn =
-    Tutils.mutexify m
-      (fun () ->
-        let i = next () in
-        let v = fn i in
-        Hashtbl.replace h i { death_time = None; value = Some v };
-        v)
-      ()
+    let rec f id =
+      let v = fn id in
+      match WeakHash.merge h v with v' when v == v' -> v | _ -> f (id + 1)
+    in
+    f 0
 
-  let size =
-    Tutils.mutexify m (fun () ->
-        clear ();
-        Hashtbl.length h)
+  let size () = WeakHash.count h
+  let clear () = WeakHash.clear h
 end
