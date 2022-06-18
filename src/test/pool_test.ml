9747189396973025bb36edf7a35cd5bb2c8b81bb
Romain Beauxis <toots@rastageeks.org>
2022-06-18 16:48:47 -0500

    Make request pool a weak hash (#2463)

--- src/test/pool_test.ml
+++ src/test/pool_test.ml
@@ -1,15 +1,26 @@
-type req = int
+type req = { id : int; destroyed : bool }
 
 module Pool = Pool.Make (struct
   type t = req
+
+  let id { id } = id
+  let destroyed id = { id; destroyed = true }
+  let is_destroyed { destroyed } = destroyed
 end)
 
 let () =
   (* Create a bunch of requests. *)
-  ignore (List.init 100 (fun _ -> Pool.add (fun i -> i)));
+  let l =
+    List.init 100 (fun _ -> Pool.add (fun id -> { id; destroyed = false }))
+  in
   (* Delete 15th one. *)
-  Pool.kill 15 0.1;
-  Unix.sleepf 1.;
+  Pool.remove 15;
+  Gc.full_major ();
   assert (Pool.size () = 99);
-  let id = Pool.add (fun i -> i) in
-  assert (id = 15)
+  let r = Pool.add (fun id -> { id; destroyed = false }) in
+  assert (r.id = 15);
+  Gc.full_major ();
+  assert (List.length l = 100);
+  assert (Pool.size () = 99);
+  Gc.full_major ();
+  assert (Pool.size () = 0)
