f2ef41cf86a7ee12cfeee4b6080920b30b1131d8
Samuel Mimram <smimram@gmail.com>
2022-06-29 08:21:16 +0200

    Fix typo.

--- src/bin/build_config.ml
+++ src/bin/build_config.ml
@@ -49,7 +49,7 @@ let build_config =
    - Ffmpeg            : %{Ffmpeg_option.detected}
    - MP3               : %{Lame_option.detected}
    - MP3 (fixed-point) : %{Shine_option.detected}
-   - Opus         x     : %{Opus_option.detected}
+   - Opus              : %{Opus_option.detected}
    - Speex             : %{Speex_option.detected}
    - Theora            : %{Theora_option.detected}
    - Vorbis            : %{Vorbis_option.detected}
