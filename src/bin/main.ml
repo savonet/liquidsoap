b1e0ed3d90918c427cf8b38e7babfd1bc96adba0
Samuel Mimram <smimram@gmail.com>
2022-07-19 09:51:17 +0200

    Cosmetic.

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -26,10 +26,10 @@ module Environment = Liquidsoap_lang.Environment
 module Profiler = Liquidsoap_lang.Profiler
 
 let usage =
-  "Usage : liquidsoap [OPTION, SCRIPT or EXPR]...\n\
-  \ - SCRIPT for evaluating a liquidsoap script file;\n\
-  \ - EXPR for evaluating a scripting expression;\n\
-  \ - OPTION is one of the options listed below:\n"
+  {|Usage : liquidsoap [OPTION, SCRIPT or EXPR]...
+ - SCRIPT for evaluating a liquidsoap script file,
+ - EXPR   for evaluating a scripting expression,
+ - OPTION is one of the options listed below:|}
 
 let () =
   Configure.conf#plug "init" Dtools.Init.conf;
@@ -80,9 +80,9 @@ let deprecated = ref true
 (* Shall we start an interactive interpreter (REPL) *)
 let interactive = ref false
 
-(** [load_libs] should be called before loading a script or looking up
-  * the documentation, to make sure that pervasive libraries have been
-  * loaded, unless the user explicitly opposed to it. *)
+(** [load_libs] should be called before loading a script or looking up the
+    documentation, to make sure that pervasive libraries have been loaded,
+    unless the user explicitly opposed to it. *)
 let load_libs =
   (* Register settings module. Needs to be done last to make sure every
      dependent OCaml module has been linked. *)
@@ -91,28 +91,28 @@ let load_libs =
   fun () ->
     if !stdlib && not !loaded then (
       try
-        let save = !Configure.display_types in
+        let display_types = !Configure.display_types in
         Configure.display_types := false;
         Runtime.load_libs ~error_on_no_stdlib ~deprecated:!deprecated
           ~parse_only:!parse_only ();
         loaded := true;
-        Configure.display_types := save
+        Configure.display_types := display_types
       with Liquidsoap_lang.Runtime.Error ->
         flush_all ();
         exit 1)
 
-(** Evaluate a script or expression.
-  * This used to be done immediately, which made it possible to
-  * write things like "liquidsoap myscript.liq -h myop" and get
-  * some doc on an operator.
-  * Now, we delay each evaluation because the last item has to be treated
-  * as a non-library, ie. all defined variables should be used.
-  * By default the last item is (the only one) not treated as a library,
-  * for better diagnosis, but this can be disabled (which is useful
-  * when --checking a lib). *)
-
 let last_item_lib = ref false
 
+(** Evaluate a script or expression.
+
+    This used to be done immediately, which made it possible to write things
+    like "liquidsoap myscript.liq -h myop" and get some doc on an operator.
+
+    Now, we delay each evaluation because the last item has to be treated as a
+    non-library, ie. all defined variables should be used. By default the last
+    item is (the only one) not treated as a library, for better diagnosis, but
+    this can be disabled (which is useful when --checking a lib).
+*)
 let do_eval, eval =
   let delayed = ref None in
   let eval src ~lib =
@@ -429,28 +429,24 @@ let () =
       log#important "Using:%s" (Configure.libs_versions ());
       if Configure.git_snapshot then
         List.iter (log#important "%s")
-          [
-            "";
-            "DISCLAIMER: This version of Liquidsoap has been";
-            "compiled from a snapshot of the development code.";
-            "As such, it should not be used in production";
-            "unless you know what you are doing!";
-            "";
-            "We are, however, very interested in any feedback";
-            "about our development code and committed to fix";
-            "issues as soon as possible.";
-            "";
-            "If you are interested in collaborating to";
-            "the development of Liquidsoap, feel free to";
-            "drop us a mail at <savonet-devl@lists.sf.net>";
-            "or to join the slack chat at <http://slack.liquidsoap.info>.";
-            "";
-            "Please send any bug report or feature request";
-            "at <https://github.com/savonet/liquidsoap/issues>.";
-            "";
-            "We hope you enjoy this snapshot build of Liquidsoap!";
-            "";
-          ]);
+          ({|
+DISCLAIMER: This version of Liquidsoap has been compiled from a snapshot of the
+development code. As such, it should not be used in production unless you know
+what you are doing!
+
+We are, however, very interested in any feedback about our development code and
+committed to fix issues as soon as possible.
+
+If you are interested in collaborating to the development of Liquidsoap, feel
+free to drop us a mail at <savonet-devl@lists.sf.net> or to join the slack chat
+at <http://slack.liquidsoap.info>.
+
+Please send any bug report or feature request at
+<https://github.com/savonet/liquidsoap/issues>.
+
+We hope you enjoy this snapshot build of Liquidsoap!
+|}
+         |> String.split_on_char '\n'));
 
   Lifecycle.on_script_parse (fun () ->
       (* Parse command-line, and notably load scripts. *)
@@ -511,9 +507,8 @@ let () =
 
 (** Main procedure *)
 
-(* When the log/pid paths have their definitive values,
- * expand substitutions and check directories.
- * This should be ran just before Dtools init. *)
+(** When the log/pid paths have their definitive values, expand substitutions
+    and check directories. This should be ran just before Dtools init. *)
 let check_directories () =
   (* Now that the paths have their definitive value, expand <shortcuts>. *)
   let subst conf = conf#set (Utils.subst_vars conf#get) in
@@ -524,9 +519,9 @@ let check_directories () =
     if not (Sys.file_exists dir) then (
       let routes = Configure.conf#routes conf_path#ut in
       Printf.printf
-        "FATAL ERROR: %s directory %S does not exist.\n\
-         To change it, add the following to your script:\n\
-        \  set(%S, \"<path>\")\n"
+        {|FATAL ERROR: %s directory %S does not exist.
+To change it, add the following to your script:
+  set(%S, "<path>")|}
         kind dir
         (Dtools.Conf.string_of_path (List.hd routes));
       flush_all ();
@@ -541,24 +536,24 @@ let check_directories () =
 let () =
   Lifecycle.on_start (fun () ->
       let main () =
-        (* See http://caml.inria.fr/mantis/print_bug_page.php?bug_id=4640
-         * for this: we want Unix EPIPE error and not SIGPIPE, which
-         * crashes the program.. *)
+        (* See http://caml.inria.fr/mantis/print_bug_page.php?bug_id=4640 for
+           this: we want Unix EPIPE error and not SIGPIPE, which crashes the
+           program... *)
         if not Sys.win32 then (
           Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
           ignore (Unix.sigprocmask Unix.SIG_BLOCK [Sys.sigpipe]));
 
         (* On Windows we need to initiate shutdown ourselves by catching INT
-         * since dtools doesn't do it. *)
+           since dtools doesn't do it. *)
         if Sys.win32 then
           Sys.set_signal Sys.sigint
             (Sys.Signal_handle (fun _ -> Tutils.shutdown 0));
 
         Dtools.Init.exec Dtools.Log.start;
 
-        (* TODO if start fails (e.g. invalid password or mountpoint) it
-         *   raises an exception and dtools catches it so we don't get
-         *   a backtrace (by default at least). *)
+        (* TODO: if start fails (e.g. invalid password or mountpoint) it raises
+           an exception and dtools catches it so we don't get a backtrace (by
+           default at least). *)
         Server.start ();
         Clock.start ();
         Tutils.start ();
