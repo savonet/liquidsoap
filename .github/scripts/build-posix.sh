0a8a35dd587adb6dd42c66ffba1767540965699d
Romain Beauxis <toots@rastageeks.org>
2022-09-09 09:10:51 +0200

    Try this.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -14,7 +14,11 @@ cd /tmp/liquidsoap-full
 
 git remote set-url origin https://github.com/savonet/liquidsoap-full.git
 git fetch --recurse-submodules=no && git checkout origin/master -- Makefile.git
-make init
+
+# Remove later
+git submodule init ocaml-metadata
+git submodule update ocaml-metadata
+
 make public
 git reset --hard
 
