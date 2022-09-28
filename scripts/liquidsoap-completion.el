b6d66ed067497e039febca1d2b75f7cabfe2a1f5
Samuel Mimram <smimram@gmail.com>
2022-09-28 09:55:34 +0200

    Annotations and metadata.

--- scripts/liquidsoap-completion.el
+++ scripts/liquidsoap-completion.el
@@ -6,7 +6,20 @@
 (require 'company)
 
 (defconst liquidsoap-completions
-  '("source.blank" "sine" "playlist"))
+  '(
+    #("source.metadata" 0 1 (:type "(string) -> [string]" :description "Show metadata."))
+    #("source.duration" 0 1 (:type "(string) -> float" :description "Duration of the source."))
+    #("playlist" 0 1 (:type "(string) -> source" :description "Stream a playlist."))
+    )
+)
+
+(defun liquidsoap-annotation (s)
+  (format " : %s" (get-text-property 0 :type s))
+)
+
+(defun liquidsoap-meta (s)
+  (get-text-property 0 :description s)
+)
 
 (defun company-liquidsoap-backend (command &optional arg &rest ignored)
   (interactive (list 'interactive))
@@ -15,9 +28,13 @@
     (interactive (company-begin-backend 'company-liquidsoap-backend))
     (prefix (and (eq major-mode 'liquidsoap-mode) (company-grab-symbol)))
     (candidates
-    (cl-remove-if-not
-      (lambda (c) (string-prefix-p arg c))
-      liquidsoap-completions))))
+     (cl-remove-if-not
+       (lambda (c) (string-prefix-p arg c))
+       liquidsoap-completions))
+    (annotation (liquidsoap-annotation arg))
+    (meta (liquidsoap-meta arg))
+  )
+)
 
 (add-to-list 'company-backends 'company-liquidsoap-backend)
 
