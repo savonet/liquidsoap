4a09e4f66dfe2fc5524f72aa2c0deaeeb2e98bb8
Jonas L <jooola@users.noreply.github.com>
2022-09-29 21:04:39 +0200

    Don't upload artifacts to s3 on pull_quests (#2656)

--- .github/scripts/build-details.sh
+++ .github/scripts/build-details.sh
@@ -10,7 +10,15 @@ fi
 
 echo "Detected branch: ${BRANCH}"
 
-if [[ "${BRANCH}" =~ "rolling-release-" ]] || [[ "${BRANCH}" =~ ^v[0-9] ]]; then
+if [ -n "${IS_FORK}" ]; then
+  echo "Branch is from a fork"
+  IS_FORK=true
+else
+  echo "Branch is from the savonet organization"
+  IS_FORK=
+fi
+
+if [ "${IS_FORK}" != "true" ] && [[ "${BRANCH}" =~ "rolling-release-" ]] || [[ "${BRANCH}" =~ ^v[0-9] ]]; then
   echo "Branch is release branch"
   IS_RELEASE=true
 
@@ -56,3 +64,4 @@ echo "##[set-output name=docker_release;]${DOCKER_RELEASE}"
 echo "##[set-output name=is_rolling_release;]${IS_ROLLING_RELEASE}"
 echo "##[set-output name=sha;]${SHA}"
 echo "##[set-output name=s3-artifact-basepath;]s3://liquidsoap-artifacts/${GITHUB_WORKFLOW}/${GITHUB_RUN_NUMBER}"
+echo "##[set-output name=is_fork;]${IS_FORK}"
