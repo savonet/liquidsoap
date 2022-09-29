a79d5820673ab7c485e12f965070fc65da256933
Samuel Mimram <smimram@gmail.com>
2022-09-29 08:50:36 +0200

    Generate completion for defined functions too.

--- scripts/gen_emacs_completion.ml
+++ scripts/gen_emacs_completion.ml
@@ -3,5 +3,6 @@ open Liquidsoap_runtime
 let () =
   Main.run_streams := false;
   Lifecycle.after_script_parse (fun () ->
+      Main.load_libs ();
       Lang_string.kprint_string ~pager:false Doc.Value.print_emacs_completions);
   Runner.run ()
