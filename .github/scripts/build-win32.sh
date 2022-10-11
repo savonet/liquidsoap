ceb04be63c32049cd4bc509289eee497b85ad048
Romain Beauxis <toots@rastageeks.org>
2022-10-10 19:07:11 -0500

    Hopefully fix windows build.

--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -63,7 +63,7 @@ cd ~
 cp -rf "${BASE_DIR}/.github/win32" "liquidsoap-$BUILD"
 cd "liquidsoap-$BUILD"
 cp "${OPAM_PREFIX}"/windows-sysroot/bin/liquidsoap ./liquidsoap.exe
-cp "${OPAM_PREFIX}"/windows-sysroot/share/liquidsoap-lang/libs/*.liq libs
+cp src/libs/*.liq libs
 cp -rf "$(ocamlfind -toolchain windows ocamlc -where)/../../share/camomile" .
 cd ..
 zip -r "liquidsoap-$BUILD.zip" "liquidsoap-$BUILD"
