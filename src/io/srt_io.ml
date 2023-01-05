cf66c0cb0e956c6d3f94e15d3c79ad9f157445b1
Romain Beauxis <toots@rastageeks.org>
2023-01-05 10:31:17 +0100

    Make srt not be self-sync.

--- src/io/srt_io.ml
+++ src/io/srt_io.ml
@@ -861,7 +861,7 @@ class virtual input_base ~kind ~max ~log_overfull ~clock_safe ~on_connect
     method is_ready =
       super#is_ready && (not self#should_stop) && self#is_connected
 
-    method self_sync = (`Dynamic, self#is_connected)
+    method self_sync = (`Static, false)
 
     method private create_decoder socket =
       let create_decoder =
