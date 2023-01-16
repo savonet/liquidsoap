bcef57ab2b9fbae8b80bfc8cb429f669db401d41
Romain Beauxis <toots@rastageeks.org>
2023-01-16 16:04:52 -0600

    Adapt srt to new API.

--- src/io/srt_io.ml
+++ src/io/srt_io.ml
@@ -491,7 +491,6 @@ module Poll = struct
   type t = {
     p : Srt.Poll.t;
     m : Mutex.t;
-    mutable max_fds : int;
     handlers : (Srt.socket, Srt.Poll.flag * (Srt.socket -> unit)) Hashtbl.t;
   }
 
@@ -499,13 +498,12 @@ module Poll = struct
     let p = Srt.Poll.create () in
     let m = Mutex.create () in
     let handlers = Hashtbl.create 0 in
-    { p; m; max_fds = 0; handlers }
+    { p; m; handlers }
 
   let process () =
     try
-      let max_fds = Tutils.mutexify t.m (fun () -> t.max_fds) () in
-      if max_fds = 0 then raise Empty;
-      let events = Srt.Poll.uwait t.p ~max_fds ~timeout:conf_timeout#get in
+      if Srt.Poll.sockets t.p = [] then raise Empty;
+      let read, write = Srt.Poll.wait t.p ~timeout:conf_timeout#get in
       Tutils.mutexify t.m
         (fun () ->
           let apply fn s =
@@ -518,22 +516,19 @@ module Poll = struct
                    (Printexc.to_string exn))
           in
           List.iter
-            (fun { Srt.Poll.fd; events } ->
-              Srt.Poll.remove_usock t.p fd;
-              t.max_fds <- t.max_fds - 1;
-              Srt.setsockflag fd Srt.sndsyn true;
-              Srt.setsockflag fd Srt.rcvsyn true;
-              let event, fn = Hashtbl.find t.handlers fd in
-              if List.mem event events then apply fn fd)
-            events)
+            (fun (event, sockets) ->
+              List.iter
+                (fun fd ->
+                  Srt.Poll.remove_usock t.p fd;
+                  Srt.setsockflag fd Srt.sndsyn true;
+                  Srt.setsockflag fd Srt.rcvsyn true;
+                  let event', fn = Hashtbl.find t.handlers fd in
+                  if event = event' then apply fn fd)
+                sockets)
+            [(`Read, read); (`Write, write)])
         ();
       0.
     with
-      (* We are seeing this error while tracking [max_fds]. This could be due to the
-         fact that the poll could remove closed sockets without us seeing it. *)
-      | Srt.Error (`Epollempty, _) ->
-          Tutils.mutexify t.m (fun () -> t.max_fds <- 0) ();
-          -1.
       | Empty -> -1.
       | Srt.Error (`Etimeout, _) -> 0.
       | exn ->
@@ -551,8 +546,7 @@ module Poll = struct
     Tutils.mutexify t.m
       (fun () ->
         Hashtbl.add t.handlers socket (mode, fn);
-        Srt.Poll.add_usock t.p socket ~flags:[(mode :> Srt.Poll.flag)];
-        t.max_fds <- t.max_fds + 1)
+        Srt.Poll.add_usock t.p socket ~flags:[(mode :> Srt.Poll.flag)])
       ();
     Duppy.Async.wake_up task
 end
