2d549da2146e2b48f5384ea08ef670d8b354d120
Romain Beauxis <toots@rastageeks.org>
2023-01-23 23:42:25 -0600

    Workaround.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -13,6 +13,12 @@ mkdir -p ~/.docker
 echo "{ \"experimental\": \"enabled\" }" > ~/.docker/config.json
 
 docker login -u "$USER" -p "$PASSWORD"
+docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
+
+# Something is odd with the docker repo
+docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64
+docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 savonet/liquidsoap-ci-build:${TAG}_amd64
+docker push savonet/liquidsoap-ci-build:${TAG}_amd64
 
 docker manifest create savonet/liquidsoap:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_arm64 savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push savonet/liquidsoap:$TAG
@@ -20,7 +26,7 @@ docker manifest push savonet/liquidsoap:$TAG
 docker manifest create savonet/liquidsoap-alpine:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_alpine_arm64 savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
 docker manifest push savonet/liquidsoap-alpine:$TAG
 
-docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
+#docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
 docker manifest create ghcr.io/savonet/liquidsoap:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64 ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push ghcr.io/savonet/liquidsoap:$TAG
