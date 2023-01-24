2f7c1fd386c4bbbf1680470ac1f282bfea2e1daf
Romain Beauxis <toots@rastageeks.org>
2023-01-24 10:51:13 -0600

    Try this.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -20,6 +20,14 @@ docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64
 docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 savonet/liquidsoap-ci-build:${TAG}_amd64
 docker push savonet/liquidsoap-ci-build:${TAG}_amd64
 
+docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64
+docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64 savonet/liquidsoap-ci-build:${TAG}_arm64
+docker push savonet/liquidsoap-ci-build:${TAG}_arm64
+
+docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf
+docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf savonet/liquidsoap-ci-build:${TAG}_armhf
+docker push savonet/liquidsoap-ci-build:${TAG}_armhf
+
 docker manifest create savonet/liquidsoap:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_arm64 savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push savonet/liquidsoap:$TAG
 
