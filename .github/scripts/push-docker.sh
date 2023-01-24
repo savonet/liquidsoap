b6ca5fe6302b24325a43e8f2f589996e46662446
Romain Beauxis <toots@rastageeks.org>
2023-01-24 11:01:37 -0600

    Let's do it for all images.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -13,9 +13,9 @@ mkdir -p ~/.docker
 echo "{ \"experimental\": \"enabled\" }" > ~/.docker/config.json
 
 docker login -u "$USER" -p "$PASSWORD"
-docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
 # Something is odd with the docker repo
+## REMOVE WHEN FIXED ##
 docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64
 docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 savonet/liquidsoap-ci-build:${TAG}_amd64
 docker push savonet/liquidsoap-ci-build:${TAG}_amd64
@@ -28,13 +28,26 @@ docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf
 docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf savonet/liquidsoap-ci-build:${TAG}_armhf
 docker push savonet/liquidsoap-ci-build:${TAG}_armhf
 
+docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64
+docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 savonet/liquidsoap-ci-build:${TAG}_alpine_amd64
+docker push savonet/liquidsoap-ci-build:${TAG}_alpine_amd64
+
+docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64
+docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64 savonet/liquidsoap-ci-build:${TAG}_alpine_arm64
+docker push savonet/liquidsoap-ci-build:${TAG}_alpine_arm64
+
+docker pull ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
+docker tag ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_armhf savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
+docker push savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
+## REMOVE WHEN FIXED ##
+
 docker manifest create savonet/liquidsoap:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_arm64 savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push savonet/liquidsoap:$TAG
 
 docker manifest create savonet/liquidsoap-alpine:$TAG --amend savonet/liquidsoap-ci-build:${TAG}_alpine_amd64 --amend savonet/liquidsoap-ci-build:${TAG}_alpine_arm64 savonet/liquidsoap-ci-build:${TAG}_alpine_armhf
 docker manifest push savonet/liquidsoap-alpine:$TAG
 
-#docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
+docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
 docker manifest create ghcr.io/savonet/liquidsoap:$TAG --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64 --amend ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64 ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf
 docker manifest push ghcr.io/savonet/liquidsoap:$TAG
