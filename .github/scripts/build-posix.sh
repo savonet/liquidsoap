da2cb4526fc29381c91a2d0eb9248658edec6ad6
Romain Beauxis <toots@rastageeks.org>
2023-02-07 13:33:39 +0100

    Install git version of inotify.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -35,7 +35,8 @@ if [ -z "${LIQ_BUILD_MIN}" ]; then
   git clone https://github.com/savonet/ocaml-posix.git
   cd ocaml-posix && opam install -y . && cd ..
   # See: https://github.com/whitequark/ocaml-inotify/pull/20
-  opam install -y uri yaml inotify.2.3
+  git clone https://github.com/whitequark/ocaml-inotify.git
+  cd ocaml-inotify  opam install -y . && cd ..
 fi
 
 # TODO: Add those to docker CI images.
