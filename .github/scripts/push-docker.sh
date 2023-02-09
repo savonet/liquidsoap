916cdfa71093393b29315d34d5ac65b7b8317911
Romain Beauxis <toots@rastageeks.org>
2023-02-09 08:04:09 -0600

    Use short commit for docker tag.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -7,11 +7,13 @@ USER=$2
 PASSWORD=$3
 GHCR_USER=$4
 GHCR_PASSWORD=$5
-SHA=$6
+GITHUB_SHA=$6
 
 rm -rf ~/.docker/config.json
 mkdir -p ~/.docker
 echo "{ \"experimental\": \"enabled\" }" > ~/.docker/config.json
+
+COMMIT_SHORT=$(echo "${GITHUB_SHA}" | cut -c-7)
 
 docker login -u "$USER" -p "$PASSWORD"
 
@@ -47,25 +49,25 @@ docker push "savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
 docker manifest create "savonet/liquidsoap:${TAG}" --amend "savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_arm64" "savonet/liquidsoap-ci-build:${TAG}_armhf"
 docker manifest push "savonet/liquidsoap:${TAG}"
 
-docker manifest create "savonet/liquidsoap:${SHA}" --amend "savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_arm64" "savonet/liquidsoap-ci-build:${TAG}_armhf"
-docker manifest push "savonet/liquidsoap:${SHA}"
+docker manifest create "savonet/liquidsoap:${COMMIT_SHORT}" --amend "savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_arm64" "savonet/liquidsoap-ci-build:${TAG}_armhf"
+docker manifest push "savonet/liquidsoap:${COMMIT_SHORT}"
 
 docker manifest create "savonet/liquidsoap-alpine:${TAG}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" "savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
 docker manifest push "savonet/liquidsoap-alpine:${TAG}"
 
-docker manifest create "savonet/liquidsoap-alpine:${SHA}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" "savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
-docker manifest push "savonet/liquidsoap-alpine:${SHA}"
+docker manifest create "savonet/liquidsoap-alpine:${COMMIT_SHORT}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" "savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
+docker manifest push "savonet/liquidsoap-alpine:${COMMIT_SHORT}"
 
 docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
 docker manifest create "ghcr.io/savonet/liquidsoap:${TAG}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64" "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf"
 docker manifest push "ghcr.io/savonet/liquidsoap:${TAG}"
 
-docker manifest create "ghcr.io/savonet/liquidsoap:${SHA}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64" "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf"
-docker manifest push "ghcr.io/savonet/liquidsoap:${SHA}"
+docker manifest create "ghcr.io/savonet/liquidsoap:${COMMIT_SHORT}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64" "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_armhf"
+docker manifest push "ghcr.io/savonet/liquidsoap:${COMMIT_SHORT}"
 
 docker manifest create "ghcr.io/savonet/liquidsoap-alpine:${TAG}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
 docker manifest push "ghcr.io/savonet/liquidsoap-alpine:${TAG}"
 
-docker manifest create "ghcr.io/savonet/liquidsoap-alpine:${SHA}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
-docker manifest push "ghcr.io/savonet/liquidsoap-alpine:${SHA}"
+docker manifest create "ghcr.io/savonet/liquidsoap-alpine:${COMMIT_SHORT}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_armhf"
+docker manifest push "ghcr.io/savonet/liquidsoap-alpine:${COMMIT_SHORT}"
