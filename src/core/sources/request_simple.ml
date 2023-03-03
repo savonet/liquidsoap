f74bc750b2895c1d19f46ea76a894decdf8222d9
Romain Beauxis <toots@rastageeks.org>
2023-03-02 23:11:51 -0600

    Fix available eval in request.dynamic. (#2938)

--- src/core/sources/request_simple.ml
+++ src/core/sources/request_simple.ml
@@ -157,21 +157,21 @@ class dynamic ~retry_delay ~available (f : Lang.value) prefetch timeout =
         retry_status <- Some (Unix.gettimeofday () +. delay);
         `Retry (fun () -> delay)
       in
-      match
-        ( available (),
-          Lang.to_valued_option Request.Value.of_value (Lang.apply f []) )
-      with
-        | false, _ -> `Empty
-        | true, Some r ->
-            Request.set_root_metadata r "source" self#id;
-            `Request r
-        | true, None -> retry ()
-        | exception exn ->
-            let bt = Printexc.get_backtrace () in
-            Utils.log_exception ~log:self#log ~bt
-              (Printf.sprintf "Failed to obtain a media request: %s"
-                 (Printexc.to_string exn));
-            retry ()
+      if available () then (
+        match
+          Lang.to_valued_option Request.Value.of_value (Lang.apply f [])
+        with
+          | Some r ->
+              Request.set_root_metadata r "source" self#id;
+              `Request r
+          | None -> retry ()
+          | exception exn ->
+              let bt = Printexc.get_backtrace () in
+              Utils.log_exception ~log:self#log ~bt
+                (Printf.sprintf "Failed to obtain a media request: %s"
+                   (Printexc.to_string exn));
+              retry ())
+      else `Empty
   end
 
 let _ =
