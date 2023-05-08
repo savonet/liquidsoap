9f99d7bc22e0870c13881f3f23d97d2c5e25eee0
Samuel Mimram <smimram@gmail.com>
2023-05-08 21:28:26 +0200

    Headers and doc.

--- src/core/tools/syncLazy.ml
+++ src/core/tools/syncLazy.ml
@@ -1,3 +1,31 @@
+(*****************************************************************************
+
+  Liquidsoap, a programmable audio stream generator.
+  Copyright 2003-2023 Savonet team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details, fully stated in the COPYING
+  file at the root of the liquidsoap distribution.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+ *****************************************************************************)
+
+(** The Lazy module is not currently compatible with multithreading, see
+    https://github.com/ocaml-multicore/ocaml-multicore/issues/750 For now, we
+    mutexify all accesses by using this module. The interface is mostly the same
+    as the one as the Lazy module.
+*)
+
 type 'a t = 'a Lazy.t * Mutex.t
 
 let[@inline] from_lazy v : 'a t = (v, Mutex.create ())
@@ -9,10 +37,6 @@ let force ((v, m) : 'a t) =
   Fun.protect ~finally:(fun () -> Mutex.unlock m) @@ fun () -> Lazy.force v
 
 let to_fun v () = force v
-
-let map f ((v, m) : 'a t) =
-  Mutex.lock m;
-  Fun.protect ~finally:(fun () -> Mutex.unlock m) @@ fun () -> (Lazy.map f v, m)
 
 let is_val ((v, m) : 'a t) =
   Mutex.lock m;
--- src/core/tools/syncLazy.mli
+++ src/core/tools/syncLazy.mli
@@ -1,3 +1,25 @@
+(*****************************************************************************
+
+  Liquidsoap, a programmable audio stream generator.
+  Copyright 2003-2023 Savonet team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details, fully stated in the COPYING
+  file at the root of the liquidsoap distribution.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+ *****************************************************************************)
+
 type 'a t
 
 val from_lazy : 'a Lazy.t -> 'a t
@@ -5,5 +27,4 @@ val from_val : 'a -> 'a t
 val from_fun : (unit -> 'a) -> 'a t
 val force : 'a t -> 'a
 val to_fun : 'a t -> unit -> 'a
-val map : ('a -> 'b) -> 'a t -> 'b Lazy.t * Mutex.t
 val is_val : 'a t -> bool
