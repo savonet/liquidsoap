751a2ce352edfc34a5e0a60fb7fe01cb5da1dfa3
Samuel Mimram <smimram@gmail.com>
2023-05-09 14:39:15 +0200

    Style.

--- src/core/tools/syncLazy.ml
+++ src/core/tools/syncLazy.ml
@@ -32,12 +32,10 @@ let[@inline] from_lazy v : 'a t = (v, Mutex.create ())
 let[@inline] from_val v : 'a t = from_lazy (Lazy.from_val v)
 let[@inline] from_fun f : 'a t = from_lazy (Lazy.from_fun f)
 
-let force ((v, m) : 'a t) =
+let map f ((v, m) : 'a t) =
   Mutex.lock m;
-  Fun.protect ~finally:(fun () -> Mutex.unlock m) @@ fun () -> Lazy.force v
+  Fun.protect ~finally:(fun () -> Mutex.unlock m) (fun () -> f v)
 
+let force v = map Lazy.force v
 let to_fun v () = force v
-
-let is_val ((v, m) : 'a t) =
-  Mutex.lock m;
-  Fun.protect ~finally:(fun () -> Mutex.unlock m) @@ fun () -> Lazy.is_val v
+let is_val v = map Lazy.is_val v
