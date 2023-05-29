c255fda49dc1c7833e3c35540477e85f077467d7
Romain Beauxis <toots@rastageeks.org>
2023-05-29 11:02:58 -0500

    Revert.

--- src/core/stream/content_audio.ml
+++ src/core/stream/content_audio.ml
@@ -50,8 +50,7 @@ module Specs = struct
 
   let blit src src_pos dst dst_pos len =
     let ( ! ) = audio_of_main in
-    let len = !(src_pos + len) - !src_pos in
-    Audio.blit src !src_pos dst !dst_pos len
+    Audio.blit src !src_pos dst !dst_pos !len
 
   let copy d = Audio.copy d 0 (Audio.length d)
 
