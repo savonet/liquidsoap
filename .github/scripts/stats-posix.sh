e6b34fc7a027bdc39fa9a5c650dec410dea0fa4b
Romain Beauxis <toots@rastageeks.org>
2023-07-06 14:55:24 -0500

    Revert "Add stats after cleanup."
    This reverts commit 87c31d7a20bc421d971113988ede2687b4a6afdf.

--- .github/scripts/stats-posix.sh
+++ .github/scripts/stats-posix.sh
@@ -8,22 +8,10 @@ OCAMLPATH="$(cat ../.ocamlpath)"
 export OCAMLPATH
 
 printf "Memory usage before loading all libraries: "
-dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --check 'runtime.gc.full_major() print(runtime.mem_usage.prettify_bytes(runtime.mem_usage().process_physical_memory))'
+dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --check 'print(runtime.mem_usage.prettify_bytes(runtime.mem_usage().process_physical_memory))'
 
 printf "Memory usage after loading all libraries: "
-dune exec --display=quiet -- src/bin/liquidsoap.exe --check 'runtime.gc.full_major() print(runtime.memory().pretty.process_physical_memory)'
-
-printf "Memory usage after starting the application: "
-dune exec --display=quiet -- src/bin/liquidsoap.exe '
-  settings.log.stdout.set(false)
-  thread.run(
-    delay=3.,
-    (fun () -> begin
-      runtime.gc.full_major()
-      print(runtime.memory().pretty.process_physical_memory)
-      shutdown()
-    end))
-  output.dummy(blank())'
+dune exec --display=quiet -- src/bin/liquidsoap.exe --check 'print(runtime.memory().pretty.process_physical_memory)'
 
 printf "Number of core functions: "
 dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --list-functions | wc -l
