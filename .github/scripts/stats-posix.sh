072eb87ab3ea499c7c891537c9ce2f931db74503
Romain Beauxis <toots@rastageeks.org>
2023-07-06 15:20:08 -0500

    Safer.

--- .github/scripts/stats-posix.sh
+++ .github/scripts/stats-posix.sh
@@ -8,10 +8,22 @@ OCAMLPATH="$(cat ../.ocamlpath)"
 export OCAMLPATH
 
 printf "Memory usage before loading all libraries: "
-dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --check 'print(runtime.mem_usage.prettify_bytes(runtime.mem_usage().process_physical_memory))'
+dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --check 'runtime.gc.full_major() print(runtime.mem_usage.prettify_bytes(runtime.mem_usage().process_physical_memory))'
 
 printf "Memory usage after loading all libraries: "
-dune exec --display=quiet -- src/bin/liquidsoap.exe --check 'print(runtime.memory().pretty.process_physical_memory)'
+dune exec --display=quiet -- src/bin/liquidsoap.exe --check 'runtime.gc.full_major() print(runtime.memory().pretty.process_physical_memory)'
+
+printf "Memory usage after starting the application: "
+dune exec --display=quiet -- src/bin/liquidsoap.exe '
+  settings.log.stdout.set(false)
+  thread.run(
+    delay=3.,
+    (fun () -> begin
+      runtime.gc.full_major()
+      print(runtime.memory().pretty.process_physical_memory)
+      exit(0)
+    end))
+  output.dummy(blank())'
 
 printf "Number of core functions: "
 dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --list-functions | wc -l
