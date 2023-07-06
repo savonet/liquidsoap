8c4b93da443c911340aad69ddaf5db6d1ad3a779
Romain Beauxis <toots@rastageeks.org>
2023-07-06 16:26:36 -0500

    Remove for now.

--- .github/scripts/stats-posix.sh
+++ .github/scripts/stats-posix.sh
@@ -13,18 +13,6 @@ dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --check 'runtime
 printf "Memory usage after loading all libraries: "
 dune exec --display=quiet -- src/bin/liquidsoap.exe --check 'runtime.gc.full_major() print(runtime.memory().pretty.process_physical_memory)'
 
-printf "Memory usage after starting the application: "
-dune exec --display=quiet -- src/bin/liquidsoap.exe '
-  settings.log.stdout.set(false)
-  thread.run(
-    delay=3.,
-    (fun () -> begin
-      runtime.gc.full_major()
-      print(runtime.memory().pretty.process_physical_memory)
-      exit(0)
-    end))
-  output.dummy(blank())'
-
 printf "Number of core functions: "
 dune exec --display=quiet -- src/bin/liquidsoap.exe --no-stdlib --list-functions | wc -l
 echo
