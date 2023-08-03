1e64a84c67fcccd1fc27cde88d4a7436ed2779b6
Samuel Mimram <smimram@gmail.com>
2023-08-03 13:25:21 +0200

    Name metrics according to time.

--- .github/scripts/export-metrics.sh
+++ .github/scripts/export-metrics.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 METRICS_DIR=$1
-METRICS_FILE="${METRICS_DIR}/metrics.yaml"
+TIME="$(date +%s)"
+METRICS_FILE="${METRICS_DIR}/${TIME}.yaml"
 
 git config --global --add safe.directory '*'
 cd /tmp/liquidsoap-full/liquidsoap || exit 1
 mkdir -p "${METRICS_DIR}"
+touch /tmp/metrics.yaml
+cat /tmp/metrics.yaml > "${METRICS_FILE}"
 {
   echo "- commit: $(git rev-parse HEAD)"
   echo "  branch: $(git rev-parse --abbrev-ref HEAD)"
-  echo "  time: $(date +%s)"
+  echo "  time: ${TIME}"
 } >> "${METRICS_FILE}"
