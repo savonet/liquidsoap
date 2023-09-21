1c36c89dc8d006f912a41713b9647d4e9cbaf01e
Romain Beauxis <toots@rastageeks.org>
2023-09-21 10:05:47 +0200

    Fix TS CI

--- src/libs/gen_dune.ml
+++ src/libs/gen_dune.ml
@@ -5,13 +5,13 @@ let mk_prettify_rule ~location ~path f =
   (alias fmt)
   (deps
    %s
-   %s/../tooling/liq-prettier
+   %s/../tooling/test-prettier
    %s/../tooling/prettier-plugin-liquidsoap/dist/liquidsoap.js
  )
   (action
     (progn
       (with-stdout-to %s.prettier
-       (chdir %s/../tooling/liq-prettier
+       (chdir %s/../tooling/test-prettier
          (run pnpm prettier --config ./config.json ../../libs/%s/%s)))
       (diff %s %s.prettier))))
 |}
@@ -21,16 +21,16 @@ let mk_tree_sitter_rule ~location ~path f =
   Printf.printf
     {|
 (rule
-  (alias tree-sitter-test)
+  (alias tree-sitter-parse)
   (deps
    %s
    (source_tree %s/../tooling/tree-sitter-liquidsoap)
-   %s/../tooling/tree-sitter-liquidsoap/node_modules
+   %s/../tooling/test-tree-sitter
  )
   (action
-    (chdir %s/../tooling/tree-sitter-liquidsoap
+    (chdir %s/../tooling/test-tree-sitter/tree-sitter-liquidsoap
      (ignore-stdout
-      (run npm exec tree-sitter -- parse ../../libs/%s/%s)))))
+      (run npm exec tree-sitter -- parse ../../../libs/%s/%s)))))
 |}
     f location location location path f
 
