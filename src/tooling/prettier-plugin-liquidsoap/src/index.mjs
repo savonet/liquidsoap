5c18a440ad7ef6349e59d78895fb69a9705c4084
Romain Beauxis <toots@rastageeks.org>
2023-10-10 13:17:30 -0500

    Revert to simple require.

--- src/tooling/prettier-plugin-liquidsoap/src/index.mjs
+++ src/tooling/prettier-plugin-liquidsoap/src/index.mjs
@@ -1,9 +1,5 @@
 import { parsers as baseParsers } from "./base.mjs";
-import { createRequire } from "node:module";
-
 export { languages, printers } from "./base.mjs";
-
-const require = createRequire(import.meta.url);
 
 const liquidsoap = require("../dist/liquidsoap");
 
