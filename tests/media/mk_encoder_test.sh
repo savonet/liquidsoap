2b67cb3a31f6ad8a6b5ff49d26f7425c3410cfc0
Romain Beauxis <toots@rastageeks.org>
2023-10-12 08:20:42 -0500

    Backport new media tests.

--- tests/media/mk_encoder_test.sh
+++ tests/media/mk_encoder_test.sh
@@ -4,6 +4,5 @@ FORMAT="$1"
 SOURCE="$2"
 FILE="$3"
 
-sed -e "s#@FORMAT@#${FORMAT}#g" "encoder.liq.in" |
-  sed -e "s#@SOURCE@#${SOURCE}#g" |
+sed -e "s#@FORMAT@#${FORMAT}#g" "encoder_${SOURCE}.liq.in" |
   sed -e "s#@FILE@#${FILE}#g"
