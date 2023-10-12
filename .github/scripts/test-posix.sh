2c4a3dad2b19bcecccd675419699a6a1b268cf17
Romain Beauxis <toots@rastageeks.org>
2023-10-12 14:09:04 -0500

    Try that.

--- .github/scripts/test-posix.sh
+++ .github/scripts/test-posix.sh
@@ -4,7 +4,7 @@ set -e
 
 TARGET=$1
 
-export OPAMJOBS="$CPU_CORES"
+#export OPAMJOBS="$CPU_CORES"
 
 cd /tmp/liquidsoap-full/liquidsoap
 eval "$(opam config env)"
@@ -13,4 +13,4 @@ export OCAMLPATH
 
 export CLICOLOR_FORCE=1
 
-dune build "${TARGET}" --error-reporting=twice --display=quiet
+dune build -j 4 "${TARGET}" --error-reporting=twice --display=quiet
