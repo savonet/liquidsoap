ec05a04e54359e4aab3005849d6d4362ddf46d73
Romain Beauxis <toots@rastageeks.org>
2023-12-08 11:39:35 -0600

    Add this.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -31,6 +31,8 @@ cd /tmp/liquidsoap-full/liquidsoap
 
 ./.github/scripts/checkout-deps.sh
 
+opam install -y saturn_lockfree
+
 cd /tmp/liquidsoap-full
 
 sed -e 's@ocaml-gstreamer@#ocaml-gstreamer@' -i PACKAGES
