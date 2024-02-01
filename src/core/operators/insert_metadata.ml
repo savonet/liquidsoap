3930cbb4c4fa5690b1790781a80459ba7ad91d11
Romain Beauxis <toots@rastageeks.org>
2024-02-01 08:49:23 -0600

    Revert "Make sure that last_metadata is the replayed meta in replay_meta source"
    This reverts commit 5c00974a823bc2c93b8df15f0c2c71e81bad12b7.
    
    Refs: AzuraCast/AzuraCast#6905

--- src/core/operators/insert_metadata.ml
+++ src/core/operators/insert_metadata.ml
@@ -118,7 +118,7 @@ let _ =
    switch classes. *)
 class replay meta src =
   object (self)
-    inherit operator ~name:"replay_metadata" [src] as super
+    inherit operator ~name:"replay_metadata" [src]
     val mutable first = true
     method stype = src#stype
     method private _is_ready = src#is_ready
@@ -126,7 +126,6 @@ class replay meta src =
     method remaining = src#remaining
     method self_sync = src#self_sync
     method seek_source = src#seek_source
-    method! last_metadata = if first then Some meta else super#last_metadata
 
     method private get_frame ab =
       let start = Frame.position ab in
