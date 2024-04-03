05e992a2535af8d8ea85bd65e63a04e61858d4b8
Romain Beauxis <toots@rastageeks.org>
2024-04-03 14:49:05 -0500

    Build with leak sanitizer.

--- .github/scripts/build-details.sh
+++ .github/scripts/build-details.sh
@@ -17,31 +17,16 @@ if [ "${IS_FORK}" == "true" ]; then
   IS_FORK=true
 fi
 
-if [[ "${IS_FORK}" != "true" && ("${BRANCH}" =~ ^rolling-release\-v[0-9]\.[0-9]\.x || "${BRANCH}" =~ ^v[0-9]\.[0-9]\.[0-9]) ]]; then
-  echo "Branch is release branch"
-  IS_RELEASE=true
+echo "Branch is not release branch"
+IS_RELEASE=
 
-  echo "Building on all architectures"
-  BUILD_OS='["debian_trixie", "debian_bookworm", "debian_bullseye", "ubuntu_jammy", "ubuntu_lunar", "alpine"]'
-  BUILD_PLATFORM='["amd64", "arm64", "armhf"]'
-  #BUILD_PLATFORM='["amd64", "arm64"]'
-  BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64", "docker-debian-os": "trixie"}, {"platform": "arm64", "runs-on": ["self-hosted", "build"], "alpine-arch": "aarch64", "docker-platform": "linux/arm64", "docker-debian-os": "trixie"}, {"platform": "armhf", "runs-on": ["self-hosted", "build"], "alpine-arch": "armv7", "docker-platform": "linux/arm/v7", "docker-debian-os": "bullseye"}]'
-  #BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64"}, {"platform": "arm64", "runs-on": ["self-hosted", "build"], "alpine-arch": "aarch64", "docker-platform": "linux/arm64"}]'
+echo "Building on amd64 only"
+BUILD_OS='["debian_bookworm"]'
+BUILD_PLATFORM='["amd64"]'
+BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64", "docker-debian-os": "trixie"}]'
 
-  echo "Branch has a docker release"
-  DOCKER_RELEASE=true
-else
-  echo "Branch is not release branch"
-  IS_RELEASE=
-
-  echo "Building on amd64 only"
-  BUILD_OS='["debian_trixie", "debian_bookworm", "ubuntu_jammy", "ubuntu_lunar", "alpine"]'
-  BUILD_PLATFORM='["amd64"]'
-  BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64", "docker-debian-os": "trixie"}]'
-
-  echo "Branch does not have a docker release"
-  DOCKER_RELEASE=
-fi
+echo "Branch does not have a docker release"
+DOCKER_RELEASE=
 
 SHA=$(git rev-parse --short HEAD)
 
