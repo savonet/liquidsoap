e35a0e4be5a10eb242223ec546f0a1d79d68a751
Romain Beauxis <toots@rastageeks.org>
2024-05-01 08:14:06 -0500

    Fix JS runtime.

---
+++ src/js/runtime.js
@@ -0,0 +1,17 @@
+//Provides: caml_thread_initialize
+function caml_thread_initialize() {};
+
+//Provides: caml_mutex_new
+function caml_mutex_new() {}
+
+//Provides: caml_thread_self
+function caml_thread_self() {}
+
+//Provides: caml_thread_id
+function caml_thread_id() {}
+
+//Provides: caml_mutex_lock
+function caml_mutex_lock() {}
+
+//Provides: caml_mutex_unlock
+function caml_mutex_unlock() {}
