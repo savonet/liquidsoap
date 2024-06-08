b0fb8d87a1fbd8bc48fd764f60e3f5005ba91b66
Romain Beauxis <toots@rastageeks.org>
2024-06-07 19:01:36 -0500

    Try this.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -85,7 +85,7 @@ fi
 
 echo "::group::Print build config"
 
-dune exec -- liquidsoap --build-config
+./liquidsoap --build-config
 
 echo "::endgroup::"
 
@@ -93,7 +93,7 @@ echo "::group::Basic tests"
 
 cd /tmp/liquidsoap-full/liquidsoap
 
-dune exec -- liquidsoap --version
-dune exec -- liquidsoap --check 'print("hello world")'
+./liquidsoap --version
+./liquidsoap --check 'print("hello world")'
 
 echo "::endgroup::"
