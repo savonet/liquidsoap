7de7d8a9c4d03d0c21c71c8238755285c86da720
Romain Beauxis <toots@rastageeks.org>
2024-06-10 17:39:40 +0200

    More.

--- tests/regression/gen_dune.ml
+++ tests/regression/gen_dune.ml
@@ -25,25 +25,4 @@ let () =
  (action (run %%{run_test} %s liquidsoap %%{test_liq} %s)))
   |}
         test test test)
-    tests;
-
-  let output_tests =
-    List.filter
-      (fun f -> Filename.extension f = ".output")
-      (Build_tools.read_files ~location "")
-  in
-  List.iter
-    (fun test ->
-      Printf.printf
-        {|
-(rule
- (alias citest)
- (package liquidsoap)
- (deps
-  %s
-  (:check_output ../check_output.exe)
-  (:run_test ../run_test.exe))
- (action (run %%{run_test} %s %%{check_output})))
-  |}
-        test test)
-    output_tests
+    tests
