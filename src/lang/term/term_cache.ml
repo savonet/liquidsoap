cde0dc934aa4755714bbe0a02956effceab14f10
Romain Beauxis <toots@rastageeks.org>
2024-06-14 10:22:21 +0200

    Report

--- src/lang/term/term_cache.ml
+++ src/lang/term/term_cache.ml
@@ -20,15 +20,16 @@ let cache_filename ?name ~trim parsed_term =
   Printf.sprintf "%s.liq-cache" hash
 
 let retrieve ?name ~trim parsed_term : Term.t option =
-  let report fn =
-    match name with
-      | None -> fn ()
-      | Some name -> Startup.time (Printf.sprintf "%s cache retrieval" name) fn
-  in
-  report (fun () ->
-      if Cache.enabled () then
-        Cache.retrieve ?name (cache_filename ?name ~trim parsed_term)
-      else None)
+  if Cache.enabled () then (
+    let report fn =
+      match name with
+        | None -> fn ()
+        | Some name ->
+            Startup.time (Printf.sprintf "%s cache retrieval" name) fn
+    in
+    report (fun () ->
+        Cache.retrieve ?name (cache_filename ?name ~trim parsed_term)))
+  else None
 
 let cache ~trim ~parsed_term term =
   Cache.store (cache_filename ~trim parsed_term) term
