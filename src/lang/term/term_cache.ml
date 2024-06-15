17a678048a776439df447ff88261cd4407f2d1e8
Samuel Mimram <smimram@gmail.com>
2024-06-15 20:56:05 +0200

    Cache plugins (#3959)
    Co-authored-by: Romain Beauxis <toots@rastageeks.org>

--- src/lang/term/term_cache.ml
+++ src/lang/term/term_cache.ml
@@ -7,131 +7,29 @@ type t = {
 }
 [@@deriving hash]
 
-let cache_enabled () =
-  try
-    let venv = Unix.getenv "LIQ_CACHE" in
-    venv = "1" || venv = "true"
-  with Not_found -> true
-
-let default_cache_dir =
-  ref (fun () ->
-      try
-        match Sys.os_type with
-          | "Win32" ->
-              let dir = Filename.dirname Sys.executable_name in
-              let cwd = Sys.getcwd () in
-              Sys.chdir dir;
-              let dir = Sys.getcwd () in
-              Sys.chdir cwd;
-              Some (Filename.concat dir ".cache")
-          | _ ->
-              Some
-                (Filename.concat
-                   (Filename.concat (Unix.getenv "HOME") ".cache")
-                   "liquidsoap")
-      with Not_found -> None)
-
-let cache_dir () =
-  if cache_enabled () then (
-    match
-      try Some (Unix.getenv "LIQ_CACHE_DIR")
-      with Not_found ->
-        let fn = !default_cache_dir in
-        fn ()
-    with
-      | None ->
-          Startup.message
-            "Could not find default cache directory! You can set it using the \
-             `$LIQ_CACHE_DIR` environment variable.";
-          None
-      | Some _ as v -> v)
-  else (
-    Startup.message "Cache disabled!";
-    None)
-
-let rec recmkdir dir =
-  if not (Sys.file_exists dir) then (
-    recmkdir (Filename.dirname dir);
-    Sys.mkdir dir 0o755)
-
 let cache_filename ?name ~trim parsed_term =
-  match cache_dir () with
-    | None -> None
-    | Some dir ->
-        recmkdir dir;
-        let report fn =
-          match name with
-            | None -> fn ()
-            | Some name ->
-                Startup.time (Printf.sprintf "%s hash computation" name) fn
-        in
-        let hash =
-          report (fun () ->
-              hash
-                { env = Environment.default_environment (); trim; parsed_term })
-        in
-        let fname = Printf.sprintf "%s.liq-cache" hash in
-        Some (Filename.concat dir fname)
-
-let retrieve ?name ~trim parsed_term : Term.t option =
   let report fn =
     match name with
       | None -> fn ()
-      | Some name -> Startup.time (Printf.sprintf "%s cache retrieval" name) fn
+      | Some name -> Startup.time (Printf.sprintf "%s hash computation" name) fn
   in
-  report (fun () ->
-      try
-        match cache_filename ?name ~trim parsed_term with
-          | None -> None
-          | Some filename ->
-              if Sys.file_exists filename then (
-                let ic = open_in_bin filename in
-                Fun.protect
-                  ~finally:(fun () -> close_in_noerr ic)
-                  (fun () ->
-                    let term = Marshal.from_channel ic in
-                    (match name with
-                      | Some name ->
-                          Startup.message "Loading %s from cache!" name
-                      | None -> ());
-                    Some term))
-              else None
-      with
-        | Failure msg
-          when String.starts_with ~prefix:"input_value: unknown code module" msg
-          ->
-            (match name with
-              | Some name ->
-                  Startup.message
-                    "Liquidsoap binary changed: %s cache invalidated!" name
-              | None -> ());
-            None
-        | exn ->
-            let bt = Printexc.get_backtrace () in
-            let exn = Printexc.to_string exn in
-            if Sys.getenv_opt "LIQ_DEBUG_CACHE" <> None then
-              Startup.message "Error while loading cache: %s\n%s" exn bt
-            else Startup.message "Error while loading cache: %s" exn;
-            None)
+  let hash =
+    report (fun () ->
+        hash { env = Environment.default_environment (); trim; parsed_term })
+  in
+  Printf.sprintf "%s.liq-cache" hash
+
+let retrieve ?name ~trim parsed_term : Term.t option =
+  if Cache.enabled () then (
+    let report fn =
+      match name with
+        | None -> fn ()
+        | Some name ->
+            Startup.time (Printf.sprintf "%s cache retrieval" name) fn
+    in
+    report (fun () ->
+        Cache.retrieve ?name (cache_filename ?name ~trim parsed_term)))
+  else None
 
 let cache ~trim ~parsed_term term =
-  try
-    match cache_filename ~trim parsed_term with
-      | None -> ()
-      | Some filename ->
-          let tmp_file, oc =
-            Filename.open_temp_file
-              ~temp_dir:(Filename.dirname filename)
-              "tmp" ".liq-cache"
-          in
-          Fun.protect
-            ~finally:(fun () ->
-              close_out_noerr oc;
-              if Sys.file_exists tmp_file then Sys.remove tmp_file)
-            (fun () ->
-              Marshal.to_channel oc term [Marshal.Closures];
-              Sys.rename tmp_file filename);
-          let fn = !Hooks.cache_maintenance in
-          fn ()
-  with exn ->
-    Startup.message "Error while saving cache: %s" (Printexc.to_string exn)
+  Cache.store (cache_filename ~trim parsed_term) term
--- src/lang/term/term_cache.mli
+++ src/lang/term/term_cache.mli
@@ -1,4 +1,2 @@
-val cache_enabled : unit -> bool
-val cache_dir : unit -> string option
 val retrieve : ?name:string -> trim:bool -> Parsed_term.t -> Term.t option
 val cache : trim:bool -> parsed_term:Parsed_term.t -> Term.t -> unit
