28c65a1017208da9db3e523c64c22a2fbdc0d5f4
Romain Beauxis <toots@rastageeks.org>
2024-06-17 01:33:59 +0200

    Treat main as release branch.

--- .github/scripts/build-details.sh
+++ .github/scripts/build-details.sh
@@ -17,7 +17,7 @@ if [ "${IS_FORK}" == "true" ]; then
   IS_FORK=true
 fi
 
-if [[ "${IS_FORK}" != "true" && ("${BRANCH}" =~ ^rolling-release\-v[0-9]\.[0-9]\.x || "${BRANCH}" =~ ^v[0-9]\.[0-9]\.[0-9]) ]]; then
+if [[ "${IS_FORK}" != "true" && ("${BRANCH}" == "main" || "${BRANCH}" =~ ^rolling-release\-v[0-9]\.[0-9]\.x || "${BRANCH}" =~ ^v[0-9]\.[0-9]\.[0-9]) ]]; then
   echo "Branch is release branch"
   IS_RELEASE=true
 
