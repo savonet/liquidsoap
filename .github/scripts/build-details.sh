0fa2945c701b79e609b406d11acb0cf9de6e0b4b
Romain Beauxis <toots@rastageeks.org>
2024-06-17 10:10:15 +0200

    Revert "Build on all arch all the time"
    This reverts commit 0d99aa66f1a3e6859b07e76a2260f4284abe8379.

--- .github/scripts/build-details.sh
+++ .github/scripts/build-details.sh
@@ -41,11 +41,6 @@ else
   DOCKER_RELEASE=
 fi
 
-echo "Building on all architectures"
-BUILD_OS='["debian_trixie", "debian_bookworm", "debian_bullseye", "ubuntu_jammy", "ubuntu_noble", "alpine"]'
-BUILD_PLATFORM='["amd64", "arm64", "armhf"]'
-BUILD_INCLUDE='[{"platform": "amd64", "runs-on": "ubuntu-latest", "alpine-arch": "x86_64", "docker-platform": "linux/amd64", "docker-debian-os": "trixie"}, {"platform": "arm64", "runs-on": ["self-hosted", "build"], "alpine-arch": "aarch64", "docker-platform": "linux/arm64", "docker-debian-os": "trixie"}, {"platform": "armhf", "runs-on": ["self-hosted", "build"], "alpine-arch": "armv7", "docker-platform": "linux/arm/v7", "docker-debian-os": "bullseye"}]'
-
 SHA=$(git rev-parse --short HEAD)
 
 if [[ "${BRANCH}" =~ "rolling-release-" ]]; then
