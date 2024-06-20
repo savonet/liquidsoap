30c23c18ad531492d9df58b5ddfb943690430cfb
Romain Beauxis <toots@rastageeks.org>
2024-06-20 10:44:08 +0200

    Bring taglib back.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -50,7 +50,7 @@ opam install -y .
 cd ..
 
 opam update
-opam install -y --confirm-level=unsafe-yes magic-mime pcre tls.0.17.3
+opam install -y --confirm-level=unsafe-yes magic-mime pcre tls.0.17.3 taglib
 
 cd /tmp/liquidsoap-full
 
