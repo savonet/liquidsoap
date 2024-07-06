1915d5b18284e25be59c72bf2f07d6452c606c3b
Romain Beauxis <toots@rastageeks.org>
2024-07-06 09:50:33 -0400

    Try this.

--- .github/scripts/build-deb.sh
+++ .github/scripts/build-deb.sh
@@ -8,7 +8,6 @@ DOCKER_TAG="$3"
 PLATFORM="$4"
 IS_ROLLING_RELEASE="$5"
 IS_RELEASE="$6"
-MINIMAL_EXCLUDE_DEPS="$7"
 DEB_RELEASE=1
 
 ARCH=$(dpkg --print-architecture)
@@ -49,63 +48,21 @@ sed -e "s#@LIQ_PACKAGE@#${LIQ_PACKAGE}#g" -i debian/control
 
 dch --create --distribution unstable --package "${LIQ_PACKAGE}" --newversion "1:${LIQ_VERSION}-${LIQ_TAG}-${DEB_RELEASE}" "Build ${COMMIT_SHORT}"
 
-fakeroot debian/rules binary
+export LIQUIDSOAP_BUILD_TARGET=posix
 
-echo "::endgroup::"
+DESTDIR=debian/tmp
 
-if [ "${PLATFORM}" = "amd64" ]; then
-  echo "::group:: save build config for ${LIQ_PACKAGE}.."
-
-  ./liquidsoap --build-config > "/tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${PLATFORM}/debian/${LIQ_PACKAGE}_${LIQ_VERSION}-${LIQ_TAG}-${DEB_RELEASE}.config"
-
-  mv /tmp/liquidsoap-full/*.deb "/tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${PLATFORM}/debian"
-fi
-
-echo "::endgroup::"
-
-echo "::group:: build ${LIQ_PACKAGE}-minimal.."
-
-# shellcheck disable=SC2086
-opam remove -y --verbose $MINIMAL_EXCLUDE_DEPS
-
-cd /tmp/liquidsoap-full
-make clean
-cp PACKAGES.minimal-build PACKAGES
-rm .ocamlpath
-cd liquidsoap
-./.github/scripts/build-posix.sh 1
-OCAMLPATH="$(cat ../.ocamlpath)"
-export OCAMLPATH
-
-rm -rf debian
-
-cp -rf .github/debian .
-
-rm -rf debian/changelog
-
-cp -f debian/control.in debian/control
-
-sed -e "s#@LIQ_PACKAGE@#${LIQ_PACKAGE}-minimal#g" -i debian/control
-
-cp -rf debian/rules-minimal debian/rules
-
-dch --create --distribution unstable --package "${LIQ_PACKAGE}-minimal" --newversion "1:${LIQ_VERSION}-${LIQ_TAG}-${DEB_RELEASE}" "Build ${COMMIT_SHORT}"
+dune build @install --release --verbose
+dune install --prefix "$DESTDIR" --verbose
+mkdir -p "${DESTDIR}/share/liquidsoap"
+cp -rf "$(opam var share)/camomile" "${DESTDIR}/share/liquidsoap"
 
 fakeroot debian/rules binary
 
 echo "::endgroup::"
-
-if [ "${PLATFORM}" = "amd64" ]; then
-  echo "::group:: save build config for ${LIQ_PACKAGE}.."
-
-  ./liquidsoap --build-config > "/tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${PLATFORM}/debian/${LIQ_PACKAGE}-minimal_${LIQ_VERSION}-${LIQ_TAG}-${DEB_RELEASE}.config"
-
-  echo "::endgroup::"
-fi
 
 mv /tmp/liquidsoap-full/*.deb "/tmp/${GITHUB_RUN_NUMBER}/${DOCKER_TAG}_${PLATFORM}/debian"
 
 {
   echo "basename=${LIQ_PACKAGE}_${LIQ_VERSION}-${LIQ_TAG}-${DEB_RELEASE}_$ARCH"
-  echo "basename-minimal=${LIQ_PACKAGE}-minimal_${LIQ_VERSION}-${LIQ_TAG}-${DEB_RELEASE}_$ARCH"
 } >> "${GITHUB_OUTPUT}"
