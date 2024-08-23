8dad152d7f77a8522819d2a8d4b829dc1ff21268
Romain Beauxis <toots@rastageeks.org>
2024-08-23 11:16:18 -0500

    Make frame slice be really at most.

--- tests/core/frame_test.ml
+++ tests/core/frame_test.ml
@@ -4,3 +4,7 @@ let () =
       (Frame.Fields.make ~audio:(Format_type.audio ()) ())
   in
   Typing.(pcm_t <: Lang.univ_t ())
+
+let () =
+  let f = Frame.create ~length:10 (Frame.Fields.from_list []) in
+  assert (Frame.position (Frame.slice f 20) = 10)
