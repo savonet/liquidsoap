89dd891460df9d6e37947f56229e113c0604c7c5
Romain Beauxis <toots@rastageeks.org>
2024-09-24 09:05:26 -0500

    Update to latest TLS API. (#4141)

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -50,7 +50,7 @@ cd ..
 
 opam update
 opam remove -y jemalloc
-opam install -y tls.0.17.4 saturn_lockfree.0.4.1 ppx_hash
+opam install -y tls.1.0.2 saturn_lockfree.0.4.1 ppx_hash
 
 cd /tmp/liquidsoap-full
 
