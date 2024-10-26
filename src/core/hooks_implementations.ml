bacdd274ca8f4b2be2bcda5580758f753434ce08
Romain Beauxis <toots@rastageeks.org>
2024-10-26 10:52:37 -0500

    Fix tracks runtime type check.

--- src/core/hooks_implementations.ml
+++ src/core/hooks_implementations.ml
@@ -20,7 +20,7 @@ let eval_check ~env:_ ~tm v =
       s#content_type_computation_allowed))
   else if Source_tracks.is_value v then (
     let s = Source_tracks.source v in
-    Typing.(s#frame_type <: tm.Term.t))
+    Typing.(s#frame_type <: Type.fresh tm.Term.t))
   else if Track.is_value v then (
     let field, source = Lang_source.to_track v in
     if not source#has_content_type then (
