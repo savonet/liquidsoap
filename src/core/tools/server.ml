0c50a54ed38f583d23e0f195868fee3e17ec6d8c
Romain Beauxis <toots@rastageeks.org>
2024-11-24 17:38:18 -0600

    Use String.trim. Fixes: #4223

--- src/core/tools/server.ml
+++ src/core/tools/server.ml
@@ -237,12 +237,9 @@ let () =
   add "help" ~usage:"help [<command>]"
     ~descr:"Get information on available commands." (fun args ->
       try
-        let args =
-          Re.Pcre.substitute ~rex:(Re.Pcre.regexp "\\s*")
-            ~subst:(fun _ -> "")
-            args
-        in
+        let args = String.trim args in
         let _, us, d = Mutex_utils.mutexify lock (Hashtbl.find commands) args in
+        Printf.printf "Done\n%!";
         Printf.sprintf "Usage: %s\r\n  %s" us d
       with Not_found ->
         (if args <> "" then "No such command: " ^ args ^ "\r\n" else "")
