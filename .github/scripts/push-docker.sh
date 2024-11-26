7fdc31bb74d72dceaefd9290e87a580167589b77
Romain Beauxis <toots@rastageeks.org>
2024-11-26 08:10:03 -0600

    Try this.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -17,26 +17,6 @@ COMMIT_SHORT=$(echo "${GITHUB_SHA}" | cut -c-7)$(echo "${GITHUB_SHA}" | cut -d'-
 
 docker login -u "$USER" -p "$PASSWORD"
 
-# Something is odd with the docker repo
-## REMOVE WHEN FIXED ##
-docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
-
-docker pull "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64"
-docker tag "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64" "savonet/liquidsoap-ci-build:${TAG}_amd64"
-docker push "savonet/liquidsoap-ci-build:${TAG}_amd64"
-
-docker pull "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64"
-docker tag "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64" "savonet/liquidsoap-ci-build:${TAG}_arm64"
-docker push "savonet/liquidsoap-ci-build:${TAG}_arm64"
-
-docker pull "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64"
-docker tag "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64" "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64"
-docker push "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64"
-
-docker pull "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64"
-docker tag "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64" "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64"
-docker push "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64"
-
 docker manifest create "savonet/liquidsoap:${TAG}" --amend "savonet/liquidsoap-ci-build:${TAG}_amd64" --amend "savonet/liquidsoap-ci-build:${TAG}_arm64"
 docker manifest push "savonet/liquidsoap:${TAG}"
 
