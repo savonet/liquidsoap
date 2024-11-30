246e105ef0e1177f17fecf45ac9cfd6eeafd0233
Romain Beauxis <toots@rastageeks.org>
2024-11-29 23:38:41 -0600

    Try this.

--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -42,15 +42,9 @@ export CC=""
 echo "::group::Installing deps"
 
 eval "$(opam config env)"
-opam repository set-url default https://github.com/ocaml/opam-repository.git
-opam repository set-url windows https://github.com/ocaml-cross/opam-cross-windows.git
+cd cd /home/opam/opam-cross-windows
+git pull --rebase https://github.com/ocaml-cross/opam-cross-windows.git main
 opam update windows
-# shellcheck disable=SC2046
-opam upgrade -y $(echo "$OPAM_DEPS" | sed -e 's#,# #g') ffmpeg-windows ffmpeg-avutil-windows
-opam remove -y pcre-windows
-
-# Debug
-opam reinstall -y cry-windows
 
 echo "::endgroup::"
 
