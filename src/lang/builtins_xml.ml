e4ab65f9af2bf21e375e6d25136cbb00762acd80
Romain Beauxis <toots@rastageeks.org>
2024-12-11 10:50:01 +0100

    Add test, almost there

--- src/lang/builtins_xml.ml
+++ src/lang/builtins_xml.ml
@@ -1,7 +1,10 @@
 let rec methods_of_xml = function
-  | Xml.PCData s -> ("text", Lang.string s)
+  | Xml.PCData s ->
+      ( "xml_text",
+        Lang.meth (Lang.string s) (xml_node ~text:s ~params:[] ~children:[] ())
+      )
   | Xml.Element (name, params, ([Xml.PCData s] as children)) ->
-      (name, Lang.meth (Lang.string s) (xml_node ~params ~children name))
+      (name, Lang.meth (Lang.string s) (xml_node ~text:s ~params ~children ()))
   | Xml.Element (name, params, children) ->
       ( name,
         Lang.record
@@ -12,16 +15,18 @@ let rec methods_of_xml = function
                   let v =
                     match Methods.find_opt name methods with
                       | None -> v
+                      | Some (Value.Tuple { value = [] } as value) ->
+                          Lang.tuple [value; v]
                       | Some (Value.Tuple { value }) -> Lang.tuple (value @ [v])
                       | Some value -> Lang.tuple [value; v]
                   in
                   Methods.add name v methods)
-                (Methods.from_list (xml_node ~params ~children name))
+                (Methods.from_list (xml_node ~params ~children ()))
                 children)) )
 
-and xml_node ~params ~children name =
+and xml_node ?text ~params ~children () =
   [
-    ("xml_node", Lang.string name);
+    ("xml_text", match text with None -> Lang.null | Some s -> Lang.string s);
     ( "xml_params",
       Lang.meth
         (Lang.list
@@ -29,10 +34,12 @@ and xml_node ~params ~children name =
               (fun (k, v) -> Lang.product (Lang.string k) (Lang.string v))
               params))
         (List.map (fun (k, v) -> (k, Lang.string v)) params) );
-    ("xml_children", Lang.tuple (List.map (fun v -> value_of_xml v) children));
+    ("xml_children", Lang.list (List.map (fun v -> value_of_xml v) children));
   ]
 
-and value_of_xml v = Lang.record [methods_of_xml v]
+and value_of_xml v =
+  let name, methods = methods_of_xml v in
+  Lang.meth (Lang.tuple [Lang.string name; methods]) [(name, methods)]
 
 let rec check_value v ty =
   let typ_meths, ty = Type.split_meths ty in
@@ -40,23 +47,25 @@ let rec check_value v ty =
   let v =
     match (v, ty.Type.descr) with
       | Value.Tuple { value = [] }, Type.Nullable _ -> Lang.null
+      | _, Type.Tuple [] -> Lang.tuple []
+      | Value.Tuple { value }, Type.Tuple l ->
+          Lang.tuple
+            (List.mapi (fun idx v -> check_value v (List.nth l idx)) value)
+      | Value.List { value = l }, Type.List { t = ty } ->
+          Lang.list (List.map (fun v -> check_value v ty) l)
       | Value.String { value = s }, Type.Int -> Lang.int (int_of_string s)
       | Value.String { value = s }, Type.Float -> Lang.float (float_of_string s)
       | Value.String { value = s }, Type.Bool -> Lang.bool (bool_of_string s)
       | Value.String _, Type.Nullable ty -> check_value v ty
-      | _, Type.Var _
-      | Value.Tuple { value = [] }, Type.Tuple []
-      | Value.String _, Type.String ->
-          v
+      | _, Type.Var _ | Value.String _, Type.String -> v
       | _ -> assert false
   in
   let meths =
     List.fold_left
-      (fun meths (name, v) ->
-        match List.find_opt (fun { Type.meth } -> meth = name) typ_meths with
-          | None -> meths
-          | Some { Type.scheme = _, ty } -> (name, check_value v ty) :: meths)
-      [] meths
+      (fun checked_meths { Type.meth; scheme = _, ty } ->
+        let v = List.assoc meth meths in
+        (meth, check_value v ty) :: checked_meths)
+      [] typ_meths
   in
   let v = Lang.meth v meths in
   v
@@ -92,13 +101,15 @@ let xml = Lang.add_module "xml"
 
 let string_of_ground v =
   match v with
-    | Value.String _ | Value.Bool _ | Value.Float _ | Value.Int _ ->
-        Value.to_string (Lang.demeth v)
+    | Value.String { value = s } -> s
+    | Value.Bool { value = b } -> string_of_bool b
+    | Value.Float { value = f } -> Utils.string_of_float f
+    | Value.Int { value = i; flags } -> Value.string_of_int_value ~flags i
     | _ -> assert false
 
 let params_of_xml_params v =
   match Lang.split_meths v with
-    | [], Value.Tuple { value = params } ->
+    | [], Value.List { value = params } ->
         List.map
           (function
             | Value.Tuple { value = [Value.String { value = s }; v] } ->
@@ -110,32 +121,58 @@ let params_of_xml_params v =
     | _ -> assert false
 
 let params_of_optional_params = function
-   | None -> []
-   | Some params -> params_of_xml_params params
+  | None -> []
+  | Some params -> params_of_xml_params params
 
-let rec xml_of_value v =
-  match Lang.split_meths v with
-    | meths, Value.Tuple { value = [] } -> xml_of_meths meths
-    | _ -> assert false
+let rec xml_of_value = function
+  | Value.Tuple
+      {
+        value =
+          [Value.String { value = name }; Value.Tuple { value = []; methods }];
+      } ->
+      xml_of_node ~name (Methods.bindings methods)
+  | Value.Tuple { value = []; methods } -> (
+      match Methods.bindings methods with
+        | [(name, Value.Tuple { value = []; methods })] ->
+            xml_of_node ~name (Methods.bindings methods)
+        | [(name, Value.String { value = s; methods })] ->
+            xml_of_node ~xml_text:s ~name (Methods.bindings methods)
+        | _ -> assert false)
+  | _ -> assert false
 
-and xml_of_meths meths =
-  let xml_name = List.assoc_opt "xml_name" meths in
-  let xml_nodes = List.assoc_opt "xml_nodes" meths in
+and xml_of_node ?xml_text ~name meths =
+  let xml_text =
+    match xml_text with
+      | Some s -> Some s
+      | None -> Option.map Lang.to_string (List.assoc_opt "xml_text" meths)
+  in
+  let xml_children =
+    Option.map Lang.to_list (List.assoc_opt "xml_children" meths)
+  in
   let xml_params = List.assoc_opt "xml_params" meths in
   let meths =
     List.filter
-      (fun (k, _) -> not (List.mem k ["xml_name"; "xml_nodes"; "xml_params"]))
+      (fun (k, _) ->
+        not (List.mem k ["xml_text"; "xml_children"; "xml_params"]))
       meths
   in
-  match (xml_name, xml_params, xml_nodes, meths) with
-    | ( Some (Value.String { value = name }),
-        params,
-        Some (Value.Tuple { value = nodes }),
-        [] )
-    | None, params, None, [(name, Value.Tuple { value = nodes, methods = [] })] ->
+  match (name, xml_params, xml_children, xml_text, meths) with
+    | "xml_text", None, None, Some s, [] -> Xml.PCData s
+    | name, xml_params, None, Some s, [] ->
+        Xml.Element (name, params_of_optional_params xml_params, [Xml.PCData s])
+    | name, xml_params, Some nodes, None, [] ->
         Xml.Element
-          (name, params_of_optional_params params, List.map xml_of_value nodes)
-    | None, params, None,  
+          ( name,
+            params_of_optional_params xml_params,
+            List.map xml_of_value nodes )
+    | name, xml_params, None, None, nodes ->
+        Xml.Element
+          ( name,
+            params_of_optional_params xml_params,
+            List.map
+              (fun (name, value) -> xml_of_value (Lang.record [(name, value)]))
+              nodes )
+    | _ -> assert false
 
 let _ =
   Lang.add_builtin ~base:xml "stringify" ~category:`String
