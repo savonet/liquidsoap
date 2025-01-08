0654c76d74bdc896ada9163a99c9fe4c05bc9e76
Vito <vitoyucepi@cock.li>
2025-01-08 04:56:35 +0000

    refactor(ci): Use docker/login-action to log in to container registries

--- .github/scripts/build-docker-alpine.sh
+++ .github/scripts/build-docker-alpine.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-set -e
+set -eux
 
 APK_FILE="$1"
 TAG="$2"
-USER="$3"
-PASSWORD="$4"
-ARCHITECTURE="$5"
+ARCHITECTURE="$3"
 
-cp "$APK_FILE" .
-
-if [ "${PUBLISH_DOCKER_IMAGE}" = "true" ]; then
-  PUSH_OPTION=--push
-fi
-
-# shellcheck disable=SC2086
 docker build \
   --pull \
   --no-cache \
@@ -22,19 +13,15 @@ docker build \
   --build-arg "APK_FILE=$APK_FILE" \
   --file .github/docker/alpine.dockerfile \
   --tag "savonet/liquidsoap-ci-build:${TAG}_alpine_${ARCHITECTURE}" \
-  ${PUSH_OPTION} \
   .
 
 if [ "${PUBLISH_DOCKER_IMAGE}" != "true" ]; then
   exit 0
 fi
 
-docker login -u "$USER" -p "$PASSWORD"
-
-docker pull "savonet/liquidsoap-ci-build:${TAG}_alpine_${ARCHITECTURE}"
-
 docker tag \
   "savonet/liquidsoap-ci-build:${TAG}_alpine_${ARCHITECTURE}" \
   "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_${ARCHITECTURE}"
 
+docker push "savonet/liquidsoap-ci-build:${TAG}_alpine_${ARCHITECTURE}"
 docker push "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_${ARCHITECTURE}"
--- .github/scripts/build-docker.sh
+++ .github/scripts/build-docker.sh
@@ -1,44 +1,29 @@
 #!/bin/sh
 
-set -e
+set -eux
 
 DEB_FILE="$1"
 DEB_DEBUG_FILE="$2"
 TAG="$3"
-USER="$4"
-PASSWORD="$5"
-ARCHITECTURE="$6"
+ARCHITECTURE="$4"
 
-cp "$DEB_FILE" "$DEB_DEBUG_FILE" .
-
-DOCKERFILE=.github/docker/debian.dockerfile
-
-if [ "${PUBLISH_DOCKER_IMAGE}" = "true" ]; then
-  PUSH_OPTION=--push
-fi
-
-# shellcheck disable=SC2086
 docker build \
   --pull \
   --no-cache \
   --provenance false \
   --build-arg "DEB_FILE=$DEB_FILE" \
   --build-arg "DEB_DEBUG_FILE=$DEB_DEBUG_FILE" \
-  --file "${DOCKERFILE}" \
+  --file .github/docker/debian.dockerfile \
   --tag "savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}" \
-  ${PUSH_OPTION} \
   .
 
 if [ "${PUBLISH_DOCKER_IMAGE}" != "true" ]; then
   exit 0
 fi
 
-docker login -u "$USER" -p "$PASSWORD"
-
-docker pull "savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}"
-
 docker tag \
   "savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}" \
   "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}"
 
+docker push "savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}"
 docker push "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_${ARCHITECTURE}"
