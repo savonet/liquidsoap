f51e9681e29f074ccd8beef9635f3504cfc5bf88
Romain Beauxis <toots@rastageeks.org>
2025-01-22 12:38:26 -0600

    Build against latest posix-socket again.

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -44,6 +44,13 @@ cd ocaml-xiph
 opam install -y .
 cd ..
 
+cd /tmp
+rm -rf ocaml-posix
+git clone https://github.com/savonet/ocaml-posix.git
+cd ocaml-posix
+opam pin -ny .
+opam install -y posix-socket.2.1.1 posix-base.2.1.1 posix-time2.2.1.1 posix-types.2.1.1
+
 cd /tmp/liquidsoap-full/liquidsoap
 
 ./.github/scripts/checkout-deps.sh
@@ -55,8 +62,7 @@ cd ..
 
 opam update
 opam remove -y jemalloc
-opam unpin -n posix-socket posix-base posix-time2 posix-types
-opam install -y tls.1.0.2 ca-certs mirage-crypto-rng cstruct saturn_lockfree.0.5.0 ppx_hash memtrace xml-light posix-socket.2.1.0 posix-base.2.1.0 posix-time2.2.1.0 posix-types.2.1.0
+opam install -y tls.1.0.2 ca-certs mirage-crypto-rng cstruct saturn_lockfree.0.5.0 ppx_hash memtrace xml-light
 
 cd /tmp/liquidsoap-full
 
