73568693a362375f203d286a369e8478a97ff09e
Romain Beauxis <toots@rastageeks.org>
2025-07-26 14:59:00 -0500

    Build 5.x using oxcaml.

--- .github/scripts/build-details.sh
+++ .github/scripts/build-details.sh
@@ -64,8 +64,8 @@ fi
 
 MINIMAL_EXCLUDE_DEPS="alsa ao bjack camlimages dssi faad fdkaac flac frei0r gd graphics gstreamer imagelib irc-client-unix ladspa lame lastfm lilv lo mad magic ogg opus osc-unix portaudio pulseaudio samplerate shine soundtouch speex srt tls theora tsdl sqlite3 vorbis sdl-liquidsoap"
 
-echo "Ocaml version to build: 4.14.2, 5.3.0"
-OCAML_VERSION='["4.14.2", "5.3.0"]'
+echo "Ocaml version to build: 4.14.2, 5.2.0_ox"
+OCAML_VERSION='["4.14.2", "5.2.0_ox"]'
 
 echo "OCaml docker release version: 4.14.2"
 OCAML_DOCKER_RELEASE_VERSION="4.14.2"
