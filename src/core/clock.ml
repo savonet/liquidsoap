77fa92fc810d64ec8299d20a030c0c9191f4148d
Romain Beauxis <romain.beauxis@gmail.com>
2026-01-07 17:58:34 -0600

    Check error in all wrap_errors calls.

--- src/core/clock.ml
+++ src/core/clock.ml
@@ -463,6 +463,7 @@ let started c =
     | `Stopped _ -> false
 
 let wrap_errors clock fn s =
+  check_stopped ();
   try fn s
   with exn when exn <> Has_stopped ->
     let bt = Printexc.get_raw_backtrace () in
@@ -488,7 +489,6 @@ and _activate_pending_sources ~clock x =
   let pending_sources = Queue.length clock.pending_activations in
   Queue.flush_iter clock.pending_activations
     (wrap_errors clock (fun s ->
-         check_stopped ();
          match s#source_type with
            | `Active _ -> WeakQueue.push x.active_sources s
            | `Output _ ->
@@ -535,7 +535,6 @@ and _tick ~clock x =
   let sources = _animated_sources x in
   List.iter
     (wrap_errors clock (fun s ->
-         check_stopped ();
          match s#source_type with
            | `Output s | `Active s -> s#output
            | _ -> assert false))
