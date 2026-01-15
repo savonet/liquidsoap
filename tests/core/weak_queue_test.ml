3dc2709eb9ed30c25aaf053bd6a95ecf0dc04a75
Romain Beauxis <romain.beauxis@gmail.com>
2026-01-14 21:35:50 -0600

    Fix this.

--- tests/core/weak_queue_test.ml
+++ tests/core/weak_queue_test.ml
@@ -37,21 +37,21 @@ let () =
 
   (* Test filter *)
   let q2 = Queues.WeakQueue.create () in
-  Queues.WeakQueue.push q2 (keep 1);
-  Queues.WeakQueue.push q2 (keep 2);
-  Queues.WeakQueue.push q2 (keep 3);
-  Queues.WeakQueue.push q2 (keep 4);
-  Queues.WeakQueue.filter q2 (fun x -> x mod 2 = 0);
-  assert (has_all (Queues.WeakQueue.elements q2) [2; 4]);
+  Queues.WeakQueue.push q2 (keep "1");
+  Queues.WeakQueue.push q2 (keep "2");
+  Queues.WeakQueue.push q2 (keep "3");
+  Queues.WeakQueue.push q2 (keep "4");
+  Queues.WeakQueue.filter q2 (fun x -> x = "2" || x = "4");
+  assert (has_all (Queues.WeakQueue.elements q2) ["2"; "4"]);
 
   (* Test filter_out *)
   let q3 = Queues.WeakQueue.create () in
-  Queues.WeakQueue.push q3 (keep 10);
-  Queues.WeakQueue.push q3 (keep 20);
-  Queues.WeakQueue.push q3 (keep 30);
-  Queues.WeakQueue.push q3 (keep 40);
-  Queues.WeakQueue.filter_out q3 (fun x -> x mod 20 = 0);
-  assert (has_all (Queues.WeakQueue.elements q3) [10; 30]);
+  Queues.WeakQueue.push q3 (keep "10");
+  Queues.WeakQueue.push q3 (keep "20");
+  Queues.WeakQueue.push q3 (keep "30");
+  Queues.WeakQueue.push q3 (keep "40");
+  Queues.WeakQueue.filter_out q3 (fun x -> x = "20" || x = "40");
+  assert (has_all (Queues.WeakQueue.elements q3) ["10"; "30"]);
 
   (* Test flush_elements *)
   let q4 = Queues.WeakQueue.create () in
@@ -64,11 +64,11 @@ let () =
 
   (* Test flush_iter *)
   let q5 = Queues.WeakQueue.create () in
-  Queues.WeakQueue.push q5 (keep 100);
-  Queues.WeakQueue.push q5 (keep 200);
+  Queues.WeakQueue.push q5 (keep "100");
+  Queues.WeakQueue.push q5 (keep "200");
   let acc = ref [] in
   Queues.WeakQueue.flush_iter q5 (fun x -> acc := x :: !acc);
-  assert (has_all !acc [100; 200]);
+  assert (has_all !acc ["100"; "200"]);
   assert (Queues.WeakQueue.length q5 = 0);
 
   Printf.printf "All WeakQueue tests passed!\n%!"
