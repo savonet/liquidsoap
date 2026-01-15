c7cf86ccbf7bda6e802ea6292a18b359954282a1
Romain Beauxis <romain.beauxis@gmail.com>
2026-01-14 20:59:09 -0600

    Rewrite weakqueue, add tests.

--- src/core/base/tools/queues.ml
+++ src/core/base/tools/queues.ml
@@ -20,7 +20,7 @@
 
  *****************************************************************************)
 
-(* Atomic queues with slow lock for mutable operations. *)
+(* Atomic queues with slow lock for mutable operations. Queues are not order-preserving. *)
 
 module Queue = struct
   type 'a t = { m : Mutex.t; l : 'a List.t Atomic.t }
@@ -71,71 +71,132 @@ module Queue = struct
 end
 
 module WeakQueue = struct
-  include Queue
+  type 'a t = { m : Mutex.t; a : 'a Weak.t Atomic.t }
 
-  type nonrec 'a t = 'a Weak.t t
+  let create () = { m = Mutex.create (); a = Atomic.make (Weak.create 0) }
+  let mutate q fn = Mutex_utils.mutexify q.m fn q
+  let get q fn = fn (Atomic.get q.a)
 
   let push q v =
-    let w = Weak.create 1 in
-    Weak.set w 0 (Some v);
-    push q w
+    mutate q (fun q ->
+        let old_arr = Atomic.get q.a in
+        let old_len = Weak.length old_arr in
+        let rec collect i acc len =
+          if i >= old_len then (acc, len)
+          else (
+            match Weak.get old_arr i with
+              | Some el -> collect (i + 1) (el :: acc) (len + 1)
+              | None -> collect (i + 1) acc len)
+        in
+        let elements, len = collect 0 [] 0 in
+        let new_arr = Weak.create (len + 1) in
+        let rec populate i = function
+          | [] -> ()
+          | el :: rest ->
+              Weak.set new_arr i (Some el);
+              populate (i - 1) rest
+        in
+        populate (len - 1) elements;
+        Weak.set new_arr len (Some v);
+        Atomic.set q.a new_arr)
 
-  let flush_iter q fn =
-    flush_iter q (fun x ->
-        for i = 0 to Weak.length x - 1 do
-          match Weak.get x i with Some v -> fn v | None -> ()
-        done)
+  let exists q fn =
+    get q (fun arr ->
+        let len = Weak.length arr in
+        let rec loop i =
+          if i >= len then false
+          else (
+            match Weak.get arr i with
+              | Some el when fn el -> true
+              | _ -> loop (i + 1))
+        in
+        loop 0)
 
-  let flush_elements q =
-    let elements = ref [] in
-    flush_iter q (fun el -> elements := el :: !elements);
-    List.rev !elements
+  let length q =
+    get q (fun arr ->
+        let len = Weak.length arr in
+        let rec loop i acc =
+          if i >= len then acc
+          else loop (i + 1) (if Weak.check arr i then acc + 1 else acc)
+        in
+        loop 0 0)
+
+  let iter q fn =
+    get q (fun arr ->
+        let len = Weak.length arr in
+        let rec loop i =
+          if i >= len then ()
+          else (
+            (match Weak.get arr i with Some el -> fn el | None -> ());
+            loop (i + 1))
+        in
+        loop 0)
+
+  let fold q fn v =
+    get q (fun arr ->
+        let len = Weak.length arr in
+        let rec loop i acc =
+          if i >= len then acc
+          else
+            loop (i + 1)
+              (match Weak.get arr i with Some el -> fn el acc | None -> acc)
+        in
+        loop 0 v)
 
   let elements q =
-    let rec elements_f rem =
-      match Queue.pop_opt q with
-        | Some entry ->
-            let len = Weak.length entry in
-            let rec get_weak_entries pos ret =
-              if len <= pos then ret
-              else (
-                let ret =
-                  match Weak.get entry pos with
-                    | Some v -> v :: ret
-                    | None -> ret
-                in
-                get_weak_entries (pos + 1) ret)
-            in
-            elements_f (get_weak_entries 0 rem)
-        | None -> rem
-    in
-    let rem = elements_f [] in
-    let len = List.length rem in
-    if len > 0 then (
-      let entry = Weak.create len in
-      for i = 0 to len - 1 do
-        Weak.set entry i (Some (List.nth rem i))
-      done;
-      Queue.push q entry);
-    rem
+    get q (fun arr ->
+        let len = Weak.length arr in
+        let rec loop i acc =
+          if i >= len then acc
+          else
+            loop (i + 1)
+              (match Weak.get arr i with Some el -> el :: acc | None -> acc)
+        in
+        loop 0 [])
 
-  let exists q fn = List.exists fn (elements q)
-  let length q = List.length (elements q)
-  let iter q fn = List.iter fn (elements q)
-  let fold q fn v = List.fold_left (fun v e -> fn e v) v (elements q)
+  let flush_elements q =
+    let arr = mutate q (fun q -> Atomic.exchange q.a (Weak.create 0)) in
+    let len = Weak.length arr in
+    let rec loop i acc =
+      if i >= len then acc
+      else
+        loop (i + 1)
+          (match Weak.get arr i with Some el -> el :: acc | None -> acc)
+    in
+    loop 0 []
+
+  let flush_iter q fn =
+    let arr = mutate q (fun q -> Atomic.exchange q.a (Weak.create 0)) in
+    let len = Weak.length arr in
+    let rec loop i =
+      if i >= len then ()
+      else (
+        (match Weak.get arr i with Some el -> fn el | None -> ());
+        loop (i + 1))
+    in
+    loop 0
 
   let filter q fn =
-    let filter_f q =
-      List.iter
-        (fun el ->
-          for i = 0 to Weak.length el - 1 do
-            match Weak.get el i with
-              | Some p when fn p -> ()
-              | _ -> Weak.set el i None
-          done)
-        (Atomic.get q.l)
-    in
-    mutate q filter_f
+    mutate q (fun q ->
+        let old_arr = Atomic.get q.a in
+        let old_len = Weak.length old_arr in
+        let rec collect i acc len =
+          if i >= old_len then (acc, len)
+          else (
+            match Weak.get old_arr i with
+              | Some el when fn el -> collect (i + 1) (el :: acc) (len + 1)
+              | _ -> collect (i + 1) acc len)
+        in
+        let elements, len = collect 0 [] 0 in
+        let new_arr = Weak.create len in
+        let rec populate i = function
+          | [] -> ()
+          | el :: rest ->
+              Weak.set new_arr i (Some el);
+              populate (i - 1) rest
+        in
+        populate (len - 1) elements;
+        Atomic.set q.a new_arr)
 
   let filter_out q fn = filter q (fun el -> not (fn el))
 end
