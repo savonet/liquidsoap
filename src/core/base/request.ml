22a0a875c9ca7df61d6acf9c4124ae530eb8f68d
Romain Beauxis <romain.beauxis@gmail.com>
2026-01-21 08:48:39 -0600

    Fix this log.

--- src/core/base/request.ml
+++ src/core/base/request.ml
@@ -613,9 +613,7 @@ let get_decoder ~ctype r =
                       (Frame.seconds_of_main cue_out);
                     Frame.slice buf (cue_out - old_pos))
                   else (
-                    if
-                      Frame.is_partial buf
-                      && Lazy.force Frame.size < cue_out - new_pos
+                    if filled < len && Lazy.force Frame.size < cue_out - new_pos
                     then
                       r.logger#important
                         "End of track reached at %.02f before cue-out point at \
