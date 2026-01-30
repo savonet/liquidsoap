b5db2e33eb148af62f5931403bc4ae579a5fcf8a
Samuel Mimram <smimram@gmail.com>
2026-01-30 18:59:39 +0100

    More portable discover :)

--- src/modules/datachannel/config/discover.ml
+++ src/modules/datachannel/config/discover.ml
@@ -2,9 +2,22 @@ module C = Configurator.V1
 
 let () =
   C.main ~name:"datachannel-config" (fun _c ->
-      let cflags = ["-I"; "/Users/toots/sources/libdatachannel/include"] in
+      let cflags =
+        [
+          "-I";
+          "/Users/toots/sources/libdatachannel/include";
+          "-I";
+          "/home/smimram/build/libdatachannel/include";
+        ]
+      in
       let libs =
-        ["-L"; "/Users/toots/sources/libdatachannel/build"; "-ldatachannel"]
+        [
+          "-L";
+          "/Users/toots/sources/libdatachannel/build";
+          "-L";
+          "/home/smimram/build/libdatachannel/build";
+          "-ldatachannel";
+        ]
       in
       C.Flags.write_sexp "c_flags.sexp" cflags;
       C.Flags.write_sexp "c_library_flags.sexp" libs;
