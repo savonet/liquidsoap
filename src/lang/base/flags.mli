277828d058010eaf371a2629de5cf68c15bd5810
Romain Beauxis <toots@rastageeks.org>
2026-03-06 13:35:19 -0600

    Persist binary string flags via content-digest weak map (#4966)
    (cherry picked from commit 4b0953d9878f531f0776704411e2214b7c3ad7fe)

--- src/lang/base/flags.mli
+++ src/lang/base/flags.mli
@@ -9,4 +9,5 @@ val itered_value : flag
 val binary : flag
 val has : flags -> flag -> bool
 val add : flags -> flag -> flags
+val merge : flags -> flags -> flags
 val remove : flags -> flag -> flags
