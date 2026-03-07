a9e04ad533b08805e38ae5a2eb00c456f4130deb
Romain Beauxis <romain.beauxis@gmail.com>
2026-03-07 14:41:05 -0600

    Move to github registry for docker builds.

--- .github/scripts/push-docker.sh
+++ .github/scripts/push-docker.sh
@@ -2,41 +2,39 @@
 
 set -e
 
-rm -rf ~/.docker/config.json
-mkdir -p ~/.docker
-echo "{ \"experimental\": \"enabled\" }" > ~/.docker/config.json
-
 COMMIT_SHORT=$(echo "${GITHUB_SHA}" | cut -c-7)$(echo "${GITHUB_SHA}" | cut -d'-' -f 2 -s | while read -r i; do echo "-$i"; done)
 
 echo "TAG: ${TAG}"
 echo "OCAML_DOCKER_RELEASE_VERSION: ${OCAML_DOCKER_RELEASE_VERSION}"
-echo "amd64 image: savonet/liquidsoap-ci-build:${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}"
-echo "arm64 image: savonet/liquidsoap-ci-build:${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
+echo "amd64 image: ghcr.io/savonet/liquidsoap:ci-build-${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}"
+echo "arm64 image: ghcr.io/savonet/liquidsoap:ci-build-${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
 
 docker login -u "$USER" -p "$PASSWORD"
-
-docker manifest create "savonet/liquidsoap:${TAG}" --amend "savonet/liquidsoap-ci-build:${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "savonet/liquidsoap-ci-build:${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "savonet/liquidsoap:${TAG}"
-
-docker manifest create "savonet/liquidsoap:${COMMIT_SHORT}" --amend "savonet/liquidsoap-ci-build:${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "savonet/liquidsoap-ci-build:${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "savonet/liquidsoap:${COMMIT_SHORT}"
-
-docker manifest create "savonet/liquidsoap-alpine:${TAG}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "savonet/liquidsoap-alpine:${TAG}"
-
-docker manifest create "savonet/liquidsoap-alpine:${COMMIT_SHORT}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "savonet/liquidsoap-ci-build:${TAG}_alpine_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "savonet/liquidsoap-alpine:${COMMIT_SHORT}"
-
 docker login ghcr.io -u "$GHCR_USER" -p "$GHCR_PASSWORD"
 
-docker manifest create "ghcr.io/savonet/liquidsoap:${TAG}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "ghcr.io/savonet/liquidsoap:${TAG}"
+AMD64="ghcr.io/savonet/liquidsoap:ci-build-${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}"
+ARM64="ghcr.io/savonet/liquidsoap:ci-build-${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
+ALPINE_AMD64="ghcr.io/savonet/liquidsoap:ci-build-${TAG}_alpine_amd64-${OCAML_DOCKER_RELEASE_VERSION}"
+ALPINE_ARM64="ghcr.io/savonet/liquidsoap:ci-build-${TAG}_alpine_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
 
-docker manifest create "ghcr.io/savonet/liquidsoap:${COMMIT_SHORT}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "ghcr.io/savonet/liquidsoap:${COMMIT_SHORT}"
+# Push to Docker Hub
+docker buildx imagetools create \
+  -t "savonet/liquidsoap:${TAG}" \
+  -t "savonet/liquidsoap:${COMMIT_SHORT}" \
+  "${AMD64}" "${ARM64}"
 
-docker manifest create "ghcr.io/savonet/liquidsoap-alpine:${TAG}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "ghcr.io/savonet/liquidsoap-alpine:${TAG}"
+docker buildx imagetools create \
+  -t "savonet/liquidsoap-alpine:${TAG}" \
+  -t "savonet/liquidsoap-alpine:${COMMIT_SHORT}" \
+  "${ALPINE_AMD64}" "${ALPINE_ARM64}"
 
-docker manifest create "ghcr.io/savonet/liquidsoap-alpine:${COMMIT_SHORT}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_amd64-${OCAML_DOCKER_RELEASE_VERSION}" --amend "ghcr.io/savonet/liquidsoap-ci-build:${TAG}_alpine_arm64-${OCAML_DOCKER_RELEASE_VERSION}"
-docker manifest push "ghcr.io/savonet/liquidsoap-alpine:${COMMIT_SHORT}"
+# Push to GHCR
+docker buildx imagetools create \
+  -t "ghcr.io/savonet/liquidsoap:${TAG}" \
+  -t "ghcr.io/savonet/liquidsoap:${COMMIT_SHORT}" \
+  "${AMD64}" "${ARM64}"
+
+docker buildx imagetools create \
+  -t "ghcr.io/savonet/liquidsoap-alpine:${TAG}" \
+  -t "ghcr.io/savonet/liquidsoap-alpine:${COMMIT_SHORT}" \
+  "${ALPINE_AMD64}" "${ALPINE_ARM64}"
