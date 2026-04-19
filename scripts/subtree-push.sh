1ab663bc5eeca5cf095696b2b2c43553392d3c24
Romain Beauxis <romain.beauxis@gmail.com>
2026-04-19 13:48:03 -0500

    Better.

--- scripts/subtree-push.sh
+++ scripts/subtree-push.sh
@@ -6,6 +6,7 @@ currentBranch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$currentBranch" = "main" ]; then
   git diff-tree --no-commit-id --name-only -r "HEAD..origin/main" | cut -d'/' -f 1-4 | sort -u | grep src/modules/subtree | while read -r dir; do
       echo "Changes detected in subtree $dir. Syncing..."
-      git subtree push --prefix="$dir" subtree-remote main
+      module=$(echo "$dir" | cut -d'/' -f 4)
+      git subtree push --prefix="$dir" "https://github.com/savonet/ocaml-$module" main
   done
 fi
