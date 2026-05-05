8540b18745d4518483f7775e8a00dedf21db8c49
Romain Beauxis <romain.beauxis@gmail.com>
2026-05-05 17:11:02 -0500

    mutex_utils: add RW-lock state type with mutable_lock and atomic_lock

--- src/core/base/tools/mutex_utils.ml
+++ src/core/base/tools/mutex_utils.ml
@@ -30,3 +30,59 @@ let[@inline never] mutexify m f x =
     | v ->
         Mutex.unlock m;
         v
+
+type state = {
+  mutex : Mutex.t;
+  condition : Condition.t;
+  lock : [ `Locked | `Mutating | `None ] Atomic.t;
+}
+
+let mk_state () =
+  {
+    mutex = Mutex.create ();
+    condition = Condition.create ();
+    lock = Atomic.make `None;
+  }
+
+let[@inline always] on_mutex_done state =
+  Mutex.lock state.mutex;
+  Condition.broadcast state.condition;
+  Atomic.set state.lock `None;
+  Mutex.unlock state.mutex
+
+let rec mutable_wait state =
+  if not (Atomic.compare_and_set state.lock `None `Mutating) then (
+    Domain.cpu_relax ();
+    mutable_wait state)
+
+let mutable_lock ~state fn v =
+  mutable_wait state;
+  try
+    let v = fn v in
+    on_mutex_done state;
+    v
+  with exn ->
+    let bt = Printexc.get_raw_backtrace () in
+    on_mutex_done state;
+    Printexc.raise_with_backtrace exn bt
+
+let rec atomic_wait state =
+  if not (Atomic.compare_and_set state.lock `None `Locked) then (
+    (match Atomic.get state.lock with
+      | `Mutating ->
+          mutexify state.mutex
+            (fun () -> Condition.wait state.condition state.mutex)
+            ()
+      | _ -> Domain.cpu_relax ());
+    atomic_wait state)
+
+let atomic_lock ~state fn v =
+  atomic_wait state;
+  try
+    let v = fn v in
+    Atomic.set state.lock `None;
+    v
+  with exn ->
+    let bt = Printexc.get_raw_backtrace () in
+    Atomic.set state.lock `None;
+    Printexc.raise_with_backtrace exn bt
