7aaae3d4f0f1e046883e610f5f2c1d09a2e15268
Romain Beauxis <romain.beauxis@gmail.com>
2026-06-01 13:27:04 -0500

    Version 1.3.0

--- .github/scripts/build-posix.sh
+++ .github/scripts/build-posix.sh
@@ -50,7 +50,7 @@ cd /tmp/liquidsoap-full
 for pkg in ogg vorbis opus flac; do
   opam pin -y -n add "$pkg" ./ocaml-xiph
 done
-opam install -y --no-depexts ogg vorbis opus flac ffmpeg.1.4.0 ffmpeg-avutil.1.4.0
+opam install -y --no-depexts ogg vorbis opus flac ffmpeg.1.3.0 ffmpeg-avutil.1.3.0
 
 opam pin -y add re 1.13.2
 # tsdl-ttf 0.7 regressed the Linux dlopen path back to the unversioned
