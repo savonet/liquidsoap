8d680cebb1bf28949df7d939e24b85cc735860f4
Romain Beauxis <romain.beauxis@gmail.com>
2026-06-10 22:42:11 -0500

    Try that.

--- .github/scripts/build-win32.sh
+++ .github/scripts/build-win32.sh
@@ -9,7 +9,6 @@ IS_ROLLING_RELEASE="$4"
 IS_RELEASE="$5"
 GITHUB_SHA="$6"
 
-OPAM_PREFIX="$(opam var prefix)"
 VERSION="$(opam show -f version ./opam/liquidsoap.opam | cut -d'-' -f 1)"
 PWD="$(dirname "$0")"
 BASE_DIR="$(cd "${PWD}/../.." && pwd)"
@@ -52,14 +51,21 @@ echo "::endgroup::"
 
 echo "::group::Install liquidsoap-windows"
 unset PKG_CONFIG_PATH
-opam install -y .github/opam/liquidsoap-windows.opam
-echo "::endgroup::"
+opam install -y --deps-only .github/opam/liquidsoap-windows.opam
 
-find "${OPAM_PREFIX}"/windows-sysroot/
+export LIQUIDSOAP_BUILD_TARGET=standalone
+export LIQUIDSOAP_SYS_CONFIG=mingw
+export LIQUIDSOAP_ENABLE_BUILD_CONFIG=false
+export LIQUIDSOAP_INSTALL_NO_OPTIONAL_FAIL=true
+export LIQUIDSOAP_LDFLAGS="-lcurl -lwldap32 -ldl -lnghttp2 -lpsl -lssh2 -lidn2 -lzstd -lunistring -lbrotlicommon -lbrotlidec -lcrypt32 -liconv -lpthread -lz -lbcrypt -lwinmm -lksuser -link /usr/src/mxe/usr/x86_64-w64-mingw32.static/lib/libavutil.a"
+export PKG_CONFIG_PATH_default_windows=/usr/src/mxe/usr/x86_64-w64-mingw32.static/lib/pkgconfig
+dune build -x windows @install --release
+
+echo "::endgroup::"
 
 echo "::group::Save build config"
 
-wine "${OPAM_PREFIX}/windows-sysroot/bin/liquidsoap" --build-config >> "/tmp/${GITHUB_RUN_NUMBER}/win32/dist/liquidsoap-$BUILD.config"
+wine "${BASE_DIR}/_build/default.windows/src/bin/liquidsoap.exe" --build-config >> "/tmp/${GITHUB_RUN_NUMBER}/win32/dist/liquidsoap-$BUILD.config"
 
 echo "Build config:"
 
@@ -73,7 +79,7 @@ cd ~
 cp -R "${BASE_DIR}/.github/win32" "liquidsoap-$BUILD"
 cp -R "${BASE_DIR}/src/libs" "liquidsoap-$BUILD"
 cd "liquidsoap-$BUILD"
-cp "${OPAM_PREFIX}"/windows-sysroot/bin/liquidsoap ./liquidsoap.exe
+cp "${BASE_DIR}/_build/default.windows/src/bin/liquidsoap.exe" ./liquidsoap.exe
 cp -R "$(ocamlfind -toolchain windows ocamlc -where)/../../share/camomile" .
 cd ..
 zip -r "liquidsoap-$BUILD.zip" "liquidsoap-$BUILD"
