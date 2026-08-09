6292ca588614f9fbd4d405fce7d05df671f90011
Romain Beauxis <romain.beauxis@gmail.com>
2026-08-08 19:50:46 -0500

    doc: stop rendering and installing HTML documentation
    The documentation is published online, built from the markdown by
    savonet/website. Rendering a second copy with pandoc and installing it served no
    one, and it was actively wrong after the previous commit: pages now link to each
    other as .md, which pandoc passes through unchanged, so every link in the
    installed HTML pointed at a file that was not there.
    
    gen_dune no longer emits the pandoc rules or the install stanza, and dune.inc
    drops from 17628 lines to 2287. What it still generates is what is actually
    used: the five files the binary produces for the reference, and the
    liquidsoap --check rule for every snippet under content/liq, 298 of them.
    
    The man page still uses pandoc and is untouched.
    
    Left for a follow-up, since they are entangled with doc/Makefile's local test
    target: the pandoc-include dependency, doc/template.html and doc/language.dtd.
    Note that doc/orig/images has to stay whatever happens -- doc/content/images is
    a symlink to it.

--- doc/gen_dune.ml
+++ doc/gen_dune.ml
@@ -15,9 +15,6 @@ let generated_md =
     ("settings.md", "--list-settings --disable-deprecated", None);
   ]
 
-let mk_html f =
-  Pcre.substitute ~rex:(Pcre.regexp "md(?:\\.in)?$") ~subst:(fun _ -> "html") f
-
 let mk_md ?(content = true) f =
   if Pcre.pmatch ~rex:(Pcre.regexp "md\\.in$") f then
     Pcre.substitute ~rex:(Pcre.regexp "\\.in$")
@@ -25,8 +22,6 @@ let mk_md ?(content = true) f =
       (Filename.basename f)
   else if content then "content/" ^ f
   else f
-
-let mk_title = Filename.remove_extension
 
 let mk_subst_rule f =
   if Pcre.pmatch ~rex:(Pcre.regexp "md\\.in$") f then (
@@ -44,42 +39,6 @@ let mk_subst_rule f =
     (with-stdout-to %%{target}
       (run %%{subst_md} %%{in_md}))))|}
       f target)
-
-let mk_html_rule ~liq ~content f =
-  let liq = liq |> List.map (fun f -> "    " ^ f) |> String.concat "\n" in
-  Printf.printf
-    {|
-(rule
-  (alias doc)
-  (package liquidsoap)
-  (enabled_if (not (and %%{bin-available:pandoc} %%{bin-available:pandoc-include})))
-  (deps (:no_pandoc no-pandoc))
-  (target %s)
-  (action (run cp %%{no_pandoc} %%{target}))
-)
-
-(rule
-  (alias doc)
-  (package liquidsoap)
-  (enabled_if (and %%{bin-available:pandoc} %%{bin-available:pandoc-include}))
-  (deps
-    liquidsoap.xml
-    language.dtd
-    template.html
-%s
-    (:md %s)
-  )
-  (target %s)
-  (action
-    (pipe-stdout
-      (run pandoc %%{md} -t json)
-      (run pandoc-include --directory content/liq)
-      (run pandoc -f json --syntax-definition=liquidsoap.xml --syntax-highlighting=pygments --metadata pagetitle=%s --template=template.html -o %%{target})
-    )
-  )
-)
-|}
-    (mk_html f) liq (mk_md ~content f) (mk_html f) (mk_title f)
 
 let mk_generated_rule (file, option, header) =
   let header_deps, header_action, header_close =
@@ -132,19 +91,6 @@ let mk_test_rule file =
 |}
     test_name file file
 
-let mk_html_install f =
-  Printf.sprintf {|    (%s as html/%s)|} (mk_html f) (mk_html f)
-
-let rec readdir ?(cur = []) ~location dir =
-  List.fold_left
-    (fun cur file ->
-      let file = Filename.concat dir file in
-      if Sys.is_directory (Filename.concat location file) then
-        readdir ~cur ~location file
-      else file :: cur)
-    cur
-    (Build_tools.read_files ~location dir)
-
 let () =
   let location = Filename.dirname Sys.executable_name in
   let md =
@@ -163,30 +109,7 @@ let () =
   in
   List.iter mk_generated_rule generated_md;
   List.iter mk_subst_rule md;
-  List.iter
-    (fun (file, _, _) -> mk_html_rule ~liq ~content:false file)
-    generated_md;
-  List.iter (mk_html_rule ~liq ~content:true) md;
-  List.iter mk_test_rule liq;
-  let files =
-    List.map
-      (fun f -> Printf.sprintf {|    (orig/%s as html/%s)|} f f)
-      (readdir ~location:(Filename.concat location "orig") "")
-    @ List.map (fun (f, _, _) -> mk_html_install f) generated_md
-    @ List.map mk_html_install md
-  in
-  let files = files |> List.sort compare |> String.concat "\n" in
-  Printf.printf
-    {|
-(install
-  (section doc)
-  (package liquidsoap)
-  (files
-%s
-  )
-)
-|}
-    files
+  List.iter mk_test_rule liq
 
 let () =
   Printf.printf
